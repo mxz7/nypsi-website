683584c80d459a743f6fbbdeb348fe313008d4c7
max <max.0x71@gmail.com>
2021-05-08 17:07:03 +0100

    removed useless js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
 $(window).on("load", function() {
     $("body").removeClass("preload")
 })
-
-let image = "fun-1"
-
-/**
- * 
- * @param {String} name 
- */
-function changeImage(name) {
-    name = name.toLowerCase()
-    if (image == name) return
-
-    console.log(name)
-
-    console.log(image)
-
-    document.getElementById(image).style.display = "none"
-
-    document.getElementById(name).style.display = "inline"
-
-    image = name
-}
