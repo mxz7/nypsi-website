6cdfc1383583186263dc3a9b9de7e9e3fbde493c
kestas69 <kestas@len.localdomain>
2023-05-28 22:41:32 +0300

    smarty pants max

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,11 @@
 </head>
 <body>
     <div class="h-screen bg-gray-900 flex justify-center items-center">
-        <div class="flex flex-col items-center">
+        <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 text-center">
             <h1 class="text-white text-9xl font-bold">nypsi</h1>
             <p class="text-gray-400 text-2xl font-bold mt-5">a pp n booby bot fr</p>
     
-            <div class="w-3/4 h-1 bg-red-500 rounded-full mt-3"></div>
+            <div class="w-3/4 h-1 bg-red-500 rounded-full mt-3 m-auto"></div>
     
             <div class="flex flex-col sm:flex-row justify-center items-center mt-2">
                 <a href="https://discord.com/invite/hJTDNST" class="flex flex-col justify-center items-center mb-4 sm:mb-0 sm:mr-4">
