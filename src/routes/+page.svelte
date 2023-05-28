5885378a931e95a9c4ccfd191958e0339d30dcf0
kestas <75070927+superboxes@users.noreply.github.com>
2023-05-28 22:49:50 +0300

    main page kinda i think yessir (#14)
    * devved it up main page kinda done
    
    * smarty pants max
    
    ---------
    
    Co-authored-by: kestas69 <kestas@len.localdomain>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nypsi</title>
+</head>
+<body>
+    <div class="h-screen bg-gray-900 flex justify-center items-center">
+        <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 text-center">
+            <h1 class="text-white text-9xl font-bold">nypsi</h1>
+            <p class="text-gray-400 text-2xl font-bold mt-5">a pp n booby bot fr</p>
+    
+            <div class="w-3/4 h-1 bg-red-500 rounded-full mt-3 m-auto"></div>
+    
+            <div class="flex flex-col sm:flex-row justify-center items-center mt-2">
+                <a href="https://discord.com/invite/hJTDNST" class="flex flex-col justify-center items-center mb-4 sm:mb-0 sm:mr-4">
+                    <p class="text-white text-2xl font-bold mt-2">discord</p>
+                </a>
+    
+                <a href="https://docs.nypsi.xyz/" class="flex flex-col justify-center items-center mb-4 sm:mb-0 sm:mr-4">
+                    <p class="text-white text-2xl font-bold mt-2">docs</p>
+                </a>
+    
+                <a href="https://statcord.com/bot/678711738845102087" class="flex flex-col justify-center items-center mb-4 sm:mb-0 sm:mr-4">
+                    <p class="text-white text-2xl font-bold mt-2">features</p>
+                </a>
+    
+                <a href="https://discord.com/oauth2/authorize?client_id=678711738845102087&permissions=1377879583830&scope=bot%20applications.commands" target="_blank" class="flex flex-col justify-center items-center mb-4 sm:mb-0 sm:mr-4">
+                    <p class="text-white text-2xl font-bold mt-2">invite</p>
+                </a>
+    
+                <a href="https://ko-fi.com/tekoh/tiers" class="flex flex-col justify-center items-center">
+                    <p class="text-white text-2xl font-bold mt-2">donate</p>
+                </a>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
