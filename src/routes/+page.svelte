e406d3d96c4a0e48f72f98f3a8c0b28a156320c5
max <29782397+tekoh@users.noreply.github.com>
2023-05-28 21:03:25 +0100

    refactor: svelte fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>nypsi</title>
-  </head>
-  <body>
-    <div class="h-screen bg-gray-900 flex justify-center items-center">
-      <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 text-center">
-        <h1 class="text-white text-9xl font-bold">nypsi</h1>
-        <p class="text-gray-400 text-2xl font-bold mt-5">the best discord bot</p>
+<svelte:head>
+  <title>nypsi</title>
+  <meta
+    name="description"
+    content="
+  the best discord bot. includes gambling, economy, items, fake cryptocurrency, moderation, reaction roles, channel stats"
+  />
+</svelte:head>
 
-        <div class="w-3/4 h-1 bg-red-500 rounded-full mt-3 m-auto lg:w-full" />
+<div class="h-screen flex justify-center items-center">
+  <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 text-center">
+    <h1 class="text-white text-9xl font-bold">nypsi</h1>
+    <p class="text-gray-400 text-2xl font-bold mt-5">the best discord bot</p>
 
-        <div class="flex flex-col sm:flex-row justify-center items-center mt-2">
-          <a
-            href="https://discord.com/invite/hJTDNST"
-            class="flex flex-col justify-center items-center mb-4 sm:mb-0 sm:mr-4"
-          >
-            <p class="text-white text-2xl font-bold mt-2">discord</p>
-          </a>
+    <div class="w-3/4 h-1 bg-red-500 rounded-full mt-3 m-auto lg:w-full" />
 
-          <a href="https://docs.nypsi.xyz/" class="flex flex-col justify-center items-center mb-4 sm:mb-0 sm:mr-4">
-            <p class="text-white text-2xl font-bold mt-2">docs</p>
-          </a>
+    <div class="flex flex-col sm:flex-row justify-center items-center mt-2">
+      <a href="https://discord.com/invite/hJTDNST" class="flex flex-col justify-center items-center mb-4 sm:mb-0 sm:mr-4">
+        <p class="text-white text-2xl font-bold mt-2">discord</p>
+      </a>
 
-          <a href="#features" class="flex flex-col justify-center items-center mb-4 sm:mb-0 sm:mr-4">
-            <p class="text-white text-2xl font-bold mt-2">features</p>
-          </a>
+      <a href="https://docs.nypsi.xyz/" class="flex flex-col justify-center items-center mb-4 sm:mb-0 sm:mr-4">
+        <p class="text-white text-2xl font-bold mt-2">docs</p>
+      </a>
 
-          <a
-            href="https://discord.com/oauth2/authorize?client_id=678711738845102087&permissions=1377879583830&scope=bot%20applications.commands"
-            target="_blank"
-            class="flex flex-col justify-center items-center mb-4 sm:mb-0 sm:mr-4"
-          >
-            <p class="text-white text-2xl font-bold mt-2">invite</p>
-          </a>
+      <a href="#features" class="flex flex-col justify-center items-center mb-4 sm:mb-0 sm:mr-4">
+        <p class="text-white text-2xl font-bold mt-2">features</p>
+      </a>
 
-          <a href="https://ko-fi.com/tekoh/tiers" class="flex flex-col justify-center items-center">
-            <p class="text-white text-2xl font-bold mt-2">donate</p>
-          </a>
-        </div>
-      </div>
+      <a
+        href="https://discord.com/oauth2/authorize?client_id=678711738845102087&permissions=1377879583830&scope=bot%20applications.commands"
+        target="_blank"
+        class="flex flex-col justify-center items-center mb-4 sm:mb-0 sm:mr-4"
+      >
+        <p class="text-white text-2xl font-bold mt-2">invite</p>
+      </a>
+
+      <a href="https://ko-fi.com/tekoh/tiers" class="flex flex-col justify-center items-center">
+        <p class="text-white text-2xl font-bold mt-2">donate</p>
+      </a>
     </div>
-  </body>
-</html>
+  </div>
+</div>
