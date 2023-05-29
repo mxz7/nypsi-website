ee8575c79465e35adc89be49c598b4bfb1707675
max <29782397+tekoh@users.noreply.github.com>
2023-05-29 19:55:42 +0100

    feat: error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<img
+  class="absolute top-1/3 lg:top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-9/12 lg:w-1/2"
+  src="https://http.cat/{$page.status}"
+  alt=""
+/>
