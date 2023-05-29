682358303e2c626bd1e5ec226f1576a0571aed87
max <29782397+tekoh@users.noreply.github.com>
2023-05-29 23:38:40 +0100

    refactor: move leaderboard to component

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -1,40 +1,19 @@
 <script>
+  import MiniLeaderboard from "../../lib/components/MiniLeaderboard.svelte";
+
   let balance = [
-    { name: "kestas", bal: 5000000 },
-    { name: "max", bal: 400000 },
-    { name: "psychotic", bal: 1 },
-    { name: "kestas", bal: 5000000 },
-    { name: "max", bal: 400000 },
-    { name: "psychotic", bal: 1 },
-    { name: "kestas", bal: 5000000 },
-    { name: "max", bal: 400000 },
-    { name: "psychotic", bal: 1 },
-    { name: "kestas", bal: 5000000 },
-    { name: "max", bal: 400000 },
-    { name: "psychotic", bal: 1 }
-
-    // Add more dummy
-  ].slice(0, 10); // Display only top 10 entries
-
-  let prestige = [
-    { name: "kestas", pres: 69 },
-    { name: "max", pres: 42 },
-    { name: "psychotic", pres: 3 },
-    { name: "kestas", pres: 69 },
-    { name: "max", pres: 42 },
-    { name: "psychotic", pres: 3 },
-    { name: "kestas", pres: 69 },
-    { name: "max", pres: 42 },
-    { name: "psychotic", pres: 3 },
-    { name: "kestas", pres: 69 },
-    { name: "max", pres: 42 },
-    { name: "psychotic", pres: 3 },
-    { name: "kestas", pres: 69 },
-    { name: "max", pres: 42 },
-    { name: "psychotic", pres: 3 },
-    { name: "kestas", pres: 69 },
-    { name: "max", pres: 42 },
-    { name: "psychotic", pres: 3 }
+    { user: "kestas", value: 5000000 },
+    { user: "max", value: 400000 },
+    { user: "psychotic", value: 1 },
+    { user: "kestas", value: 5000000 },
+    { user: "max", value: 400000 },
+    { user: "psychotic", value: 1 },
+    { user: "kestas", value: 5000000 },
+    { user: "max", value: 400000 },
+    { user: "psychotic", value: 1 },
+    { user: "kestas", value: 5000000 },
+    { user: "max", value: 400000 },
+    { user: "psychotic", value: 1 }
 
     // Add more dummy
   ].slice(0, 10); // Display only top 10 entries
@@ -51,20 +30,7 @@
 </header>
 
 <div class="flex flex-row mt-10 overflow-x-auto text-white sm:p-3 overflow-y-hidden">
-  <div class="mx-5 sm:grow">
-    <h2 class="text-center text-xl font-bold sm:text-3xl">top balance</h2>
-    <table class="mt-1 text-sm sm:text-xl mx-auto">
-      <tbody>
-        {#each balance as { name, bal }}
-          <tr
-            class="bg-gray-950 border-b-[8px] border-gray-900 border-opacity-100 bg-opacity-20 hover:scale-105 duration-200 ease-in"
-          >
-            <td class="px-1 py-1 text-gray-400">#{balance.findIndex((item) => item.name === name) + 1}</td>
-            <td class="px-4 py-1">{name}</td>
-            <td class="px-4 py-1 text-right">${bal.toLocaleString()}</td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
-  </div>
+  <MiniLeaderboard data={balance} />
+  <MiniLeaderboard data={balance} />
+  <MiniLeaderboard data={balance} />
 </div>
