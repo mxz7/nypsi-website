6ee5590b2f053704608c3242a349bc8c87af365f
max <29782397+tekoh@users.noreply.github.com>
2023-05-29 20:10:30 +0100

    refactor: spacing from top

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,11 +37,11 @@
 </div>
 
 <div class="h-screen flex justify-center items-center">
-  <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 text-center">
+  <div class="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
     <h1 class="text-white text-9xl font-bold">nypsi</h1>
     <p class="text-gray-400 text-2xl font-bold mt-5">the best discord bot</p>
 
-    <div class="w-3/4 h-1 bg-red-500 rounded-full mt-3 m-auto lg:w-full" />
+    <div class="w-3/4 h-1 bg-red-500 rounded-full mt-3 m-auto sm:w-full" />
 
     <div class="flex flex-col sm:flex-row justify-center items-center mt-2">
       <a href="https://discord.com/invite/hJTDNST" class="flex flex-col justify-center items-center mb-4 sm:mb-0 sm:mr-4">
