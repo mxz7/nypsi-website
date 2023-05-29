a581cca076373bbe63b637691d966866697f7d0b
max <29782397+tekoh@users.noreply.github.com>
2023-05-29 20:10:34 +0100

    fix: scaling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <img
-  class="absolute top-1/3 lg:top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-9/12 lg:w-1/2"
+  class="absolute top-1/3 sm:top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-9/12 sm:w-1/2"
   src="https://http.cat/{$page.status}"
   alt=""
 />
