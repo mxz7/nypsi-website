f7e4ef1d593e80bfb4c91057a9ad4d7b548036c7
max <29782397+tekoh@users.noreply.github.com>
2023-05-29 22:08:28 +0100

    comitting before it breaks again

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -1,65 +1,70 @@
-<svelte:head>
-  <title>nypsi leaderboard</title>
-  <meta name="description" content="leaderboard for the nypsi bot" />
-</svelte:head>
-
 <script>
   let balance = [
-    { name: 'kestas', bal: 5000000 },
-    { name: 'max', bal: 400000 },
-    { name: 'psychotic', bal: 1 },
+    { name: "kestas", bal: 5000000 },
+    { name: "max", bal: 400000 },
+    { name: "psychotic", bal: 1 },
+    { name: "kestas", bal: 5000000 },
+    { name: "max", bal: 400000 },
+    { name: "psychotic", bal: 1 },
+    { name: "kestas", bal: 5000000 },
+    { name: "max", bal: 400000 },
+    { name: "psychotic", bal: 1 },
+    { name: "kestas", bal: 5000000 },
+    { name: "max", bal: 400000 },
+    { name: "psychotic", bal: 1 }
+
     // Add more dummy
   ].slice(0, 10); // Display only top 10 entries
 
   let prestige = [
-    { name: 'kestas', pres: 69 },
-    { name: 'max', pres: 42 },
-    { name: 'psychotic', pres: 3 },
+    { name: "kestas", pres: 69 },
+    { name: "max", pres: 42 },
+    { name: "psychotic", pres: 3 },
+    { name: "kestas", pres: 69 },
+    { name: "max", pres: 42 },
+    { name: "psychotic", pres: 3 },
+    { name: "kestas", pres: 69 },
+    { name: "max", pres: 42 },
+    { name: "psychotic", pres: 3 },
+    { name: "kestas", pres: 69 },
+    { name: "max", pres: 42 },
+    { name: "psychotic", pres: 3 },
+    { name: "kestas", pres: 69 },
+    { name: "max", pres: 42 },
+    { name: "psychotic", pres: 3 },
+    { name: "kestas", pres: 69 },
+    { name: "max", pres: 42 },
+    { name: "psychotic", pres: 3 }
+
     // Add more dummy
   ].slice(0, 10); // Display only top 10 entries
 </script>
 
-<div class="h-screen flex justify-center items-center">
-  <div class="flex flex-col text-center">
-    <h1 class="text-white text-6xl font-bold">leaderboard</h1>
-    <div class="w-3/4 h-1 bg-red-500 rounded-full mt-3 m-auto lg:w-full"></div>
+<svelte:head>
+  <title>nypsi leaderboards</title>
+  <meta name="description" content="leaderboards for the nypsi discord bot" />
+</svelte:head>
 
-    <div class="flex mt-8">
-      <div class="w-1/2 mx-10">
-        <h2 class="text-white text-3xl font-bold">Top Bal</h2>
-        <div class="w-3/4 h-1 bg-red-500 rounded-full mt-3 m-auto lg:w-1/2"></div>
+<header class="text-center mt-5">
+  <h1 class="text-white text-5xl font-bold">leaderboards</h1>
+  <div class="w-3/4 sm:w-96 h-1 bg-red-500 rounded-full mt-3 m-auto" />
+</header>
 
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 mt-8">
-          <thead>
-          </thead>
-          <tbody>
-            {#each balance as { name, bal }}
-            <tr>
-              <td class="border px-4 py-2 text-white">{name}</td>
-              <td class="border px-4 py-2 text-white">{bal}</td>
-            </tr>
-            {/each}
-          </tbody>
-        </table>
-      </div>
-
-      <div class="w-1/2">
-        <h2 class="text-white text-3xl font-bold">Top Pres</h2>
-        <div class="w-3/4 h-1 bg-red-500 rounded-full mt-3 m-auto lg:w-1/2"></div>
-
-        <table class="table-auto mt-8">
-          <thead>
-          </thead>
-          <tbody>
-            {#each prestige as { name, pres }}
-            <tr>
-              <td class="border px-4 py-2 text-white">{name}</td>
-              <td class="border px-4 py-2 text-white">{pres}</td>
-            </tr>
-            {/each}
-          </tbody>
-        </table>
-      </div>
-    </div>
+<div class="flex flex-row mt-10 overflow-x-auto text-white sm:p-3 overflow-y-hidden">
+  <div class="mx-5 sm:grow">
+    <h2 class="text-center text-xl font-bold sm:text-3xl">top balance</h2>
+    <table class="mt-1 text-sm sm:text-xl mx-auto">
+      <tbody>
+        {#each balance as { name, bal }}
+          <tr
+            class="bg-gray-950 border-b-[8px] border-gray-900 border-opacity-100 bg-opacity-20 hover:scale-105 duration-200 ease-in"
+          >
+            <td class="px-1 py-1 text-gray-400">#{balance.findIndex((item) => item.name === name) + 1}</td>
+            <td class="px-4 py-1">{name}</td>
+            <td class="px-4 py-1 text-right">${bal.toLocaleString()}</td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
   </div>
 </div>
