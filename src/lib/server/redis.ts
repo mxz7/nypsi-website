41e04800bf45d88319518afc7e91a215532426fe
max <29782397+tekoh@users.noreply.github.com>
2023-05-30 20:07:34 +0100

    feat: change redis provider

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,1 +1,9 @@
+import { REDIS_PASS, REDIS_URL } from "$env/static/private";
+import { Redis } from "@upstash/redis";
 
+const redis = new Redis({
+  url: REDIS_URL,
+  token: REDIS_PASS
+});
+
+export default redis;
--- src/routes/api/leaderboard/prestige/+server.ts
+++ src/routes/api/leaderboard/prestige/+server.ts
@@ -1,7 +1,7 @@
 import prisma from "$lib/server/database";
 import rateLimiter from "$lib/server/ratelimit.js";
+import redis from "$lib/server/redis.js";
 import { json } from "@sveltejs/kit";
-import { kv } from "@vercel/kv";
 
 export async function GET({ getClientAddress }) {
   const rateLimitAttempt = await rateLimiter.limit(getClientAddress());
@@ -13,8 +13,8 @@ export async function GET({ getClientAddress }) {
     });
   }
 
-  if (await kv.exists("top-prestiges")) {
-    return json(await kv.get("top-prestiges"));
+  if (await redis.exists("top-prestiges")) {
+    return json(await redis.get("top-prestiges"));
   }
 
   const query = await prisma.economy
@@ -52,7 +52,7 @@ export async function GET({ getClientAddress }) {
       });
     });
 
-  await kv.set("top-prestiges", JSON.stringify(query), { ex: 300 });
+  await redis.set("top-prestiges", JSON.stringify(query), { ex: 300 });
 
   return json(query);
 }
