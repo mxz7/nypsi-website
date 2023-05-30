443c3fb3e51c13c579e78fa728ca338faaed9f50
max <29782397+tekoh@users.noreply.github.com>
2023-05-30 13:19:37 +0100

    feat: desktop scaling

--- src/lib/components/ItemIcon.svelte
+++ src/lib/components/ItemIcon.svelte
@@ -5,7 +5,7 @@
 
 <a
   href={url}
-  class="p-3 bg-gray-950 bg-opacity-20 w-12 h-12 flex justify-center items-center m-1 rounded-md duration-200 ease-in hover:scale-105 border border-transparent hover:border-gray-500"
+  class="p-3 bg-gray-950 bg-opacity-20 w-12 h-12 sm:w-16 sm:h-16 flex justify-center items-center m-1 sm:m-1.5 rounded-md duration-200 ease-in hover:scale-105 border border-transparent hover:border-gray-500"
 >
   <img class="max-w-full max-h-full object-contain w-auto h-auto" src={item.emoji} alt="" />
 </a>
