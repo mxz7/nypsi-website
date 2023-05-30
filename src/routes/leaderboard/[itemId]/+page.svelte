f328057b796f4d0fc50b239e240ddfe105c17311
max <29782397+tekoh@users.noreply.github.com>
2023-05-30 19:33:37 +0100

    fix: scaling

--- src/routes/leaderboard/[itemId]/+page.svelte
+++ src/routes/leaderboard/[itemId]/+page.svelte
@@ -8,13 +8,15 @@
 </svelte:head>
 
 {#if data.item}
-  <header class="text-center mt-5">
-    <h1 class="text-white text-5xl font-bold">{data.item.plural ? data.item.plural : `${data.item.name}s`} leaderboard</h1>
-    <div class="w-3/4 sm:w-96 h-1 bg-red-500 rounded-full mt-3 m-auto" />
+  <header class="text-center mt-5 sm:w-full">
+    <h1 class="text-white text-2xl sm:text-4xl font-bold">
+      {data.item.plural ? data.item.plural : `${data.item.name}s`} leaderboard
+    </h1>
+    <div class="w-3/4 sm:w-1/2 h-1 bg-red-500 rounded-full mt-3 m-auto" />
   </header>
 
-  <div class="mt-10 px-5">
-    <table class="mt-1 sm:text-xl mx-auto text-gray-200 w-full">
+  <div class="mt-10 px-5 sm:px-24">
+    <table class="mt-1 sm:text-xl mx-auto text-gray-200 w-full sm:w-1/2">
       <tbody>
         {#each data.itemData.slice(0, 100) as { username, value, position }}
           <tr
@@ -22,7 +24,9 @@
           >
             <td class="px-1 py-1 text-gray-400">#{position}</td>
             <td class="px-4 py-1">{username}</td>
-            <td class="px-4 py-1 text-right">{value} {data.item.plural ? data.item.plural : `${data.item.name}s`}</td>
+            <td class="px-4 py-1 text-right"
+              >{value} <span class="opacity-75">{data.item.plural ? data.item.plural : `${data.item.name}s`}</span></td
+            >
           </tr>
         {/each}
       </tbody>
