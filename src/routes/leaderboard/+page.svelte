c4fff61471ceff47c41bb88a19100ba9c4ad54ef
kestas <75070927+superboxes@users.noreply.github.com>
2023-05-30 15:25:59 +0300

    feat: add leaderboards (#18)
    * no idea what to do, handing it off to max
    
    * comitting before it breaks again
    
    * refactor: move leaderboard to component
    
    * fix: add title to component
    
    * fix: ensure 10 or less
    
    * feat: items list
    
    * feat: item searching
    
    * fix: case sensitivity
    
    * feat: desktop scaling
    
    ---------
    
    Co-authored-by: kestas69 <kestas@len.localdomain>
    Co-authored-by: max <29782397+tekoh@users.noreply.github.com>

---
+++ src/routes/leaderboard/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+  import ItemList from "$lib/components/ItemList.svelte";
+  import MiniLeaderboard from "../../lib/components/MiniLeaderboard.svelte";
+
+  let balance = [
+    { user: "kestas", value: 5000000 },
+    { user: "max", value: 400000 },
+    { user: "psychotic", value: 1 },
+    { user: "kestas", value: 5000000 },
+    { user: "max", value: 400000 },
+    { user: "psychotic", value: 1 },
+    { user: "kestas", value: 5000000 },
+    { user: "max", value: 400000 },
+    { user: "psychotic", value: 1 },
+    { user: "kestas", value: 5000000 },
+    { user: "max", value: 400000 },
+    { user: "psychotic", value: 1 }
+
+    // Add more dummy
+  ].slice(0, 10); // Display only top 10 entries
+
+  export let data;
+</script>
+
+<svelte:head>
+  <title>nypsi leaderboards</title>
+  <meta name="description" content="leaderboards for the nypsi discord bot" />
+</svelte:head>
+
+<header class="text-center mt-5">
+  <h1 class="text-white text-5xl font-bold">leaderboards</h1>
+  <div class="w-3/4 sm:w-96 h-1 bg-red-500 rounded-full mt-3 m-auto" />
+</header>
+
+<div class="flex flex-row mt-10 overflow-x-auto text-white sm:p-3 overflow-y-hidden">
+  <MiniLeaderboard data={balance} title="top balance" />
+  <MiniLeaderboard data={balance} title="another one" />
+  <MiniLeaderboard data={balance} title="last one !!" />
+</div>
+
+<ItemList items={data.items} />
