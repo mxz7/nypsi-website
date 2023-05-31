1c88094f19c41cba9c0f8cbc79388f57ae90c4df
max <29782397+tekoh@users.noreply.github.com>
2023-05-31 16:06:25 +0100

    feat: loading icon when data loading

--- src/lib/components/LoadingIcon.svelte
+++ src/lib/components/LoadingIcon.svelte
@@ -1,5 +1,6 @@
 <div
-  class="w-full h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-30 bg-gray-900 overflow-hidden"
+  id="loadingpage"
+  class="w-full h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-30 bg-gray-900 overflow-hidden duration-700"
 >
   <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
     <div class="lds-ring">
