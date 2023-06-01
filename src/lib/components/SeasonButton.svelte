be3fe8a44061b71e8d3a4b216d8e26f3da033d5b
max <29782397+tekoh@users.noreply.github.com>
2023-06-01 17:32:13 +0100

    feat: seasons landing page

---
+++ src/lib/components/SeasonButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let number: number;
+</script>
+
+<a
+  href="/season/{number}"
+  class="p-4 bg-gray-950 text-lg bg-opacity-20 flex justify-center items-center m-1 sm:m-1.5 rounded-md duration-200 ease-in hover:scale-105 border border-transparent hover:border-gray-500"
+>
+  <p class="text-white text-center sm:text-xl">{number}</p>
+  <!-- <img class="max-w-full max-h-full object-contain w-auto h-auto" src={item.emoji} alt="" /> -->
+</a>
