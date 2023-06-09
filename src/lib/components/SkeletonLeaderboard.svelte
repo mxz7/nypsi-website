c0b83183988c64a405333668b24066877d4d13c6
max <29782397+tekoh@users.noreply.github.com>
2023-06-09 16:32:20 +0100

    fix: reduce height

--- src/lib/components/SkeletonLeaderboard.svelte
+++ src/lib/components/SkeletonLeaderboard.svelte
@@ -5,15 +5,15 @@
         class="flex animate-pulse border-b-[8px] border-gray-900 border-opacity-100 bg-gray-950 bg-opacity-20 duration-200 ease-in hover:scale-105"
       >
         <td class="px-1 py-2 text-gray-400">
-          <div class="h-3 w-6 rounded-lg !bg-gray-700 sm:h-5" />
+          <div class="h-3 w-6 rounded-lg !bg-gray-700 sm:h-4" />
         </td>
         <td class="grow px-4 py-2"
           ><div
             style="width: {Math.floor(Math.random() * 35) + 40}%"
-            class="h-3 rounded-lg bg-gray-700 sm:h-5"
+            class="h-3 rounded-lg bg-gray-700 sm:h-4"
           /></td
         >
-        <td class="px-4 py-2"><div class="h-3 w-16 rounded-lg bg-gray-700 sm:h-5" /> </td>
+        <td class="px-4 py-2"><div class="h-3 w-16 rounded-lg bg-gray-700 sm:h-4" /> </td>
       </tr>
     {/each}
   </tbody>
