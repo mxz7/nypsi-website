9615579e46d0cf3287d3f3dd97fbeab49fc1383a
max <29782397+tekoh@users.noreply.github.com>
2023-06-09 18:25:39 +0100

    feat: api for specific game

---
+++ src/routes/api/game/[id]/+server.ts
@@ -0,0 +1,63 @@
+import prisma from "$lib/server/database";
+import { json } from "@sveltejs/kit";
+
+export async function GET({ setHeaders, params }) {
+  const query = await prisma.game
+    .findUnique({
+      where: {
+        id: parseInt(params.id, 36),
+      },
+      select: {
+        bet: true,
+        date: true,
+        earned: true,
+        xpEarned: true,
+        game: true,
+        id: true,
+        outcome: true,
+        win: true,
+        economy: {
+          select: {
+            user: {
+              select: {
+                Preferences: {
+                  select: {
+                    leaderboards: true,
+                  },
+                },
+                lastKnownTag: true,
+              },
+            },
+          },
+        },
+      },
+    })
+    .then((r) => {
+      return {
+        ok: Boolean(r),
+        bet: Number(r?.bet),
+        date: r?.date,
+        earned: Number(r?.earned),
+        game: r?.game,
+        id: r?.id,
+        outcome: r?.outcome,
+        win: r?.win,
+        xpEarned: r?.xpEarned,
+        username: r?.economy?.user.Preferences?.leaderboards
+          ? r?.economy?.user.lastKnownTag.split("#")[0] || "[redacted]"
+          : "[hidden]",
+      };
+    });
+
+  if (query.ok) {
+    setHeaders({
+      "cache-control": "max-age=86400",
+    });
+  } else {
+    setHeaders({
+      "cache-control": "max-age=60",
+    });
+  }
+
+  return json(query);
+}
