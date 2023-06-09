36c6bb51ba5413ba757d04206342e83a99185bde
max <29782397+tekoh@users.noreply.github.com>
2023-06-09 20:46:12 +0100

    feat: animations

--- src/routes/game/[id]/+page.svelte
+++ src/routes/game/[id]/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import { fade, fly } from "svelte/transition";
+
   export let data;
 </script>
 
 <div class="mt-8 flex flex-col justify-center">
   {#await data.streamed.game}
-    <div class="flex items-center justify-center">
+    <div out:fade={{ duration: 300 }} class="flex items-center justify-center">
       <svg
         class="-ml-1 mr-3 h-10 w-10 animate-spin text-red-500"
         xmlns="http://www.w3.org/2000/svg"
@@ -20,70 +22,72 @@
       </svg>
     </div>
   {:then game}
-    {#if game.ok}
-      <div class="my-3">
-        {#if game.game === "slots"}
-          <div class="flex w-full items-center justify-center">
-            <img
-              src="https://cdn.discordapp.com/emojis/{game.outcome
-                ?.split('**|**')[0]
-                .split(':')[2]
-                .replace('>', '')}"
-              alt=""
-              class="h-14 w-14"
-            />
-            <p class="mx-3 text-5xl text-gray-200">-</p>
-            <img
-              src="https://cdn.discordapp.com/emojis/{game.outcome
-                ?.split('**|**')[1]
-                .split(':')[2]
-                .replace('>', '')}"
-              alt=""
-              class="h-14 w-14"
-            />
-            <p class="mx-3 text-5xl text-gray-200">-</p>
-            <img
-              src="https://cdn.discordapp.com/emojis/{game.outcome
-                ?.split('**|**')[2]
-                .split(':')[2]
-                .replace('>', '')}"
-              alt=""
-              class="h-14 w-14"
-            />
-          </div>
-        {/if}
-      </div>
-      <div class="text-center text-xl">
-        {#if game.win}
-          <p class="font-bold text-green-400">won</p>
-          <p class="text-lg text-green-400 opacity-75">
-            ${(game.earned - game.bet).toLocaleString()} profit
-          </p>
-        {:else}
-          <p class=" text-red-500">lost</p>
-        {/if}
-      </div>
+    <div in:fly={{ y: 25, delay: 300, duration: 500 }}>
+      {#if game.ok}
+        <div class="my-3">
+          {#if game.game === "slots"}
+            <div class="flex w-full items-center justify-center">
+              <img
+                src="https://cdn.discordapp.com/emojis/{game.outcome
+                  ?.split('**|**')[0]
+                  .split(':')[2]
+                  .replace('>', '')}"
+                alt=""
+                class="h-14 w-14"
+              />
+              <p class="mx-3 text-5xl text-gray-200">-</p>
+              <img
+                src="https://cdn.discordapp.com/emojis/{game.outcome
+                  ?.split('**|**')[1]
+                  .split(':')[2]
+                  .replace('>', '')}"
+                alt=""
+                class="h-14 w-14"
+              />
+              <p class="mx-3 text-5xl text-gray-200">-</p>
+              <img
+                src="https://cdn.discordapp.com/emojis/{game.outcome
+                  ?.split('**|**')[2]
+                  .split(':')[2]
+                  .replace('>', '')}"
+                alt=""
+                class="h-14 w-14"
+              />
+            </div>
+          {/if}
+        </div>
+        <div class="text-center text-xl">
+          {#if game.win}
+            <p class="font-bold text-green-400">won</p>
+            <p class="text-lg text-green-400 opacity-75">
+              ${(game.earned - game.bet).toLocaleString()} profit
+            </p>
+          {:else}
+            <p class=" text-red-500">lost</p>
+          {/if}
+        </div>
 
-      <div class="mt-5 text-center text-gray-300">
-        {#if game.win}
-          <p>
-            bet ${game.bet.toLocaleString()} and won ${game.earned.toLocaleString()}{game.xpEarned >
-            0
-              ? `(${game.xpEarned.toLocaleString()}xp)`
-              : ""}
-          </p>
-        {:else}
-          <p>bet ${game.bet.toLocaleString()}</p>
-        {/if}
+        <div class="mt-5 text-center text-gray-300">
+          {#if game.win}
+            <p>
+              bet ${game.bet.toLocaleString()} and won ${game.earned.toLocaleString()}{game.xpEarned >
+              0
+                ? `(${game.xpEarned.toLocaleString()}xp)`
+                : ""}
+            </p>
+          {:else}
+            <p>bet ${game.bet.toLocaleString()}</p>
+          {/if}
 
-        <p class=" text-center text-gray-300">
-          played by <span class="font-bold text-red-500 opacity-75">{game.username}</span> on {new Date(
-            game.date
-          ).toLocaleDateString()} at {new Date(game.date).toLocaleTimeString()}
-        </p>
-      </div>
-    {:else}
-      <p class="text-xl font-bold text-white">unknown game</p>
-    {/if}
+          <p class=" text-center text-gray-300">
+            played by <span class="font-bold text-red-500 opacity-75">{game.username}</span> on {new Date(
+              game.date
+            ).toLocaleDateString()} at {new Date(game.date).toLocaleTimeString()}
+          </p>
+        </div>
+      {:else}
+        <p class="text-xl font-bold text-white">unknown game</p>
+      {/if}
+    </div>
   {/await}
 </div>
