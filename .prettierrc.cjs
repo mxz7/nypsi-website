9a9a07748799c9d3f71ca004665be5c08d27e48b
max <29782397+tekoh@users.noreply.github.com>
2023-06-09 16:35:32 +0100

    style: use mostly prettier defaults

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,8 +2,5 @@ module.exports = {
   pluginSearchDirs: false,
   plugins: [require("prettier-plugin-svelte"), require("prettier-plugin-tailwindcss")],
   overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
-  trailingComma: "none",
-  printWidth: 100,
-  tabWidth: 2,
-  endOfLine: "lf"
+  printWidth: 100
 };
