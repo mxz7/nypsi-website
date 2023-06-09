b7148d82f81c5d67969b4273bd6903db83bfad9f
max <29782397+tekoh@users.noreply.github.com>
2023-06-09 22:59:18 +0100

    refactor: lazy loading

--- src/lib/components/ItemIcon.svelte
+++ src/lib/components/ItemIcon.svelte
@@ -7,5 +7,10 @@
   class="m-1 flex h-16 w-16 items-center justify-center rounded-md border border-transparent bg-gray-950 bg-opacity-20 p-3 duration-200 ease-in hover:scale-105 hover:border-gray-500 sm:m-1.5 sm:h-20 sm:w-20"
 >
   <!-- <p>{item.id}</p> -->
-  <img class="h-auto max-h-full w-auto max-w-full object-contain" src={item.emoji} alt="" />
+  <img
+    class="h-auto max-h-full w-auto max-w-full object-contain"
+    src={item.emoji}
+    alt=""
+    loading="lazy"
+  />
 </a>
