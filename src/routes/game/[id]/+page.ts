4e8652e9090ca9e700f14a4daf0eef4b6eddd6e6
max <29782397+tekoh@users.noreply.github.com>
2023-06-09 20:41:07 +0100

    fix: lowercase

--- src/routes/game/[id]/+page.ts
+++ src/routes/game/[id]/+page.ts
@@ -2,6 +2,8 @@ import type Game from "$lib/types/Game.js";
 
 export const load = async ({ fetch, params }) => {
   return {
-    streamed: { game: fetch(`/api/game/${params.id}`).then((r) => r.json()) as Promise<Game> },
+    streamed: {
+      game: fetch(`/api/game/${params.id.toLowerCase()}`).then((r) => r.json()) as Promise<Game>,
+    },
   };
 };
