f7a1f0af9a35a12fcffaff5fb3ba9fe352da6be2
max <29782397+tekoh@users.noreply.github.com>
2023-06-11 16:05:48 +0100

    feat: add server count (#28)
    * feat: endpoint
    
    * fix: endpoint
    
    * feat: used by x servers
    
    * refactor: less cache time

---
+++ src/routes/+page.ts
@@ -0,0 +1,4 @@
+export const load = async ({ fetch }) => {
+  const data = fetch("/api/server-count").then(async (r) => r.json());
+  return { base: 69, streamed: { topgg: data as Promise<{ server_count: number }> } };
+};
