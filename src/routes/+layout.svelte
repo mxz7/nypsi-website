bc17ffec57aa3df8412bbbf38fb8bb01de7bdcd6
max <29782397+tekoh@users.noreply.github.com>
2023-06-11 15:15:07 +0100

    feat: add google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script>
   import { dev } from "$app/environment";
+  import GAnalytics from "$lib/components/GAnalytics.svelte";
   import Navigation from "$lib/components/Navigation.svelte";
   import { inject } from "@vercel/analytics";
   import "../app.css";
 
   inject({ mode: dev ? "development" : "production" });
 </script>
+
+<GAnalytics />
 
 <Navigation />
 
