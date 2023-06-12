2fcae8aaab7125f968179986bd4740d8a9ffa308
max <29782397+tekoh@users.noreply.github.com>
2023-06-12 20:19:26 +0100

    fix: top commands

--- src/lib/functions/getCommandsData.ts
+++ src/lib/functions/getCommandsData.ts
@@ -1,23 +1,9 @@
-import { inPlaceSort } from "fast-sort";
-
 export async function getCommandsData(
   fetch: (input: RequestInfo | URL, init?: RequestInit | undefined) => Promise<Response>
 ) {
   const res = await fetch("/api/commands").then((res) => res.json());
 
   if (!res.total || !res.users) return null;
-
-  inPlaceSort(res.users as { user: string; amount: number }[]).desc((i) => i.amount);
-
-  let count = 0;
-  res.users = res.users.slice(0, 10).map((i: { user: string; amount: number }) => {
-    count++;
-    return {
-      username: i.user.length > 12 ? `${i.user.slice(0, 10).trim()}..` : i.user,
-      value: i.amount.toLocaleString(),
-      position: count
-    };
-  });
 
   return res as { total: number; users: { username: string; value: string; position: number }[] };
 }
