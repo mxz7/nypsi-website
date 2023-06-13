2c296ac11a841142c9e2f618a5f5cff99f2dab66
max <29782397+tekoh@users.noreply.github.com>
2023-06-13 16:35:21 +0100

    feat: it works!!

---
+++ src/routes/login/+server.ts
@@ -0,0 +1,37 @@
+import { DISCORD_OAUTH_CLIENTID, DISCORD_OAUTH_SECRET } from "$env/static/private";
+import { PUBLIC_OAUTH_URL } from "$env/static/public";
+import { error, redirect } from "@sveltejs/kit";
+
+export const GET = async ({ url, fetch, cookies }) => {
+  const code = url.searchParams.get("code");
+
+  if (code) {
+    const res = await fetch("https://discord.com/api/oauth2/token", {
+      method: "post",
+      body: new URLSearchParams({
+        client_id: DISCORD_OAUTH_CLIENTID,
+        client_secret: DISCORD_OAUTH_SECRET,
+        grant_type: "authorization_code",
+        redirect_uri: "http://localhost:5173/login",
+        code,
+        scope: "identify",
+      }),
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    }).then((r) => r.json());
+
+    if (res.error) {
+      console.error(res);
+      throw error(400, { message: "something went wrong", ...res });
+    }
+
+    const accessTokenExpire = new Date(Date.now() + res.expires_in); // 10 minutes
+    const refreshTokenExpire = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000); // 30 days
+
+    cookies.set("discord_access_token", res.access_token, { expires: accessTokenExpire });
+    cookies.set("discord_refresh_token", res.refresh_token, { expires: refreshTokenExpire });
+
+    throw redirect(302, "/");
+  }
+
+  throw redirect(302, PUBLIC_OAUTH_URL);
+};
