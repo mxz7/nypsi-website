831551a8b70b44fe8dc97661e6a72b0a3769b231
max <29782397+tekoh@users.noreply.github.com>
2023-06-15 12:55:35 +0100

    feat: badges data

---
+++ src/lib/data/bages.ts
@@ -0,0 +1,33 @@
+const badges = new Map([
+  ["owner", { icon: "/badges/crown.png", text: "i own nypsi lol" }],
+  [
+    "staff",
+    {
+      icon: "https://em-content.zobj.net/thumbs/240/twitter/351/hammer-and-pick_2692-fe0f.png",
+      text: "nypsi staff",
+    },
+  ],
+  [
+    "contributor",
+    {
+      icon: "https://cdn.discordapp.com/role-icons/959541364540993606/0f59884798437a54c4384f7dc941fffe.png",
+      text: "contributor",
+    },
+  ],
+  [
+    "season_winner",
+    {
+      icon: "https://em-content.zobj.net/thumbs/240/twitter/351/trophy_1f3c6.png",
+      text: "season winner",
+    },
+  ],
+  [
+    "og",
+    {
+      icon: "https://em-content.zobj.net/thumbs/240/twitter/351/fire_1f525.png",
+      text: "og player",
+    },
+  ],
+]);
+
+export default badges;
