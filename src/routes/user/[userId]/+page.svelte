b1d11b1bd7d073cfee8cd5f555e85d1669fc63e1
max <29782397+tekoh@users.noreply.github.com>
2023-06-15 13:59:28 +0100

    refactor: go above wordle

--- src/routes/user/[userId]/+page.svelte
+++ src/routes/user/[userId]/+page.svelte
@@ -330,6 +330,39 @@
         {/await}
       {/if}
 
+      <div class="mt-4 flex w-full flex-row">
+        <div
+          class="mr-2 flex grow flex-col rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 text-center duration-300 hover:border-opacity-20 hover:bg-opacity-40"
+        >
+          <h1 class="text-white lg:text-xl">last seen</h1>
+          <p class="text-sm text-gray-300 lg:text-base">
+            {#if dayjs(userData.lastCommand).isBefore(dayjs().subtract(1, "year"))}
+              {new Date(userData.lastCommand).toLocaleDateString()}
+            {:else if daysAgo(userData.lastCommand) <= 1}
+              today
+            {:else}
+              {daysAgo(userData.lastCommand).toLocaleString()} days ago
+            {/if}
+          </p>
+        </div>
+
+        <div
+          class="ml-2 flex grow flex-col rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 text-center duration-300 hover:border-opacity-20 hover:bg-opacity-40"
+        >
+          <h1 class="text-white lg:text-xl">completion</h1>
+          <p class="text-sm text-gray-300 lg:text-base">
+            {#await fetch("https://raw.githubusercontent.com/tekoh/nypsi/main/data/achievements.json").then( (r) => r.json() )}
+              calculating...
+            {:then achievementData}
+              {(
+                (userData.Achievements.length / Object.keys(achievementData).length) *
+                100
+              ).toPrecision(3)}%
+            {/await}
+          </p>
+        </div>
+      </div>
+
       {#if userData.WordleStats}
         <div
           class="mx-auto mt-4 flex flex-col rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 duration-300 hover:border-opacity-20 hover:bg-opacity-40"
@@ -511,39 +544,6 @@
           </div>
         </div>
       {/if}
-
-      <div class="mt-4 flex w-full flex-row">
-        <div
-          class="mr-2 flex grow flex-col rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 text-center duration-300 hover:border-opacity-20 hover:bg-opacity-40"
-        >
-          <h1 class="text-white lg:text-xl">last seen</h1>
-          <p class="text-sm text-gray-300 lg:text-base">
-            {#if dayjs(userData.lastCommand).isBefore(dayjs().subtract(1, "year"))}
-              {new Date(userData.lastCommand).toLocaleDateString()}
-            {:else if daysAgo(userData.lastCommand) <= 1}
-              today
-            {:else}
-              {daysAgo(userData.lastCommand).toLocaleString()} days ago
-            {/if}
-          </p>
-        </div>
-
-        <div
-          class="ml-2 flex grow flex-col rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 text-center duration-300 hover:border-opacity-20 hover:bg-opacity-40"
-        >
-          <h1 class="text-white lg:text-xl">completion</h1>
-          <p class="text-sm text-gray-300 lg:text-base">
-            {#await fetch("https://raw.githubusercontent.com/tekoh/nypsi/main/data/achievements.json").then( (r) => r.json() )}
-              calculating...
-            {:then achievementData}
-              {(
-                (userData.Achievements.length / Object.keys(achievementData).length) *
-                100
-              ).toPrecision(3)}%
-            {/await}
-          </p>
-        </div>
-      </div>
 
       {#if userData.Economy.Game.length > 0}
         <div
