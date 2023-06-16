dd1269f4327123e4b8391a1e673dd804b55dc0e6
max <29782397+tekoh@users.noreply.github.com>
2023-06-16 18:19:01 +0100

    fix: move to layout?

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,18 +1,10 @@
 // See https://kit.svelte.dev/docs/types#app
 
-import type { UserSession } from "$lib/types/User";
-import type { Cookies } from "@sveltejs/kit";
-
 // for information about these interfaces
 declare global {
   namespace App {
     // interface Error {}
-    interface Locals {
-      getUser(
-        cookies: Cookies,
-        fetch: (input: RequestInfo | URL, init?: RequestInit | undefined) => Promise<Response>
-      ): Promise<UserSession>;
-    }
+    // interface Locals {}
     // interface PageData {}
     // interface Platform {}
   }
