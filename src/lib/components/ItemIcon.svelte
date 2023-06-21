a9d4810297b80a10d1ac36f63c5f224136713944
max <29782397+tekoh@users.noreply.github.com>
2023-06-21 16:42:24 +0100

    feat: tooltip on item icons

--- src/lib/components/ItemIcon.svelte
+++ src/lib/components/ItemIcon.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
+  import Tooltip from "sv-tooltip";
+
   export let item: { name: string; emoji: string; id: string; aliases: string[] };
 </script>
 
-<a
-  href="/leaderboard/{item.id}"
-  class="m-1 flex h-16 w-16 items-center justify-center rounded-md border border-transparent bg-gray-950 bg-opacity-20 p-3 duration-200 ease-in hover:scale-105 hover:border-gray-500 sm:m-1.5 sm:h-20 sm:w-20"
->
-  <!-- <p>{item.id}</p> -->
-  <img
-    class="h-auto max-h-full w-auto max-w-full object-contain"
-    src={item.emoji}
-    alt=""
-    loading="lazy"
-  />
-</a>
+<Tooltip tip={item.name} top color="#0E1422">
+  <a
+    href="/leaderboard/{item.id}"
+    class="m-1 flex h-16 w-16 items-center justify-center rounded-md border border-transparent bg-gray-950 bg-opacity-20 p-3 duration-200 ease-in hover:scale-105 hover:border-gray-500 sm:m-1.5 sm:h-20 sm:w-20"
+  >
+    <!-- <p>{item.id}</p> -->
+    <img
+      class="h-auto max-h-full w-auto max-w-full object-contain"
+      src={item.emoji}
+      alt=""
+      loading="lazy"
+    />
+  </a>
+</Tooltip>
