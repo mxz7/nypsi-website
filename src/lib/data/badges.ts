e6846932d2d9f759548cc3fa8e1230b430df7245
max <29782397+tekoh@users.noreply.github.com>
2023-06-21 16:49:38 +0100

    feat: badges descriptions

--- src/lib/data/badges.ts
+++ src/lib/data/badges.ts
@@ -1,10 +1,20 @@
 const badges = new Map([
-  ["owner", { icon: "/badges/crown.png", text: "i own nypsi lol" }],
+  [
+    "owner",
+    {
+      icon: "/badges/crown.png",
+      text: "i own nypsi lol",
+      name: "owner",
+      description: "owner(s) of nypsi",
+    },
+  ],
   [
     "staff",
     {
       icon: "https://em-content.zobj.net/thumbs/240/twitter/351/hammer-and-pick_2692-fe0f.png",
       text: "nypsi staff",
+      name: "nypsi staff",
+      description: "staff of the official nypsi server or the discord bot. these people help to moderate and answer any questions that users might have",
     },
   ],
   [
@@ -12,6 +22,8 @@ const badges = new Map([
     {
       icon: "https://cdn.discordapp.com/role-icons/959541364540993606/0f59884798437a54c4384f7dc941fffe.png",
       text: "contributor",
+      name: "contributor",
+      description: "these people are active contributors (changing/writing/fixing code) to either this website or the nypsi bot itself."
     },
   ],
   [
@@ -19,6 +31,8 @@ const badges = new Map([
     {
       icon: "https://em-content.zobj.net/thumbs/240/twitter/351/trophy_1f3c6.png",
       text: "season winner",
+      name: "season winner",
+      description: "these people have played nypsi enough to have won an economy season. nypsi's economy is seasonal. meaning that everything resets (except some things) every few months"
     },
   ],
   [
@@ -26,6 +40,8 @@ const badges = new Map([
     {
       icon: "https://em-content.zobj.net/thumbs/240/twitter/351/fire_1f525.png",
       text: "og player",
+      name: "og player",
+      description: "these people have been using nypsi since it very first started back in 2020. as a sign of appreciation they receive this badge (:<br>if you have been using nypsi since 2020, please contact me on discord (@tekoh.)"
     },
   ],
   [
@@ -33,6 +49,8 @@ const badges = new Map([
     {
       icon: "https://em-content.zobj.net/thumbs/240/twitter/351/money-with-wings_1f4b8.png",
       text: "i've donated lots of money xd",
+      name: "highroller",
+      description: "highrollers have donated at least $250 to nypsi"
     },
   ],
 ]);
