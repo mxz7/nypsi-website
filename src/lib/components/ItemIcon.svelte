ee088b25227a6c7031f3b42693cec6cad870189c
max <29782397+tekoh@users.noreply.github.com>
2023-06-23 21:52:11 +0100

    fix: leaderboards page right side thing

--- src/lib/components/ItemIcon.svelte
+++ src/lib/components/ItemIcon.svelte
@@ -4,11 +4,11 @@
   export let item: { name: string; emoji: string; id: string; aliases: string[] };
 </script>
 
-<Tooltip tip={item.name} top color="#0E1422">
-  <a
-    href="/leaderboard/{item.id}"
-    class="m-1 flex h-16 w-16 items-center justify-center rounded-md border border-transparent bg-gray-950 bg-opacity-20 p-3 duration-200 ease-in hover:scale-105 hover:border-gray-500 sm:m-1.5 sm:h-20 sm:w-20"
-  >
+<a
+  href="/leaderboard/{item.id}"
+  class="flex h-16 w-16 items-center justify-center rounded-md border border-transparent bg-gray-950 bg-opacity-20 p-3 duration-200 ease-in hover:scale-105 hover:border-gray-500 sm:m-1.5 sm:h-20 sm:w-20"
+>
+  <Tooltip tip={item.name} top color="#0E1422">
     <!-- <p>{item.id}</p> -->
     <img
       class="h-auto max-h-full w-auto max-w-full object-contain"
@@ -16,5 +16,5 @@
       alt=""
       loading="lazy"
     />
-  </a>
-</Tooltip>
+  </Tooltip>
+</a>
