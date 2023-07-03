3690f698a8e33f8b805fb84c7613f36b7452af25
max <29782397+tekoh@users.noreply.github.com>
2023-07-03 14:16:14 +0100

    feat: toast for rate limit

--- src/routes/user/+layout.svelte
+++ src/routes/user/+layout.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
   import { navigating, page } from "$app/stores";
   import { userSearchTerm } from "$lib/data/stores.js";
+  import toast from "svelte-french-toast";
   import { fade } from "svelte/transition";
 
   export let data;
+
+  $: if (data.error === 429) {
+    toast.error("you are being rate limited", {
+      position: "bottom-center",
+      style: "color: #fff; background: #b91c1c;",
+    });
+  }
 
   $userSearchTerm = $page.url.searchParams.get("search") || $page.params.id || "";
 </script>
@@ -35,9 +43,7 @@
   </form>
 </div>
 
-{#if data.error === 429}
-  <h2 transition:fade class="-mt-8 text-center text-red-500">you are being rate limited</h2>
-{:else if data.error === 451}
+{#if data.error === 451}
   <h2 transition:fade class="mt-8 text-center text-2xl font-semibold text-white">
     private profile
   </h2>
