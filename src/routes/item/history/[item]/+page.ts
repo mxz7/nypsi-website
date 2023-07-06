96d1098a12f77c4aa0008ddf57bab899a11df9b5
max <29782397+tekoh@users.noreply.github.com>
2023-07-06 18:10:25 +0100

    feat(history): show user's items too

--- src/routes/item/history/[item]/+page.ts
+++ src/routes/item/history/[item]/+page.ts
@@ -5,15 +5,15 @@ import type { ChartConfiguration } from "chart.js";
 export const load = async ({ params, fetch, parent }) => {
   const parentData = await parent();
 
-  if (!parentData.premium) return;
+  if (!parentData.premium || !parentData.user.authenticated) return;
 
   const item = await getItems().then((items) => items.find((i) => i.id === params.item));
 
   if (!item) throw error(400, "invalid item");
 
-  const data: Promise<ChartConfiguration> = fetch("/api/itemhistory/" + params.item).then((r) =>
-    r.json()
-  );
+  const data: Promise<ChartConfiguration> = fetch(
+    `/api/itemhistory/${params.item}?user=${parentData.user.id}`
+  ).then((r) => r.json());
 
   return { base: 69, item, streamed: { graphData: data } };
 };
