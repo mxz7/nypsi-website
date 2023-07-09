00ee3fd57c47313663cc3aa4911c2743a8d003d0
max <29782397+tekoh@users.noreply.github.com>
2023-07-09 21:55:04 +0100

    AAAHHHHHH

--- src/routes/user/[userId]/+page.ts
+++ src/routes/user/[userId]/+page.ts
@@ -1,9 +1,8 @@
+import { browser } from "$app/environment";
 import getItems from "$lib/functions/getItems.js";
 import type { UserApiResponse } from "$lib/types/User.js";
 import { redirect } from "@sveltejs/kit";
 import dayjs from "dayjs";
-
-export const ssr = false;
 
 export const load = async ({ parent, params, fetch, url }) => {
   let userId = params.userId;
@@ -27,21 +26,25 @@ export const load = async ({ parent, params, fetch, url }) => {
         const res = await fetch(`/api/user/getid/${userId}`).then((r) => r.json());
         if (res.error !== 429) {
           res.expire = dayjs().add(3, "hours").toDate().getTime();
-          localStorage.setItem(`id-user-map-${userId}`, JSON.stringify(res));
+          if (browser) localStorage.setItem(`id-user-map-${userId}`, JSON.stringify(res));
         }
         return res;
       };
 
       let usernameRes;
 
-      if (localStorage.getItem(`id-user-map-${userId}`)) {
-        usernameRes = JSON.parse(localStorage.getItem(`id-user-map-${userId}`) as string);
-        if (usernameRes.expire && usernameRes.expire < Date.now()) {
-          localStorage.removeItem(`id-user-map-${userId}`);
+      if (!browser) {
+        usernameRes = await fetchFromApi();
+      } else {
+        if (localStorage.getItem(`id-user-map-${userId}`)) {
+          usernameRes = JSON.parse(localStorage.getItem(`id-user-map-${userId}`) as string);
+          if (usernameRes.expire && usernameRes.expire < Date.now()) {
+            localStorage.removeItem(`id-user-map-${userId}`);
+            usernameRes = await fetchFromApi();
+          }
+        } else {
           usernameRes = await fetchFromApi();
         }
-      } else {
-        usernameRes = await fetchFromApi();
       }
 
       return usernameRes;
@@ -51,11 +54,21 @@ export const load = async ({ parent, params, fetch, url }) => {
     });
   }
 
-  return {
-    base: 69,
-    streamed: {
-      userData: data,
-      items: getItems(),
-    },
-  };
+  if (navigator?.userAgent.includes("Mozilla")) {
+    return {
+      base: 69,
+      streamed: {
+        userData: data,
+        items: getItems(),
+      },
+    };
+  } else {
+    return {
+      base: 69,
+      streamed: {
+        userData: await Promise.resolve(data),
+        items: await getItems(),
+      },
+    };
+  }
 };
