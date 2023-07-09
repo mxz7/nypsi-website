a73a859a8aeaa657d541d0bdf5bd1dd54e3532a3
max <29782397+tekoh@users.noreply.github.com>
2023-07-09 21:55:04 +0100

    AAAHHHHHH

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,8 @@
+export const config = {
+  runtime: "edge",
+  regions: "all",
+};
+
 export const load = async ({ fetch }) => {
   const data = fetch("/api/server-count").then(async (r) => r.json());
   return { base: 69, streamed: { topgg: data as Promise<{ server_count: number }> } };
