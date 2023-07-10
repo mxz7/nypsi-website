0cd539f3f45cca5560c748b9559d35f2c1ce05f1
max <29782397+tekoh@users.noreply.github.com>
2023-07-10 13:22:49 +0100

    refactor: remove item history from api

--- src/routes/item/history/[item]/+page.server.ts
+++ src/routes/item/history/[item]/+page.server.ts
@@ -1,19 +1,17 @@
 import getItems from "$lib/functions/getItems.js";
+import getChartData from "$lib/server/functions/getChartData.js";
 import { error } from "@sveltejs/kit";
-import type { ChartConfiguration } from "chart.js";
 
-export const load = async ({ params, fetch, parent }) => {
+export const load = async ({ params, parent, setHeaders }) => {
   const parentData = await parent();
 
   if (!parentData.premium || !parentData.user.authenticated) return;
+
+  setHeaders({ "cache-control": "max-age=600" });
 
   const item = await getItems().then((items) => items.find((i) => i.id === params.item));
 
   if (!item) throw error(400, "invalid item");
 
-  const data: Promise<ChartConfiguration> = fetch(
-    `/api/itemhistory/${params.item}?user=${parentData.user.id}`
-  ).then((r) => r.json());
-
-  return { base: 69, item, streamed: { graphData: data } };
+  return { base: 69, item, streamed: { graphData: getChartData(params.item, parentData.user.id) } };
 };
