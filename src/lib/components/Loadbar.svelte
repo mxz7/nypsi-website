972a15ed3cbb1894b0ce18f6a3330dc2f873b0cb
max <29782397+tekoh@users.noreply.github.com>
2023-07-10 18:40:22 +0100

    refactor: move lading bar to component

---
+++ src/lib/components/Loadbar.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { navigating } from "$app/stores";
+  import { fade } from "svelte/transition";
+
+  let loadingSize = 0;
+  let interval: number | undefined;
+
+  $: if (
+    $navigating &&
+    !interval //&&
+    // $navigating.to?.params?.userId &&
+    // $navigating.from?.route.id?.startsWith("/user")
+  ) {
+    loadingSize = 0;
+    interval = setInterval(() => {
+      loadingSize += Math.floor(Math.random() * 10) + 5;
+      if (loadingSize > 85) loadingSize = 85;
+    }, 100);
+  } else if (interval && !$navigating) {
+    loadingSize = 100;
+    setTimeout(() => {
+      clearInterval(interval);
+      interval = undefined;
+    }, 100);
+  }
+</script>
+
+{#if interval}
+  <div
+    in:fade={{ delay: 150, duration: 50 }}
+    out:fade={{ duration: 750 }}
+    style="width: {loadingSize}%;"
+    class="absolute top-[64px] h-[2px] w-0 rounded bg-red-500 duration-100"
+  />
+{/if}
