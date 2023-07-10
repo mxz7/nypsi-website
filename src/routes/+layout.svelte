f6ee66f978d9d61000a31937611194f128e01c83
max <29782397+tekoh@users.noreply.github.com>
2023-07-10 18:40:22 +0100

    refactor: move lading bar to component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,38 +1,16 @@
 <script lang="ts">
   import { dev } from "$app/environment";
   import { goto } from "$app/navigation";
-  import { navigating, page } from "$app/stores";
+  import { page } from "$app/stores";
   import GAnalytics from "$lib/components/GAnalytics.svelte";
+  import Loadbar from "$lib/components/Loadbar.svelte";
   import Navigation from "$lib/components/Navigation.svelte";
   import { inject } from "@vercel/analytics";
   import { onMount } from "svelte";
   import toast, { Toaster } from "svelte-french-toast";
-  import { fade } from "svelte/transition";
   import "../app.css";
 
   export let data;
-
-  let loadingSize = 0;
-  let interval: number | undefined;
-
-  $: if (
-    $navigating &&
-    !interval &&
-    $navigating.to?.params?.userId &&
-    $navigating.from?.route.id?.startsWith("/user")
-  ) {
-    loadingSize = 0;
-    interval = setInterval(() => {
-      loadingSize += Math.floor(Math.random() * 10) + 5;
-      if (loadingSize > 85) loadingSize = 85;
-    }, 100);
-  } else if (interval && !$navigating) {
-    loadingSize = 100;
-    setTimeout(() => {
-      clearInterval(interval);
-      interval = undefined;
-    }, 100);
-  }
 
   inject({ mode: dev ? "development" : "production" });
 
@@ -64,13 +42,7 @@
 
 <Navigation user={data.user} />
 
-{#if interval}
-  <div
-    out:fade={{ duration: 500 }}
-    style="width: {loadingSize}%;"
-    class="absolute top-[64px] h-[2px] w-0 rounded bg-red-500 duration-100"
-  />
-{/if}
+<Loadbar />
 
 <slot />
 
