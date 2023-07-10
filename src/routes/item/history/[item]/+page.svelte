d5d64f7c46a6c5a2cc07eb7550d9a18380fb6f5c
max <29782397+tekoh@users.noreply.github.com>
2023-07-10 17:16:04 +0100

    refactor: loading icon is a component

--- src/routes/item/history/[item]/+page.svelte
+++ src/routes/item/history/[item]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Loading from "$lib/components/Loading.svelte";
   import { Chart as ChartJs, registerables } from "chart.js";
 
   import { onMount } from "svelte";
@@ -11,79 +12,79 @@
   let chartCanvas: HTMLCanvasElement;
 
   onMount(async () => {
-    setTimeout(async () => {
-      const chartData = await Promise.resolve(data.streamed.graphData);
+    const chartData = await Promise.resolve(data.streamed.graphData);
 
-      const chart = new ChartJs(chartCanvas, {
-        ...chartData,
-        options: {
-          plugins: {
-            tooltip: {
-              callbacks: {
-                label(tooltipItem) {
-                  if (tooltipItem.dataset.label.includes("items in world"))
-                    return `in world: ${tooltipItem.formattedValue}`;
-                  else if (
-                    tooltipItem.dataset.label ===
-                    (data.user.authenticated ? data.user.username : "null")
-                  )
-                    return `${data.user.authenticated ? data.user.username : "null"}: ${
-                      tooltipItem.formattedValue
-                    }`;
+    if (typeof chartData === "string") return;
 
-                  return `${tooltipItem.dataset.label} average: $${tooltipItem.formattedValue}`;
-                },
-              },
-            },
-          },
-          maintainAspectRatio: false,
-          elements: {
-            line: {
-              tension: 0.4,
-            },
-            point: {
-              radius: /Android|iPhone/i.test(navigator.userAgent) ? 1 : 5,
-            },
-          },
-          scales: {
-            y2: {
-              position: "right",
-              ticks: {
-                callback(tickValue) {
-                  return Math.floor(Number(tickValue)).toLocaleString();
-                },
-              },
-            },
-            y1: {
-              position: "left",
-              ticks: {
-                callback(tickValue) {
-                  return `$${Math.floor(Number(tickValue)).toLocaleString()}`;
-                },
+    new ChartJs(chartCanvas, {
+      ...chartData,
+      options: {
+        plugins: {
+          tooltip: {
+            callbacks: {
+              label(tooltipItem) {
+                if (tooltipItem.dataset.label.includes("items in world"))
+                  return `in world: ${tooltipItem.formattedValue}`;
+                else if (
+                  tooltipItem.dataset.label ===
+                  (data.user.authenticated ? data.user.username : "null")
+                )
+                  return `${data.user.authenticated ? data.user.username : "null"}: ${
+                    tooltipItem.formattedValue
+                  }`;
+
+                return `${tooltipItem.dataset.label} average: $${tooltipItem.formattedValue}`;
               },
             },
           },
         },
-      });
-    }, 100);
+        maintainAspectRatio: false,
+        elements: {
+          line: {
+            tension: 0.4,
+          },
+          point: {
+            radius: /Android|iPhone/i.test(navigator.userAgent) ? 1 : 5,
+          },
+        },
+        scales: {
+          y2: {
+            position: "right",
+            ticks: {
+              callback(tickValue) {
+                return Math.floor(Number(tickValue)).toLocaleString();
+              },
+            },
+          },
+          y1: {
+            position: "left",
+            ticks: {
+              callback(tickValue) {
+                return `$${Math.floor(Number(tickValue)).toLocaleString()}`;
+              },
+            },
+          },
+        },
+      },
+    });
   });
 </script>
 
 <svelte:head>
-  <title>{data.item.name} history</title>
+  <title>{data.item?.name} history</title>
   <meta
     name="description"
-    content="showing auction/offers average and total item count for {data.item.name}"
+    content="showing auction/offers average and total item count for {data.item?.name}"
   />
 
-  <meta name="og:title" content="{data.item.name} history" />
+  <meta name="og:title" content="{data.item?.name} history" />
   <meta
     name="og:description"
-    content="showing auction/offers average and total item count for {data.item.name}"
+    content="showing auction/offers average and total item count for {data.item?.name}"
   />
   <meta name="og:site_name" content="nypsi" />
   {#if data.item}
-    <meta name="og:image" content={data.item.emoji} />
+    <meta name="og:image" content={data.item?.emoji} />
     <meta property="og:image:width" content="128" />
     <meta property="og:image:height" content="128" />
   {/if}
@@ -91,40 +92,35 @@
 
 <header class="mb-10 mt-5 text-center sm:mb-3 sm:w-full">
   <h1 class="text-4xl font-bold text-white sm:text-5xl">
-    {data.item.name} history
+    {data.item?.name} history
   </h1>
   <div class="m-auto mt-3 h-1 w-3/4 rounded-full bg-red-500 sm:w-1/2" />
 </header>
 
 {#await data.streamed.graphData}
-  <div class="absolute left-1/2 top-1/4 mb-[100vh] -translate-x-1/2 -translate-y-1/2 transform">
-    <div out:fade={{ duration: 300 }} class="flex items-center justify-center">
-      <svg
-        class="-ml-1 mr-3 h-10 w-10 animate-spin text-red-500"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        />
-      </svg>
-    </div>
-  </div>
-{:then}
-  <div class="flex justify-center">
-    <div class=" w-[1100px] overflow-x-scroll p-4">
-      <div class="h-full w-fit px-4 sm:w-full">
-        <canvas
-          in:fade={{ delay: 300, duration: 250 }}
-          style="width: {/Android|iPhone/i.test(navigator.userAgent) ? '150vw' : '100%'};"
-          bind:this={chartCanvas}
-        />
+  <Loading fadeInSettings={{ delay: 50, duration: 50 }} fadeOutSettings={{ duration: 150 }} />
+{:then graphData}
+  <div in:fade={{ delay: 150, duration: 300 }}>
+    {#if graphData === "invalid item"}
+      <div class="mb-48 flex justify-center text-2xl font-semibold text-red-400">
+        <h1>invalid item</h1>
       </div>
-    </div>
+    {:else if graphData === "not enough data"}
+      <div class="mb-48 flex justify-center text-2xl font-semibold text-red-400">
+        <h1>not enough data</h1>
+      </div>
+    {:else}
+      <div class="flex justify-center">
+        <div class=" w-[1100px] overflow-x-scroll p-4">
+          <div class="h-full w-fit px-4 sm:w-full">
+            <canvas
+              style="width: {/Android|iPhone/i.test(navigator.userAgent) ? '150vw' : '100%'};"
+              bind:this={chartCanvas}
+            />
+          </div>
+        </div>
+      </div>
+    {/if}
   </div>
 
   <!-- <ItemList url="/item/history" /> -->
