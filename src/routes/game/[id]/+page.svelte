685558471050c8de3e56112c81d75d5cfdc3e627
max <29782397+tekoh@users.noreply.github.com>
2023-07-12 17:54:05 +0100

    better coloouuurrrr

--- src/routes/game/[id]/+page.svelte
+++ src/routes/game/[id]/+page.svelte
@@ -43,20 +43,19 @@
             <p class="text-center text-white">game: {game.game}<br />outcome: {game.outcome}</p>
           {/if}
         </div>
-        <div class="text-center text-xl">
+        <div class="text-center text-xl font-bold">
           <!-- <p class="text-xs text-white">{game.outcome}</p> -->
           {#if game.win == 1 && !game.game.includes("scratch")}
-            <p class="font-bold text-green-400">won</p>
+            <p class=" text-green-400">won</p>
 
             <p class="text-lg text-green-400 opacity-75">
               ${(game.earned - game.bet).toLocaleString()} profit
             </p>
-            {:else if game.win == 0 && !game.game.includes("scratch")}
-              <p class=" text-red-500">lost</p>
-            {:else if game.win == 2 && !game.game.includes("scratch")}
-              <!--change yellow to less piss-->
-              <p class=" text-yellow-200">draw</p>
-            {/if}
+          {:else if game.win == 0 && !game.game.includes("scratch")}
+            <p class=" text-red-500">lost</p>
+          {:else if game.win == 2 && !game.game.includes("scratch")}
+            <p class=" text-yellow-500">draw</p>
+          {/if}
         </div>
 
         <div class="mt-5 px-2 text-center text-gray-300">
