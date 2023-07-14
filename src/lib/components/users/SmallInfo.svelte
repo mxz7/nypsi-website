eab2c8085707834c80dbf396425f4e88b3aa2c0b
max <29782397+tekoh@users.noreply.github.com>
2023-07-14 20:57:18 +0100

    refactor: split up user page into components

---
+++ src/lib/components/users/SmallInfo.svelte
@@ -0,0 +1,5 @@
+<div
+  class="flex grow flex-col rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 text-center duration-300 hover:border-opacity-20 hover:bg-opacity-40"
+>
+  <slot />
+</div>
