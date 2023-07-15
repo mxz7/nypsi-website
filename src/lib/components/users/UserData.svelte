dd2fb6cf59a880d58f0244bbade0efef01a4c43e
max <29782397+tekoh@users.noreply.github.com>
2023-07-15 12:33:44 +0100

    refactor: purple borders

--- src/lib/components/users/UserData.svelte
+++ src/lib/components/users/UserData.svelte
@@ -63,7 +63,7 @@
 
 <div class="xl:[20vw] md:w-[40vw mx-3 mb-10 mt-7 flex flex-col sm:mx-auto sm:w-[50vw]">
   <div
-    class="flex w-full flex-col rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 duration-300 hover:border-opacity-20 hover:bg-opacity-40"
+    class="flex w-full flex-col rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 duration-300 hover:border-accent hover:border-opacity-20 hover:bg-opacity-40"
     id="user"
     in:fly={{ delay: 300, duration: 500, y: 75 }}
   >
@@ -210,7 +210,7 @@
   <div in:fly={{ delay: 600, duration: 500, y: 75 }}>
     {#if userData.Economy.EconomyGuildMember?.guild}
       <div
-        class="mt-4 flex w-full flex-col rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 duration-300 hover:border-opacity-20 hover:bg-opacity-40"
+        class="mt-4 flex w-full flex-col rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 duration-300 hover:border-accent hover:border-opacity-20 hover:bg-opacity-40"
       >
         <h1 class="w-full text-center text-lg text-gray-100 lg:text-2xl">
           {userData.Economy.EconomyGuildMember.guild.guildName} [{userData.Economy
@@ -245,9 +245,9 @@
   </div>
 
   <div in:fly={{ delay: 700, duration: 500, y: 75 }}>
-    {#if userData.Economy.Inventory.length > 0}
+    {#if userData.Economy.Inventory.length > 1}
       <div
-        class="mt-4 flex w-full flex-col justify-center rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 duration-300 hover:border-opacity-20 hover:bg-opacity-40"
+        class="mt-4 flex w-full flex-col justify-center rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 duration-300 hover:border-accent hover:border-opacity-20 hover:bg-opacity-40"
         id="inventory"
       >
         <h1 class="mb-3 w-full text-center text-white lg:text-xl">inventory</h1>
@@ -256,7 +256,7 @@
             {#if items.find((i) => i.id === item.item)}
               <a
                 href="/leaderboard/{item.item}"
-                class="hover:bg-opacity- mx-2 flex flex-col items-center justify-center rounded border border-gray-500 border-opacity-10 bg-gray-700 bg-opacity-5 py-2 align-middle text-xs text-gray-300 shadow duration-300 hover:border-opacity-25 lg:text-sm"
+                class="hover:bg-opacity- mx-2 flex flex-col items-center justify-center rounded border border-gray-500 border-opacity-10 bg-gray-700 bg-opacity-5 py-2 align-middle text-xs text-gray-300 shadow duration-300 hover:border-accent hover:border-opacity-25 lg:text-sm"
               >
                 <div class="flex h-6 w-6 items-center justify-center align-middle lg:h-8 lg:w-8">
                   <img
@@ -308,7 +308,7 @@
   <div in:fly={{ delay: 900, duration: 500, y: 75 }}>
     {#if userData.Leaderboards.length > 0}
       <div
-        class="mt-4 flex w-full flex-col justify-center rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 duration-300 hover:border-opacity-20 hover:bg-opacity-40"
+        class="mt-4 flex w-full flex-col justify-center rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 duration-300 hover:border-accent hover:border-opacity-20 hover:bg-opacity-40"
         id="leaderboards"
       >
         <h1 class="mb-3 w-full text-center text-white lg:text-xl">leaderboards</h1>
@@ -316,7 +316,7 @@
           {#each inPlaceSort(userData.Leaderboards).asc((i) => i.position) as lb}
             <a
               href="/leaderboard/{lb.leaderboard.replace('item-', '')}"
-              class="hover:bg-opacity- mx-2 flex flex-col items-center justify-center rounded border border-gray-500 border-opacity-10 bg-gray-700 bg-opacity-5 py-2 align-middle text-xs text-gray-300 shadow duration-300 hover:border-opacity-25 lg:text-sm"
+              class="hover:bg-opacity- mx-2 flex flex-col items-center justify-center rounded border border-gray-500 border-opacity-10 bg-gray-700 bg-opacity-5 py-2 align-middle text-xs text-gray-300 shadow duration-300 hover:border-accent hover:border-opacity-25 lg:text-sm"
             >
               {#if lb.leaderboard.startsWith("item-")}
                 <div class="flex h-6 w-6 items-center justify-center align-middle lg:h-8 lg:w-8">
@@ -349,7 +349,7 @@
   <div in:fly={{ delay: 1000, duration: 500, y: 75 }}>
     {#if userData.Economy.Game.length > 0}
       <div
-        class="mx-auto mt-4 flex flex-col rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 duration-300 hover:border-opacity-20 hover:bg-opacity-40 lg:w-full"
+        class="mx-auto mt-4 flex flex-col rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 duration-300 hover:border-accent hover:border-opacity-20 hover:bg-opacity-40 lg:w-full"
       >
         <h1 class="text-center text-white lg:text-xl">recent games</h1>
         <div
@@ -364,7 +364,7 @@
                 : game.win == 0
                 ? 'rgb(239, 68, 68)'
                 : 'rgb(254, 240, 138)'};"
-              class="mt-3 flex w-full flex-col items-center justify-center rounded border border-gray-500 border-opacity-10 bg-gray-700 bg-opacity-5 p-2 px-4 align-middle shadow duration-300 hover:border-opacity-25 lg:mt-0"
+              class="mt-3 flex w-full flex-col items-center justify-center rounded border border-gray-500 border-opacity-10 bg-gray-700 bg-opacity-5 p-2 px-4 align-middle shadow duration-300 hover:border-accent hover:border-opacity-25 lg:mt-0"
             >
               <h2 class="text-center lg:text-lg">{game.game.replaceAll("_", " ")}</h2>
 
@@ -391,7 +391,7 @@
   <div in:fly={{ delay: 1100, duration: 500, y: 75 }} class="w-full">
     {#if userData.WordleStats}
       <div
-        class="mx-auto mt-4 flex w-fit flex-col rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 duration-300 hover:border-opacity-20 hover:bg-opacity-40"
+        class="mx-auto mt-4 flex w-fit flex-col rounded border border-gray-300 border-opacity-5 bg-gray-950 bg-opacity-25 p-4 duration-300 hover:border-accent hover:border-opacity-20 hover:bg-opacity-40"
         id="wordle"
       >
         <h1 class="mb-2 text-center text-white lg:mb-4 lg:text-xl">wordle</h1>
