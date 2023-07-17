65f06970efd97ba8a2d9454b34fe160129f54154
max <29782397+tekoh@users.noreply.github.com>
2023-07-17 21:53:04 +0100

    LEADERBOARD ANIMATION WOOO TOOK ME SLEEPLESS NIGHTS TO FIGURE OUT ONLY TO DISCOVER I ONLY NEED TO ADD |global

--- src/lib/components/leaderboards/Leaderboard.svelte
+++ src/lib/components/leaderboards/Leaderboard.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
   import type { LeaderboardData } from "$lib/types/LeaderboardData";
+  import { fly } from "svelte/transition";
 
   export let data: LeaderboardData;
   export let suffix: (value: string) => string;
 </script>
 
 <table class="mx-auto mt-1 w-full text-gray-200 sm:w-1/2 sm:text-xl">
-  {#each data.slice(0, 100) as { user, value, position }}
+  {#each data.slice(0, 100) as { user, value, position }, i}
     <tr
       class="flex border-b-[8px] border-gray-900 border-opacity-100 bg-gray-950 bg-opacity-20 duration-200 ease-in hover:scale-105"
+      in:fly|global={{ delay: i * 55, duration: 500, y: 250 }}
     >
       <td class="px-1 py-1 text-gray-400">#{position}</td>
       <td class="line-clamp-1 grow px-4 py-1">
