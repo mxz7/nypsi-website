b3b4baba3fdf246b758a1d1d8ad9c3a26322c009
max <29782397+tekoh@users.noreply.github.com>
2023-07-17 20:16:26 +0100

    Revert "fix: 0 minimum tick"
    This reverts commit 080479ae24c61e1ce47e05ac8304ed703f8e94d2.

--- src/lib/components/leaderboards/Leaderboard.svelte
+++ src/lib/components/leaderboards/Leaderboard.svelte
@@ -1,31 +1,30 @@
 <script lang="ts">
   import type { LeaderboardData } from "$lib/types/LeaderboardData";
-  import { fade } from "svelte/transition";
 
   export let data: LeaderboardData;
   export let suffix: (value: string) => string;
 </script>
 
-<div class="mx-auto mt-1 w-full text-gray-200 sm:w-1/2 sm:text-xl" in:fade={{ duration: 5000 }}>
-  {#each data.slice(0, 100) as { user, value, position }, i}
-    <div
+<table class="mx-auto mt-1 w-full text-gray-200 sm:w-1/2 sm:text-xl">
+  {#each data.slice(0, 100) as { user, value, position }}
+    <tr
       class="flex border-b-[8px] border-gray-900 border-opacity-100 bg-gray-950 bg-opacity-20 duration-200 ease-in hover:scale-105"
     >
-      <p class="px-1 py-1 text-gray-400">#{position}</p>
-      <p class="line-clamp-1 grow px-4 py-1">
+      <td class="px-1 py-1 text-gray-400">#{position}</td>
+      <td class="line-clamp-1 grow px-4 py-1">
         {#if user.id}
           <a href="/user/{user.id}">{user.username}</a>
         {:else}
           {user.username}
         {/if}
-      </p>
-      <p class="px-4 py-1 text-right">
-        {value}
-        <span class="hidden opacity-75 sm:inline">{suffix(value)}</span>
-      </p>
-    </div>
+      </td>
+      <td class="px-4 py-1 text-right"
+        >{value}
+        <span class="hidden opacity-75 sm:inline">{suffix(value)}</span></td
+      >
+    </tr>
   {/each}
-</div>
+</table>
 
 <style>
   tr:nth-child(1) {
