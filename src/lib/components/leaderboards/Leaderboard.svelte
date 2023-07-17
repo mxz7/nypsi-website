b8d3ffbf6df90728e722917b12c51c98c391a602
max <29782397+tekoh@users.noreply.github.com>
2023-07-17 22:14:24 +0100

    think i did it !!

--- src/lib/components/leaderboards/Leaderboard.svelte
+++ src/lib/components/leaderboards/Leaderboard.svelte
@@ -9,18 +9,18 @@
 <table class="mx-auto mt-1 w-full px-3 text-gray-200 sm:w-1/2 sm:px-0 sm:text-xl">
   {#each data.slice(0, 100) as { user, value, position }, i}
     <tr
-      class="flex w-full border-b-[8px] border-gray-900 border-opacity-100 bg-gray-950 bg-opacity-20 duration-200 ease-in hover:scale-105"
+      class="flex w-full items-center gap-2 border-b-[8px] border-gray-900 border-opacity-100 bg-gray-950 bg-opacity-20 duration-200 ease-in hover:scale-105"
       in:fly|global={{ delay: i * 55, duration: 500, y: 250 }}
     >
-      <td class="px-1 py-1 text-gray-400">#{position}</td>
-      <td class="line-clamp-1 grow px-4 py-1">
+      <td class="pl-2 text-gray-400">#{position}</td>
+      <td class="line-clamp-1 break-all py-1">
         {#if user.id}
           <a href="/user/{user.id}">{user.username}</a>
         {:else}
           {user.username}
         {/if}
       </td>
-      <td class="px-4 py-1 text-right"
+      <td class="grow py-1 pr-2 text-right"
         >{value}
         <span class="hidden opacity-75 sm:inline">{suffix(value)}</span></td
       >
