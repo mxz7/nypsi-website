41ed88d2a5d37d7e9a5582b61ca0db5a25418774
max <29782397+tekoh@users.noreply.github.com>
2023-07-18 22:15:13 +0100

    nav barrr

--- src/routes/me/+layout.svelte
+++ src/routes/me/+layout.svelte
@@ -2,17 +2,28 @@
   import tooltip from "$lib/Tooltips.js";
   import Loading from "$lib/components/Loading.svelte";
   import Profile from "$lib/components/users/Profile.svelte";
+  import { onMount } from "svelte";
   import { fly } from "svelte/transition";
 
+  let graphsAllowed = false;
+
   export let data;
+
+  onMount(async () => {
+    const userData = await Promise.resolve(data.streamed.userData);
+
+    if (userData.message === "success" && userData?.Premium?.level > 0) {
+      graphsAllowed = true;
+    }
+  });
 </script>
 
 <svelte:head>
   <title>dashboard | nypsi</title>
 </svelte:head>
 
-<div class="flex w-full justify-center">
-  <div class="overflow-show mt-4 flex h-40 w-full justify-center sm:mt-8">
+<div class="flex w-full flex-col justify-center">
+  <div class="overflow-show mt-4 flex h-40 w-full justify-center sm:mt-8 md:h-[30vh]">
     <div class="flex h-fit w-full flex-col sm:w-[50vw]">
       {#await data.streamed.userData}
         <div class="relative mt-10 h-full w-full">
@@ -41,6 +52,46 @@
           </h1>
         {/if}
       {/await}
+    </div>
+  </div>
+
+  <div class="flex w-full justify-center">
+    <div class="mt-3 flex w-full flex-row justify-center px-4 text-gray-200 sm:w-[60vw]">
+      {#if graphsAllowed}
+        <a
+          class="grow border-b border-gray-700 pb-2 text-center hover:border-accent"
+          href="/me/graphs"
+        >
+          graphs
+        </a>
+      {:else}
+        <a
+          class="grow cursor-not-allowed border-b border-gray-700 pb-2 text-center hover:border-red-500"
+          href="/me"
+          use:tooltip={{
+            theme: "tooltip",
+            placement: "top",
+            allowHTML: true,
+            content:
+              'you require <a href="https://ko-fi.com/tekoh/tiers" target="_blank" class="text-sky-300 underline">premium</a> to view graphs',
+            interactive: true,
+          }}
+        >
+          graphs
+        </a>
+      {/if}
+      <a
+        class="grow border-b border-gray-700 pb-2 text-center hover:border-accent"
+        href="/me/other"
+      >
+        other
+      </a>
+      <a
+        class="grow border-b border-gray-700 pb-2 text-center hover:border-accent"
+        href="/me/other"
+      >
+        other
+      </a>
     </div>
   </div>
 </div>
