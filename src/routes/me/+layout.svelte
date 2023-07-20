bed5d678237b00780b410cc691c9b05a6d7929bd
max <29782397+tekoh@users.noreply.github.com>
2023-07-20 18:01:40 +0100

    feat: purchase history

--- src/routes/me/+layout.svelte
+++ src/routes/me/+layout.svelte
@@ -85,6 +85,7 @@
           graphs
         </a>
       {/if}
+
       <a
         class="grow border-b border-gray-700 pb-2 text-center hover:border-accent {$page.url.pathname.includes(
           '/stats'
@@ -94,6 +95,17 @@
         href="/me/stats"
       >
         stats
+      </a>
+
+      <a
+        class="grow border-b border-gray-700 pb-2 text-center hover:border-accent {$page.url.pathname.includes(
+          '/purchasehistory'
+        )
+          ? 'border-b-accent'
+          : ''}"
+        href="/me/purchasehistory"
+      >
+        purchases
       </a>
     </div>
   </div>
