423ae35293e6594935104b44e7b0f0cfb1f9d2e9
max <29782397+tekoh@users.noreply.github.com>
2023-07-20 18:16:16 +0100

    fix: hide stats link

--- src/routes/me/+layout.svelte
+++ src/routes/me/+layout.svelte
@@ -85,7 +85,7 @@
           graphs
         </a>
       {/if}
-
+<!-- 
       <a
         class="grow border-b border-gray-700 pb-2 text-center hover:border-accent {$page.url.pathname.includes(
           '/stats'
@@ -95,7 +95,7 @@
         href="/me/stats"
       >
         stats
-      </a>
+      </a> -->
 
       <a
         class="grow border-b border-gray-700 pb-2 text-center hover:border-accent {$page.url.pathname.includes(
