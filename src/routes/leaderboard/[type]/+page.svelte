144d85d1eaca6214aefb3be0853b22be16d2372b
max <29782397+tekoh@users.noreply.github.com>
2023-07-25 15:02:11 +0100

    fix: add leaderboard desc

--- src/routes/leaderboard/[type]/+page.svelte
+++ src/routes/leaderboard/[type]/+page.svelte
@@ -10,6 +10,8 @@
 <svelte:head>
   <title>{$page.params.type} leaderboard | nypsi</title>
 
+  <meta name="description" content="leaderboard for {$page.params.type}">
+
   <meta name="og:title" content="{$page.params.type} leaderboard | nypsi" />
   <meta name="og:site_name" content="nypsi" />
   {#if data.item}
