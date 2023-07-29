c8246dfd9ffd2fe085345478954c80080e1bc7a4
max <29782397+tekoh@users.noreply.github.com>
2023-07-29 21:03:26 +0100

    refactor: change loader

--- src/lib/components/Loading.svelte
+++ src/lib/components/Loading.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+  import { Loader2 } from "lucide-svelte";
   import { fade, type FadeParams } from "svelte/transition";
 
-  export let size = 10;
+  export let size = 56;
   export let fadeOutSettings: FadeParams = { duration: 150 };
   export let fadeInSettings: FadeParams = { duration: 50 };
 </script>
@@ -12,18 +13,8 @@
   out:fade|global={fadeOutSettings}
 >
   <div class="flex items-center justify-center">
-    <svg
-      class="-ml-1 mr-3 h-{size} w-{size} animate-spin text-accent"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-    >
-      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
-      <path
-        class="opacity-75"
-        fill="currentColor"
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      />
-    </svg>
+    <div class="animate-spin">
+      <Loader2 size="{size}px" strokeWidth="2.5px" color="#8b5cf6" />
+    </div>
   </div>
 </div>
