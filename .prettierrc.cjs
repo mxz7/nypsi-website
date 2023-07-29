0f6f092579202bf97bee7486763a8d9a8cfa3be2
max <29782397+tekoh@users.noreply.github.com>
2023-07-29 20:24:18 +0100

    fix: prettier works now!!!

--- .prettierrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  pluginSearchDirs: false,
-  plugins: [require('prettier-plugin-svelte'), require('prettier-plugin-tailwindcss')],
-  overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
-  printWidth: 100,
-  singleQuote: true,
-};
