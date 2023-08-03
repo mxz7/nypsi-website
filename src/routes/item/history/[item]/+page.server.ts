b4100e33fec3a2d1f58d2c4155e530658fec28c9
max <29782397+tekoh@users.noreply.github.com>
2023-08-03 22:43:21 +0100

    fix: weirrddd

--- src/routes/item/history/[item]/+page.server.ts
+++ src/routes/item/history/[item]/+page.server.ts
@@ -14,5 +14,9 @@ export const load = async ({ params, parent, setHeaders }) => {
 
   if (!item) return { item: undefined, streamed: { graphData: "invalid item" } };
 
-  return { base: 69, item, streamed: { graphData: getChartData(params.item, parentData.user.id) } };
+  return {
+    base: 69,
+    item,
+    streamed: { graphData: getChartData(params.item, parentData.user.id) },
+  };
 };
