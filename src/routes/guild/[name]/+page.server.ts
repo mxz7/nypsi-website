cce2c365ef985da7c85c939c41ae11afe1e9f435
max <29782397+tekoh@users.noreply.github.com>
2023-08-14 17:35:32 +0100

    fix: guild page

--- src/routes/guild/[name]/+page.server.ts
+++ src/routes/guild/[name]/+page.server.ts
@@ -1,6 +1,8 @@
 import getGuildData from "$lib/server/functions/graphs/getGuildData.js";
 import type { ApiGuildResponse } from "$lib/types/Guild.js";
 
+export const ssr = false;
+
 export const load = async ({ params, fetch }) => {
   const guild = await fetch(`/api/guild/${params.name}`).then(
     (r) => r.json() as unknown as ApiGuildResponse,
