98179d3997d39db152d3eb2dd0ee04433823f505
max <29782397+tekoh@users.noreply.github.com>
2023-08-14 14:25:04 +0100

    refactor: dont use promises (test for google search console)

--- src/routes/guild/[name]/+page.svelte
+++ src/routes/guild/[name]/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { page } from "$app/stores";
-  import Loading from "$lib/components/Loading.svelte";
   import Guild from "$lib/components/guild/Guild.svelte";
   import { userSearchTerm } from "$lib/data/stores.js";
   import type { ApiGuildResponse } from "$lib/types/Guild.js";
@@ -25,24 +24,26 @@
   <meta name="og:title" content={title} />
 </svelte:head>
 
-{#await data.streamed.data}
-  <div class="relative mt-16 w-full">
-    <Loading fadeInSettings={{ delay: 100, duration: 200 }} fadeOutSettings={{ duration: 300 }} />
-  </div>
-{:then guildData}
-  <div>
-    {#if !guildData.success}
-      <div
-        class="absolute left-1/2 top-1/4 -translate-x-1/2 -translate-y-1/2 transform"
-        in:fly={{ delay: 300, duration: 500, y: 75 }}
-      >
-        <p class="text-xl font-bold text-slate-300">unknown guild</p>
-      </div>
-    {:else}
-      <Guild {guildData}></Guild>
-    {/if}
-  </div>
-{/await}
+<!-- {#await data.streamed.data} -->
+<!-- <div class="relative mt-16 w-full">
+  <Loading fadeInSettings={{ delay: 100, duration: 200 }} fadeOutSettings={{ duration: 300 }} />
+</div> -->
+<!-- {:then guildData} -->
+<div>
+  {#if !data.streamed.data.success}
+    <div
+      class="absolute left-1/2 top-1/4 -translate-x-1/2 -translate-y-1/2 transform"
+      in:fly={{ delay: 300, duration: 500, y: 75 }}
+    >
+      <p class="text-xl font-bold text-slate-300">unknown guild</p>
+    </div>
+  {:else}
+    {#key data.streamed.data}
+      <Guild guildData={data.streamed.data}></Guild>
+    {/key}
+  {/if}
+</div>
+<!-- {/await} -->
 <!-- https://cdn.discordapp.com/emojis/1118558624197902347.png
 https://cdn.discordapp.com/emojis/987046773157691452.png
 https://cdn.discordapp.com/emojis/1118563689008734258.png -->
--- src/routes/guild/[name]/+page.ts
+++ src/routes/guild/[name]/+page.ts
@@ -3,7 +3,9 @@ import type { ApiGuildResponse } from "$lib/types/Guild.js";
 export const load = async ({ params, fetch }) => {
   return {
     streamed: {
-      data: fetch(`/api/guild/${params.name}`).then((r) => r.json() as unknown as ApiGuildResponse),
+      data: await fetch(`/api/guild/${params.name}`).then(
+        (r) => r.json() as unknown as ApiGuildResponse,
+      ),
     },
   };
 };
