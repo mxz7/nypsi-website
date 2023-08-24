aaf3061fab41dfa1f06b331a01caed5aae0cd893
max <29782397+tekoh@users.noreply.github.com>
2023-08-24 16:16:23 +0100

    Revert "feat: add sentry.io"
    This reverts commit 8cbfa825daaead20331b8d1baf677d96845d18c5.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,7 @@
 import { dev } from "$app/environment";
 import rateLimiter from "$lib/server/ratelimit";
-import * as Sentry from "@sentry/sveltekit";
-import { sequence } from "@sveltejs/kit/hooks";
 
-Sentry.init({
-    dsn: "https://a73111e3e265cbe1bf97fa484c0e1a0e@o4505075574898688.ingest.sentry.io/4505760518832128",
-    tracesSampleRate: 1
-})
-
-export const handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
+export const handle = async ({ event, resolve }) => {
   if (!dev && event.url.pathname.startsWith("/api")) {
     const rateLimitAttempt = await rateLimiter.limit(event.getClientAddress()).catch(() => {
       return { success: true, reset: 69 };
@@ -30,5 +23,4 @@ export const handle = sequence(Sentry.sentryHandle(), async ({ event, resolve })
   }
 
   return await resolve(event);
-});
-export const handleError = Sentry.handleErrorWithSentry();
+};
