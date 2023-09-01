7e8dedd88c70e8e858344874c870f96fa6c47e08
max <29782397+tekoh@users.noreply.github.com>
2023-09-01 19:05:35 +0100

    feat: meta tags

--- src/routes/user/[search]/+page.svelte
+++ src/routes/user/[search]/+page.svelte
@@ -10,6 +10,14 @@
 <svelte:head>
   <title>{title}</title>
   <meta name="og:title" content={title} />
+
+  <meta name="description" content="view {data.user.lastKnownUsername}'s nypsi profile" />
+  <meta name="og:description" content="view {data.user.lastKnownUsername}'s nypsi profile" />
+
+  <meta name="og:site_name" content="nypsi" />
+  <meta name="og:image" content={data.user.avatar} />
+  <meta property="og:image:width" content="128" />
+  <meta property="og:image:height" content="128" />
 </svelte:head>
 
 <div in:fade={{ delay: 0, duration: 200 }} out:fly={{ y: 15, duration: 250 }}>
