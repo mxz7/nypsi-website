7a06481bc971210491ffe226cdc46352a847b115
max <29782397+tekoh@users.noreply.github.com>
2023-09-01 19:30:16 +0100

    fix:???>>>>

--- src/routes/user/[search]/+page.svelte
+++ src/routes/user/[search]/+page.svelte
@@ -5,23 +5,26 @@
 
   export let data;
 
-  $: title = `${data.user.lastKnownUsername} | nypsi`;
-  $userSearchTerm = data.user.lastKnownUsername;
+  $: title = `${data.baseUserData.lastKnownUsername} | nypsi`;
+  $userSearchTerm = data.baseUserData.lastKnownUsername;
 </script>
 
 <svelte:head>
   <title>{title}</title>
   <meta name="og:title" content={title} />
 
-  <meta name="description" content="view {data.user.lastKnownUsername}'s nypsi profile" />
-  <meta name="og:description" content="view {data.user.lastKnownUsername}'s nypsi profile" />
+  <meta name="description" content="view {data.baseUserData.lastKnownUsername}'s nypsi profile" />
+  <meta
+    name="og:description"
+    content="view {data.baseUserData.lastKnownUsername}'s nypsi profile"
+  />
 
   <meta name="og:site_name" content="nypsi" />
-  <meta name="og:image" content={data.user.avatar} />
+  <meta name="og:image" content={data.baseUserData.avatar} />
   <meta property="og:image:width" content="128" />
   <meta property="og:image:height" content="128" />
 </svelte:head>
 
 <div in:fade={{ delay: 0, duration: 200 }} out:fly={{ y: 15, duration: 250 }}>
-  <UserData baseData={data.user} userData={data.streamed.allUserData} items={data.items} />
+  <UserData baseData={data.baseUserData} userData={data.streamed.allUserData} items={data.items} />
 </div>
