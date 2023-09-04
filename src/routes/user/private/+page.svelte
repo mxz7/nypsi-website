de61023f0df139ea2818120ce68ba109279adeee
max <29782397+tekoh@users.noreply.github.com>
2023-09-04 13:19:25 +0100

    fix: noindex for invalid pages

--- src/routes/user/private/+page.svelte
+++ src/routes/user/private/+page.svelte
@@ -1,5 +1,6 @@
 <svelte:head>
   <title>unknown user | nypsi</title>
+  <meta name="robots" content="noindex" />
 </svelte:head>
 
 <h1 class="w-full text-center text-xl font-bold text-slate-200">private profile</h1>
