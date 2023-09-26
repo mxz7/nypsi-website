dd380171818a357993f12badc60f81f4d22ca0d6
max <29782397+tekoh@users.noreply.github.com>
2023-09-26 20:11:06 +0100

    smelly legal stuf

---
+++ src/routes/(legal)/privacy/+page.svelte
@@ -0,0 +1,51 @@
+<svelte:head>
+  <title>terms of service / nypsi</title>
+</svelte:head>
+
+<div class="flex w-full justify-center">
+  <div
+    class="font-xl w-full pt-14 md:max-w-xl [&>h2]:my-4 [&>h2]:font-semibold [&>h3]:my-2 [&>h3]:font-semibold [&>h3]:text-gray-300"
+  >
+    <h1 class="mb-6 text-center text-2xl font-semibold">Privacy Policy</h1>
+
+    <h2>Usage of Data</h2>
+    <p>The bot may use stored data for certain features provided by the service.</p>
+    <p>This data is not shared with any third party services.</p>
+
+    <h2>Automatically Gathered Information</h2>
+    <p>Only states values stored in a persistant storage when configured to do so by the user.</p>
+    <h3>Guilds</h3>
+    <ul>
+      <li>Guild ID</li>
+      <li>Member Peaks</li>
+      <li class="text-sm">Default settings generated for the guild by the bot.</li>
+    </ul>
+
+    <h3>Users</h3>
+    <ul>
+      <li>User ID</li>
+      <li>Username</li>
+      <li>Avatar</li>
+      <li>Mention Data</li>
+      <li>Username History</li>
+    </ul>
+
+    <h3>Updating Data</h3>
+    <p>
+      The data mentioned and data that has not been mentioned may be automatically updated upon user
+      interaction or through websocket events
+    </p>
+
+    <h2>Removal of Data</h2>
+    <p>
+      A user can request their data deletion via the command `/data delete`. The only exemption to
+      this is under the rare occasion that a user is blacklisted, meaning we must keep a record of
+      their user ID to prevent access to our services.
+      <br /><br />
+      To request deletion of guild data, please use the contact details below
+    </p>
+
+    <h2>Contact</h2>
+    <a href="https://discord.com/invite/hJTDNST" class="text-sky-300 underline">Discord</a>
+  </div>
+</div>
---
+++ src/routes/(legal)/terms/+page.svelte
@@ -0,0 +1,56 @@
+<svelte:head>
+  <title>privacy policy / nypsi</title>
+</svelte:head>
+
+<div class="flex w-full justify-center">
+  <div class="font-xl w-full pt-14 md:max-w-xl [&>h2]:my-4 [&>h2]:font-semibold">
+    <h1 class="mb-6 text-center text-2xl font-semibold">Terms of Service</h1>
+
+    <h2>Usage Agreement</h2>
+    <p>
+      By inviting the bot and using its features you are agreeing to the below mentioned Terms and
+      <a class="text-sky-300 underline" href="/privacy">Privacy Policy (Policy)</a> of the bot.
+      <br />
+      You acknowledge that you have the priviledge to use the bot freely on any Discord Server (Server)
+      you share with it, that you can invite it to any Server that you have "Manage Server" rights for
+      and that this priviledge might get revoked for you, if you're subject of breaking the terms and/or
+      policy of this bot, or the Terms of Service, Privacy Policy and/or Community Guidelines of Discord
+      Inc.
+      <br />
+      Through Inviting the bot may it collect specific data as described in its Policy. The intended
+      usage of this data is for core functionalities of the bot such as but not limited to: command handling
+      and guild-specific settings
+    </p>
+
+    <h2>Liability</h2>
+    <p>
+      The owner of the bot may face to liability due to users using the bot in its otherwise
+      intended purpose.<br />This website and the bot may include user created content which is by
+      their own self and opinion, not created by the owner of nypsi. Moderation of user created
+      content is ongoing however errors do happen and things are missed.
+    </p>
+
+    <h2>Termination</h2>
+    <p>
+      Your access may be terminated to the bot and any services at any time without reason or notice
+      under our discretion. To prevent this, please follow the rules in the <a
+        href="https://discord.com/invite/hJTDNST"
+        class="text-sky-300 underline"
+        target="_blank">community Discord Server</a
+      >
+    </p>
+
+    <h2>Changes</h2>
+    <p>
+      We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If
+      a revision is material we will provide at least 30 days notice prior to any new terms taking
+      effect. What constitutes a material change will be determined at our sole discretion. By
+      continuing to access or use our Service after any revisions become effective, you agree to be
+      bound by the revised terms. If you do not agree to the new terms, you are no longer authorized
+      to use the Service.
+    </p>
+
+    <h2>Contact</h2>
+    <a href="https://discord.com/invite/hJTDNST" class="text-sky-300 underline">Discord</a>
+  </div>
+</div>
