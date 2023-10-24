c286cbe8ce7a448ef142f84683fa2a150313a7bb
max <29782397+tekoh@users.noreply.github.com>
2023-10-24 17:00:43 +0100

    logs

--- src/routes/user/[search]/+page.server.ts
+++ src/routes/user/[search]/+page.server.ts
@@ -7,10 +7,14 @@ export const load = async ({ parent, params, fetch, setHeaders }) => {
     "cache-control": "s-maxage=300",
   });
 
+  console.log("a");
+
   const search = params.search;
   let userId: string;
 
   if (!search) throw redirect(302, "/user/unknown");
+
+  console.log("b");
 
   if (search === "me") {
     const parentData = await parent();
@@ -20,11 +24,14 @@ export const load = async ({ parent, params, fetch, setHeaders }) => {
   } else if (search.match(/^\d{17,19}$/)) {
     userId = search;
 
+    console.log("c");
+
     const res = await fetch(`/api/user/check/${userId}`).then((r) => r.json());
 
     if (!res.exists) throw redirect(302, `/user/unknown?user=${search}`);
     if (res.private) throw redirect(302, "/user/private");
   } else {
+    console.log("d");
     const res = await fetch(`/api/user/getid/${search}`).then((r) => r.json());
 
     if (res.id) {
@@ -33,6 +40,8 @@ export const load = async ({ parent, params, fetch, setHeaders }) => {
       throw redirect(302, `/user/unknown?user=${search}`);
     }
   }
+
+  console.log("e");
 
   const data = await prisma.user.findUnique({
     where: {
@@ -57,6 +66,8 @@ export const load = async ({ parent, params, fetch, setHeaders }) => {
       },
     },
   });
+
+  console.log("f");
 
   return {
     baseUserData: data,
