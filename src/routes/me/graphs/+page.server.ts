8ec09bdd171049ca208519697c6cd77c553c2062
max <29782397+tekoh@users.noreply.github.com>
2023-10-25 22:44:47 +0100

    fix: parent already fetches items

--- src/routes/me/graphs/+page.server.ts
+++ src/routes/me/graphs/+page.server.ts
@@ -1,11 +1,12 @@
-import getItems from "$lib/functions/getItems.js";
 import getItemCountDataForUser from "$lib/server/functions/graphs/getItemCountDataForUser.js";
 import { redirect } from "@sveltejs/kit";
 
 export const ssr = false;
 
 export async function load({ setHeaders, parent, url }) {
-  const [parentData, items] = await Promise.all([parent(), getItems()]);
+  const parentData = await parent();
+
+  console.log(parentData);
 
   if (!parentData.user.authenticated) throw redirect(303, "/me");
 
@@ -22,28 +23,33 @@ export async function load({ setHeaders, parent, url }) {
     const categories: string[] = [];
 
     for (const itemId of url.searchParams.get("items").split(" ")) {
-      if (!items.find((i) => i.id === itemId) || categories.length >= 10) {
+      if (!parentData.items.find((i) => i.id === itemId) || categories.length >= 10) {
         url.searchParams.set(
           "items",
           url.searchParams.get("items").replace(itemId, "").replaceAll("  ", " "),
         );
       } else {
-        categories.push(`user-item-${items.find((i) => i.id === itemId).id}`);
+        categories.push(`user-item-${parentData.items.find((i) => i.id === itemId).id}`);
       }
     }
 
     return {
       streamed: {
-        items: getItemCountDataForUser(categories, parentData.user.id, items, days),
+        items: getItemCountDataForUser(categories, parentData.user.id, parentData.items, days),
       },
     };
   } else {
     return {
       streamed: {
-        balance: getItemCountDataForUser(["user-money"], parentData.user.id, items, days),
-        networth: getItemCountDataForUser(["user-net"], parentData.user.id, items, days),
-        karma: getItemCountDataForUser(["user-karma"], parentData.user.id, items, days),
-        level: getItemCountDataForUser(["user-level"], parentData.user.id, items, days),
+        balance: getItemCountDataForUser(
+          ["user-money"],
+          parentData.user.id,
+          parentData.items,
+          days,
+        ),
+        networth: getItemCountDataForUser(["user-net"], parentData.user.id, parentData.items, days),
+        karma: getItemCountDataForUser(["user-karma"], parentData.user.id, parentData.items, days),
+        level: getItemCountDataForUser(["user-level"], parentData.user.id, parentData.items, days),
       },
     };
   }
--- src/routes/user/[search]/+page.server.ts
+++ src/routes/user/[search]/+page.server.ts
@@ -7,14 +7,10 @@ export const load = async ({ parent, params, fetch, setHeaders }) => {
     "cache-control": "s-maxage=300",
   });
 
-  console.log("a");
-
   const search = params.search;
   let userId: string;
 
   if (!search) throw redirect(302, "/user/unknown");
-
-  console.log("b");
 
   if (search === "me") {
     const parentData = await parent();
@@ -24,14 +20,11 @@ export const load = async ({ parent, params, fetch, setHeaders }) => {
   } else if (search.match(/^\d{17,19}$/)) {
     userId = search;
 
-    console.log("c");
-
     const res = await fetch(`/api/user/check/${userId}`).then((r) => r.json());
 
     if (!res.exists) throw redirect(302, `/user/unknown?user=${search}`);
     if (res.private) throw redirect(302, "/user/private");
   } else {
-    console.log("d");
     const res = await fetch(`/api/user/getid/${search}`).then((r) => r.json());
 
     if (res.id) {
@@ -40,8 +33,6 @@ export const load = async ({ parent, params, fetch, setHeaders }) => {
       throw redirect(302, `/user/unknown?user=${search}`);
     }
   }
-
-  console.log("e");
 
   const data = await prisma.user.findUnique({
     where: {
@@ -66,8 +57,6 @@ export const load = async ({ parent, params, fetch, setHeaders }) => {
       },
     },
   });
-
-  console.log("f");
 
   return {
     baseUserData: data,
