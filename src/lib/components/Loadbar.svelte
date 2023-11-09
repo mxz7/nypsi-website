31dcdd7a41bf928287e138e39a6783008b912e60
max <29782397+tekoh@users.noreply.github.com>
2023-11-09 16:32:53 +0000

    refactor: longer delay

--- src/lib/components/Loadbar.svelte
+++ src/lib/components/Loadbar.svelte
@@ -8,13 +8,14 @@
 
   let status: "loading" | "inactive" | "finishing" = "inactive";
   let started = 0;
+  const delay = 550;
 
   navigating.subscribe((value) => {
     if (value) {
       progress.set(0, { duration: 0 });
       status = "loading";
       started = Date.now();
-      progress.set(60, { delay: 400, duration: 4500 });
+      progress.set(60, { delay: delay, duration: 4500 });
     } else {
       if (status === "inactive") return;
       if (started > Date.now() - 200) {
@@ -34,7 +35,7 @@
 <div class="sticky top-0 h-[2px] w-full">
   {#if ["loading", "finishing"].includes(status)}
     <div
-      in:fade={{ delay: 400, duration: 50 }}
+      in:fade={{ delay: delay, duration: 50 }}
       out:fade={{ duration: 750 }}
       style="width: {$progress}%;"
       class="top-0 h-[2px] rounded bg-accent duration-100"
