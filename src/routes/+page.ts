44fd727ce6c27c9aba7bc406f74103b0754dd4bd
max <29782397+tekoh@users.noreply.github.com>
2023-11-09 16:39:15 +0000

    feat: inital page cache

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,7 +3,8 @@ export const config = {
   regions: "all",
 };
 
-export const load = async ({ fetch }) => {
+export const load = async ({ fetch, setHeaders }) => {
+  setHeaders({'cache-control': 'max-age=60'})
   const data = fetch("/api/server-count").then(async (r) => r.json());
   return { base: 69, streamed: { topgg: data as Promise<{ server_count: number }> } };
 };
