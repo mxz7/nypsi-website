fdefcb8c5d8327d86d8aa667f04feb61c7dabc13
max <29782397+tekoh@users.noreply.github.com>
2023-11-14 13:43:18 +0000

    fix: stupid typescript

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,10 +3,10 @@ import {
   DISCORD_OAUTH_REDIRECT,
   DISCORD_OAUTH_SECRET,
 } from "$env/static/private";
-import type { User, UserSession } from "$lib/types/User.js";
+import type { UserSession } from "$lib/types/User.js";
 
-export const load = async ({ cookies, fetch, url }) => {
-  const user: UserSession = { authenticated: false };
+export const load = async ({ cookies, fetch }) => {
+  let user: UserSession;
 
   if (cookies.get("discord_refresh_token") && !cookies.get("discord_access_token")) {
     const res = await fetch("https://discord.com/api/oauth2/token", {
@@ -66,11 +66,12 @@ export const load = async ({ cookies, fetch, url }) => {
       return { user: { authenticated: false } };
     }
 
-    (user as unknown as User).authenticated = true;
-    (user as unknown as User).avatar = userRequest.avatar;
-    (user as unknown as User).discriminator = userRequest.discriminator;
-    (user as unknown as User).username = userRequest.username;
-    (user as unknown as User).id = userRequest.id;
+    user = {
+      authenticated: true,
+      avatar: userRequest.avatar,
+      username: userRequest.username,
+      id: userRequest.id,
+    };
   } else if (cookies.get("discord_access_token")) {
     const userRequest = await fetch("https://discord.com/api/users/@me", {
       headers: { Authorization: `Bearer ${cookies.get("discord_access_token")}` },
@@ -89,14 +90,17 @@ export const load = async ({ cookies, fetch, url }) => {
       return { user: { authenticated: false } };
     }
 
-    (user as unknown as User).authenticated = true;
-    (user as unknown as User).avatar = userRequest.avatar;
-    (user as unknown as User).discriminator = userRequest.discriminator;
-    (user as unknown as User).username = userRequest.username;
-    (user as unknown as User).id = userRequest.id;
+    user = {
+      authenticated: true,
+      avatar: userRequest.avatar,
+      username: userRequest.username,
+      id: userRequest.id,
+    };
   }
 
-  if (user.authenticated && !(user as User).id) {
+  if (!user) user = { authenticated: false };
+
+  if (user.authenticated && !user.id) {
     console.log(user);
     console.log(cookies.getAll());
   }
@@ -105,7 +109,7 @@ export const load = async ({ cookies, fetch, url }) => {
     user: user as UserSession,
     streamed: {
       premium: user.authenticated
-        ? fetch(`/api/user/ispremium/${(user as User).id}`).then(
+        ? fetch(`/api/user/ispremium/${user.id}`).then(
             (r) => r.json().then((r) => r?.premium || false) as Promise<boolean>,
           )
         : false,
