e282525a0c94c1bd2cd4d42f410935b42066b057
max <29782397+tekoh@users.noreply.github.com>
2023-11-14 13:57:50 +0000

    refactor: use func for privacy check

---
+++ src/lib/server/functions/user/privacy.ts
@@ -0,0 +1,13 @@
+import type { APIUserCheck } from "$lib/types/api/UserCheck";
+import { error } from "@sveltejs/kit";
+
+export async function privacyCheck(
+  userId: string,
+  fetch: (input: RequestInfo | URL, init?: RequestInit) => Promise<Response>,
+) {
+  const privacyCheck: APIUserCheck = await fetch(`/api/user/check/${userId}`).then((r) => r.json());
+
+  if (!privacyCheck?.ok || !privacyCheck.exists) return error(404, { message: "user not found" });
+  if (privacyCheck.private) return error(403, { message: "user has a private profile" });
+  return "continue";
+}
