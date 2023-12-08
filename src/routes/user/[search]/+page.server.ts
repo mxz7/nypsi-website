b755a357909df7a2293cb2999a14190733a5c67c
max <29782397+tekoh@users.noreply.github.com>
2023-12-08 15:36:17 +0000

    i think this will work!!

--- src/routes/user/[search]/+page.server.ts
+++ src/routes/user/[search]/+page.server.ts
@@ -1,8 +1,10 @@
 import getItems from "$lib/functions/getItems.js";
+import prisma from "$lib/server/database.js";
 import type { BaseUserData } from "$lib/types/User.js";
 import { redirect } from "@sveltejs/kit";
+import dayjs from "dayjs";
 
-export const load = async ({ params, fetch, setHeaders }) => {
+export const load = async ({ params, fetch, setHeaders, parent, getClientAddress, request }) => {
   setHeaders({
     "cache-control": "max-age=300",
   });
@@ -37,6 +39,37 @@ export const load = async ({ params, fetch, setHeaders }) => {
         .then((r) => r.text())
         .then((r) => JSON.parse(r)),
       allUserData: fetch(`/api/user/${userId}`).then((r) => r.json()),
+      _view: (async () => {
+        const parentData = await parent();
+
+        if (parentData.user.authenticated) {
+          if (parentData.user.id === userId) return;
+        }
+
+        const views = await prisma.profileView.findMany({
+          where: {
+            AND: [{ createdAt: { gt: dayjs().subtract(10, "minutes").toDate() } }, { userId }],
+          },
+        });
+
+        for (const view of views) {
+          if (parentData.user.authenticated) {
+            if (parentData.user.id === view.viewerId) return;
+          }
+          if (view.viewerIp === getClientAddress()) return;
+          if (view.createdAt.getTime() >= dayjs().subtract(1, "minute").toDate().getTime()) return;
+        }
+
+        await prisma.profileView.create({
+          data: {
+            source: "WEB",
+            referrer: request.headers.get("referer"),
+            viewerId: parentData.user.authenticated ? parentData.user.id : null,
+            viewerIp: getClientAddress(),
+            userId,
+          },
+        });
+      })(),
     },
   };
 };
