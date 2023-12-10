7f5411404f83e741eaf0a575bd20d3e1108582c2
max <29782397+tekoh@users.noreply.github.com>
2023-12-10 14:22:00 +0000

    use edge more

--- src/routes/leaderboard/+page.ts
+++ src/routes/leaderboard/+page.ts
@@ -1,5 +1,10 @@
 import getItems from "$lib/functions/getItems.js";
 
+export const config = {
+  runtime: "edge",
+  regions: "all",
+};
+
 export async function load({ setHeaders }) {
   setHeaders({
     "cache-control": "s-maxage=3600, stale-while-revalidate",
