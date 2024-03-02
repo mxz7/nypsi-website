2dc18786798d87408a064ad31420e6d4e695f54a
max <29782397+tekoh@users.noreply.github.com>
2024-03-02 19:45:22 +0000

    omg leaderboards are perfect now

--- src/routes/leaderboard/BigLeaderboard.svelte
+++ src/routes/leaderboard/BigLeaderboard.svelte
@@ -23,7 +23,7 @@
         >
           {#each new Array(20) as _, i}
             <div
-              class="flex w-full items-center gap-2 rounded border border-slate-400 border-opacity-5 bg-slate-950 bg-opacity-20 px-2 py-2 duration-200 ease-in hover:scale-105 hover:border-accent hover:border-opacity-20"
+              class="flex w-full items-center gap-2 rounded-lg border border-slate-400 border-opacity-5 bg-slate-950 bg-opacity-20 px-2 py-2 duration-200 ease-in hover:scale-105 hover:border-accent hover:border-opacity-20"
             >
               <div class="my-1 h-4 w-8 animate-pulse rounded-xl bg-slate-600" />
               <div
@@ -48,17 +48,14 @@
         >
           {#each data as { position, user, value }, i}
             <div
-              class="flex w-full items-center gap-2 rounded border border-slate-400 border-opacity-5 bg-slate-950 bg-opacity-20 px-2 py-2 duration-200 ease-in hover:scale-105 hover:border-accent hover:border-opacity-20"
+              class="flex w-full items-center gap-2 rounded-lg border border-slate-400 border-opacity-5 bg-slate-950 bg-opacity-20 px-2 py-2 duration-200 ease-in hover:scale-105 hover:border-accent hover:border-opacity-20"
             >
-              <span class="text-slate-400 {i === 0 ? 'font-semibold' : ''}">#{position}</span>
+              <div class="text-slate-400 {i === 0 ? 'font-semibold' : ''}">#{position}</div>
 
-              <a
-                href={user.username === "[hidden]"
-                  ? "https://docs.nypsi.xyz/economy/hidden"
-                  : `${userRoute}/${user.id}`}
+              <div
                 class="{i === 0
                   ? 'font-semibold text-accent'
-                  : 'text-slate-300'} line-clamp-1 flex items-center break-words"
+                  : 'text-slate-300'} flex w-full items-center overflow-hidden"
               >
                 {#if user.id}
                   {#if user.tag}
@@ -77,15 +74,23 @@
                       <p class="mr-1">]</p>
                     {/await}
                   {/if}
-                  <a href="{userRoute}/{user.id}">{user.username}</a>
+                  <a
+                    href="{userRoute}/{user.id}"
+                    class="min-w-0 overflow-hidden overflow-ellipsis whitespace-nowrap"
+                    >{user.username}</a
+                  >
                 {:else}
                   {user.username}
                 {/if}
-              </a>
+              </div>
 
-              <div class="grow"></div>
-
-              <span class={i === 0 ? "font-semibold text-accent" : "text-slate-300"}>{value}</span>
+              <div
+                class="{i === 0
+                  ? 'font-semibold text-accent'
+                  : 'text-slate-300'} w-fit whitespace-nowrap pl-4 text-right"
+              >
+                {value}
+              </div>
             </div>
           {/each}
         </div>
