cdbae386be88c13c6bb2413e744b841a5b4792a3
max <29782397+tekoh@users.noreply.github.com>
2024-03-04 16:26:14 +0000

    better seo

--- src/routes/user/[search]/+page.server.ts
+++ src/routes/user/[search]/+page.server.ts
@@ -1,18 +1,18 @@
 import { VIEW_AUTH } from "$env/static/private";
 import getItems from "$lib/functions/getItems.js";
 import type Game from "$lib/types/Game.js";
-import type { BaseUserData } from "$lib/types/User.js";
+import type { BaseUserData, UserApiResponsexd } from "$lib/types/User.js";
 import { redirect } from "@sveltejs/kit";
 
 export const load = async ({ params, fetch, setHeaders, parent, getClientAddress, request }) => {
   setHeaders({
-    "cache-control": "max-age=300",
+    "cache-control": "max-age=30",
   });
 
   const search = params.search;
   let userId: string;
 
-  if (!search) redirect(302, "/user/unknown");
+  if (!search) return redirect(302, "/user/unknown");
 
   if (search.match(/^\d{17,19}$/)) {
     userId = search;
@@ -38,45 +38,62 @@ export const load = async ({ params, fetch, setHeaders, parent, getClientAddress
 
   const before = Date.now();
 
-  return {
-    baseUserData,
-    items,
-    allUserData: fetch(`/api/user/${userId}`).then((r) => r.json()),
-    games: fetch(`/api/game?user=${userId}&before=${before}&take=20`).then((r) =>
-      r.json(),
-    ) as Promise<{
-      ok: boolean;
-      games: Game[];
-    }>,
-    gamesBefore: before,
-    _view: (async () => {
-      if (request.headers.get("user-agent").includes("bot")) return;
-      const parentData = await parent();
+  if (request.headers.get("user-agent").toLowerCase().includes("bot")) {
+    return {
+      baseUserData,
+      items,
+      allUserData: await fetch(`/api/user/${userId}`).then(
+        (r) => r.json() as Promise<UserApiResponsexd>,
+      ),
+      games: (await fetch(`/api/game?user=${userId}&before=${before}&take=20`).then((r) =>
+        r.json(),
+      )) as Promise<{
+        ok: boolean;
+        games: Game[];
+      }>,
+      gamesBefore: before,
+    };
+  } else {
+    return {
+      baseUserData,
+      items,
+      allUserData: fetch(`/api/user/${userId}`).then((r) => r.json() as Promise<UserApiResponsexd>),
+      games: fetch(`/api/game?user=${userId}&before=${before}&take=20`).then((r) =>
+        r.json(),
+      ) as Promise<{
+        ok: boolean;
+        games: Game[];
+      }>,
+      gamesBefore: before,
+      _view: (async () => {
+        if (request.headers.get("user-agent").includes("bot")) return;
+        const parentData = await parent();
 
-      if (parentData.user.authenticated) {
-        if (parentData.user.id === userId) return;
-      }
+        if (parentData.user.authenticated) {
+          if (parentData.user.id === userId) return;
+        }
 
-      let ip: string;
+        let ip: string;
 
-      try {
-        ip = getClientAddress();
-      } catch {
-        ip = "127.0.0.1";
-      }
+        try {
+          ip = getClientAddress();
+        } catch {
+          ip = "127.0.0.1";
+        }
 
-      return fetch("/api/user/view/add", {
-        method: "POST",
-        headers: { Authorization: VIEW_AUTH },
-        body: JSON.stringify({
-          userId,
-          viewerId: parentData.user.authenticated ? parentData.user.id : undefined,
-          viewerIp: ip,
-          referrer: request.headers.get("referer") || undefined,
-        }),
-      })
-        .then((r) => r.json().catch(() => null))
-        .catch(() => null);
-    })(),
-  };
+        return fetch("/api/user/view/add", {
+          method: "POST",
+          headers: { Authorization: VIEW_AUTH },
+          body: JSON.stringify({
+            userId,
+            viewerId: parentData.user.authenticated ? parentData.user.id : undefined,
+            viewerIp: ip,
+            referrer: request.headers.get("referer") || undefined,
+          }),
+        })
+          .then((r) => r.json().catch(() => null))
+          .catch(() => null);
+      })(),
+    };
+  }
 };
