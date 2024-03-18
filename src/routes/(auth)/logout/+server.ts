92a0bb1d11a7be13f6febb06a47a3ee8f2a9188b
max <29782397+tekoh@users.noreply.github.com>
2024-03-18 19:27:47 +0000

    cant run on edge ):

--- src/routes/(auth)/logout/+server.ts
+++ src/routes/(auth)/logout/+server.ts
@@ -1,11 +1,6 @@
 import { lucia } from "$lib/server/functions/auth.js";
 import { redirect } from "@sveltejs/kit";
 
-export const config = {
-  runtime: "edge",
-  regions: "all",
-};
-
 export const GET = async ({ locals }) => {
   const auth = await locals.validate();
 
