63d21e2c42597765801a6814072973c05c356fe6
max <29782397+tekoh@users.noreply.github.com>
2024-03-18 20:04:33 +0000

    wtf why not main page

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,8 @@
+export const config = {
+  runtime: "edge",
+  regions: "all",
+};
+
 export const load = async ({ fetch }) => {
   const data = fetch("/api/server-count").then(async (r) => r.json());
   return { topgg: data as Promise<{ server_count: number }> };
