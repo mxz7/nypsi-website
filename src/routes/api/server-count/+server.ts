ce4f64e479dc6a5c0db81a080167ceb3654f3e24
max <29782397+tekoh@users.noreply.github.com>
2024-03-25 13:13:17 +0000

    remove redis caching

--- src/routes/api/server-count/+server.ts
+++ src/routes/api/server-count/+server.ts
@@ -1,5 +1,4 @@
 import { TOPGG_TOKEN } from "$env/static/private";
-import redis from "$lib/server/redis.js";
 import { json } from "@sveltejs/kit";
 
 export const config = {
@@ -12,10 +11,6 @@ export const GET = async ({ setHeaders }) => {
     "cache-control": "max-age=0, s-maxage=600, stale-while-revalidate",
   });
 
-  if (await redis.exists("server-count")) {
-    return json(await redis.get("server-count"));
-  }
-
   const res = await fetch("https://top.gg/api/bots/678711738845102087/stats", {
     headers: {
       Authorization: TOPGG_TOKEN,
@@ -25,8 +20,6 @@ export const GET = async ({ setHeaders }) => {
       // boobies
     }),
   );
-
-  await redis.set("server-count", JSON.stringify(res), { ex: 120 });
 
   return json(res);
 };
