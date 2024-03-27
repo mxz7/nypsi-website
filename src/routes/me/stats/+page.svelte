b42fa382865fbf8df71ab48430eedd641a7dd412
max <29782397+tekoh@users.noreply.github.com>
2024-03-27 22:39:55 +0000

    masonry yay :DDD

--- src/routes/me/stats/+page.svelte
+++ src/routes/me/stats/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import Loading from "$lib/components/Loading.svelte";
-  import Stats from "$lib/components/users/Stats.svelte";
   import { onMount } from "svelte";
-  import { fly } from "svelte/transition";
+  import { fly, fade } from "svelte/transition";
+  import Stats from "$lib/components/users/Stats.svelte";
 
   export let data;
   let loading = true;
@@ -18,22 +18,22 @@
 </svelte:head>
 
 <div class="flex w-full justify-center">
-  <div class="flex w-full flex-col gap-24 px-6 sm:w-fit sm:px-0">
-    {#if loading}
-      <div class="relative mt-2 w-full">
-        <Loading
-          fadeOutSettings={{ delay: 0, duration: 150 }}
-          fadeInSettings={{ delay: 50, duration: 100 }}
-        />
-      </div>
-    {:else}
+  {#if loading}
+    <div class="relative mt-2 w-full">
+      <Loading
+        fadeOutSettings={{ delay: 0, duration: 150 }}
+        fadeInSettings={{ delay: 50, duration: 100 }}
+      />
+    </div>
+  {:else}
+    <div class="columns-3 gap-3 px-4 sm:max-w-4xl sm:px-0">
       {#await data.streamed.gambleStats then gambleStats}
-        <div class="grid w-full grid-cols-1 gap-4 md:grid-cols-3">
-          {#each gambleStats as stat, i}
+        {#each gambleStats as stat, i}
+          <div class="break-inside-avoid-column pb-3">
             <a
               href="/game?user={data.user ? data.user.id : ''}&game={stat.game}"
-              class="h-fit rounded border border-slate-300 border-opacity-5 bg-slate-950 bg-opacity-25 p-4 duration-300 hover:border-accent hover:border-opacity-20 hover:bg-opacity-40"
-              in:fly|global={{ y: 50, duration: 500, delay: 100 * i }}
+              class="block rounded border border-slate-300 border-opacity-5 bg-slate-950 bg-opacity-25 p-4 duration-300 hover:border-accent hover:border-opacity-20 hover:bg-opacity-40"
+              in:fade|global={{ duration: 300, delay: 150 }}
             >
               <h1 class="text-center text-xl font-bold">{stat.game}</h1>
 
@@ -72,17 +72,17 @@
                 </p>
               </div>
             </a>
-          {/each}
-        </div>
+          </div>
+        {/each}
       {/await}
 
       {#await data.streamed.scratchStats then scratchStats}
-        <div class="grid w-full grid-cols-1 gap-4 md:grid-cols-3">
-          {#each scratchStats as stat, i}
+        {#each scratchStats as stat, i}
+          <div class="break-inside-avoid-column pb-3">
             <a
               href="/game?user={data.user ? data.user.id : ''}&game={stat.game}"
-              class="h-fit rounded border border-slate-300 border-opacity-5 bg-slate-950 bg-opacity-25 p-4 duration-300 hover:border-accent hover:border-opacity-20 hover:bg-opacity-40"
-              in:fly|global={{ y: 50, duration: 500, delay: 100 * i }}
+              class="block h-fit break-inside-avoid-column rounded border border-slate-300 border-opacity-5 bg-slate-950 bg-opacity-25 p-4 duration-300 hover:border-accent hover:border-opacity-20 hover:bg-opacity-40"
+              in:fade|global={{ duration: 300, delay: 150 }}
             >
               <h1 class="text-center text-xl font-bold">{stat.game.replaceAll("_", " ")}</h1>
 
@@ -93,76 +93,77 @@
                 ).toFixed(1)}%)
               </p>
             </a>
-          {/each}
-        </div>
+          </div>
+        {/each}
       {/await}
 
-      <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
-        {#await data.streamed.itemStats then itemData}
-          <div in:fly={{ y: 25, duration: 500 }}>
-            <Stats
-              title="items"
-              data={itemData
-                .filter((i) => data.items.find((item) => item.id === i.itemId))
-                .map(
-                  (i) =>
-                    `<div class="flex gap-2 items-center"><img loading="lazy" src="${
-                      data.items.find((item) => item.id === i.itemId).emoji
-                    }" class="h-4" /> <p><span class="text-slate-300">${
-                      data.items.find((item) => item.id === i.itemId).name
-                    }</span> <span class="text-slate-100"><span class='font-bold'>${i.amount.toLocaleString()}</span> uses</span></p></div>`,
-                )}
-            />
-          </div>
-        {/await}
-        {#await data.streamed.commandStats then commandData}
-          <div in:fly={{ y: 25, duration: 500 }}>
-            <Stats
-              title="commands"
-              data={commandData.map(
+      {#await data.streamed.itemStats then itemData}
+        <div class="break-inside-avoid-column pb-3" in:fade|global={{ duration: 300, delay: 150 }}>
+          <Stats
+            title="items"
+            data={itemData
+              .filter((i) => data.items.find((item) => item.id === i.itemId))
+              .map(
                 (i) =>
-                  `<p><span class="text-slate-300">$</span><span class="text-slate-100">${
-                    i.command
-                  }: <span class='font-bold'>${i.uses.toLocaleString()}</span> uses</span></p>`,
+                  `<div class="flex gap-2 items-center"><img loading="lazy" src="${
+                    data.items.find((item) => item.id === i.itemId).emoji
+                  }" class="h-4" /> <p><span class="text-slate-300">${
+                    data.items.find((item) => item.id === i.itemId).name
+                  }</span> <span class="text-slate-100"><span class='font-bold'>${i.amount.toLocaleString()}</span> uses</span></p></div>`,
               )}
+          />
+        </div>
+      {/await}
+      {#await data.streamed.commandStats then commandData}
+        <div class="break-inside-avoid-column pb-3" in:fade|global={{ duration: 300, delay: 150 }}>
+          <Stats
+            title="commands"
+            data={commandData.map(
+              (i) =>
+                `<p><span class="text-slate-300">$</span><span class="text-slate-100">${
+                  i.command
+                }: <span class='font-bold'>${i.uses.toLocaleString()}</span> uses</span></p>`,
+            )}
+          />
+        </div>
+      {/await}
+      {#await data.streamed.leaderboards then leaderboardData}
+        {#if leaderboardData.length > 0}
+          <div
+            class="break-inside-avoid-column pb-3"
+            in:fade|global={{ duration: 300, delay: 150 }}
+          >
+            <Stats
+              title="leaderboards"
+              data={leaderboardData.map((i) => {
+                let returnData = `<div class="flex gap-2 items-center">`;
+
+                if (i.leaderboard.startsWith("item-")) {
+                  returnData += `<img loading="lazy" src="${
+                    data.items.find((item) => item.id === i.leaderboard.substring(5)).emoji
+                  }" class="h-4" /> <p><span class="text-slate-300">${
+                    data.items.find((item) => item.id === i.leaderboard.substring(5)).name
+                  }</span>`;
+                } else {
+                  returnData += `<span class="text-slate-300">${i.leaderboard}</span>`;
+                }
+
+                returnData += ` <span class="${
+                  i.position === 1 ? "text-accent" : "text-slate-100"
+                } font-bold">#${i.position.toLocaleString()}</span></p></div>`;
+
+                return returnData;
+
+                // return `<p><span class="text-slate-300">$</span><span class="text-slate-100">${
+                //   i.command
+                // }: <span class='font-bold'>${i.uses.toLocaleString()}</span> uses</span></p>`;
+              })}
             />
           </div>
-        {/await}
-        {#await data.streamed.leaderboards then leaderboardData}
-          {#if leaderboardData.length > 0}
-            <div in:fly={{ y: 25, duration: 500 }}>
-              <Stats
-                title="leaderboards"
-                data={leaderboardData.map((i) => {
-                  let returnData = `<div class="flex gap-2 items-center">`;
-
-                  if (i.leaderboard.startsWith("item-")) {
-                    returnData += `<img loading="lazy" src="${
-                      data.items.find((item) => item.id === i.leaderboard.substring(5)).emoji
-                    }" class="h-4" /> <p><span class="text-slate-300">${
-                      data.items.find((item) => item.id === i.leaderboard.substring(5)).name
-                    }</span>`;
-                  } else {
-                    returnData += `<span class="text-slate-300">${i.leaderboard}</span>`;
-                  }
-
-                  returnData += ` <span class="${
-                    i.position === 1 ? "text-accent" : "text-slate-100"
-                  } font-bold">#${i.position.toLocaleString()}</span></p></div>`;
-
-                  return returnData;
-
-                  // return `<p><span class="text-slate-300">$</span><span class="text-slate-100">${
-                  //   i.command
-                  // }: <span class='font-bold'>${i.uses.toLocaleString()}</span> uses</span></p>`;
-                })}
-              />
-            </div>
-          {/if}
-        {/await}
-      </div>
-    {/if}
-  </div>
+        {/if}
+      {/await}
+    </div>
+  {/if}
 </div>
 
 <style>
