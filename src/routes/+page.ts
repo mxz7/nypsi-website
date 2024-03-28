1df047df38017fb6c52b08b2f1037df45300b223
max <29782397+tekoh@users.noreply.github.com>
2024-03-28 14:15:44 +0000

    remove cache header?

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,7 +4,7 @@ export const config = {
 };
 
 export const load = async ({ fetch, setHeaders }) => {
-  setHeaders({ "cache-control": "s-maxage=600" });
+  // setHeaders({ "cache-control": "s-maxage=600" });
   const data = fetch("/api/server-count").then(async (r) => r.json());
   return { topgg: data as Promise<{ server_count: number }> };
 };
