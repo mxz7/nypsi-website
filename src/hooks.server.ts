ee843bb02ed358c0bb2d106f9098446c345e00f9
max <29782397+tekoh@users.noreply.github.com>
2024-03-28 16:43:33 +0000

    maybe better ratelimiting

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,24 +1,16 @@
 import { dev } from "$app/environment";
 import rateLimiter from "$lib/server/ratelimit";
+import { error } from "@sveltejs/kit";
 
 export const handle = async ({ event, resolve }) => {
-  if (!dev && !event.isSubRequest && event.url.pathname.startsWith("/api")) {
+  if (!dev && (event.isDataRequest || event.url.pathname.startsWith("/api"))) {
     const rateLimitAttempt = await rateLimiter.limit(event.getClientAddress()).catch(() => {
       return { success: true, reset: 69 };
     });
 
     if (!rateLimitAttempt.success) {
       const timeRemaining = Math.floor((rateLimitAttempt.reset - new Date().getTime()) / 1000);
-      return new Response(
-        JSON.stringify({
-          message: `Too many requests. Please try again in ${timeRemaining} seconds.`,
-          error: 429,
-          status: 429,
-        }),
-        {
-          status: 429,
-        },
-      );
+      return error(429, { message: `too many requests. try again in ${timeRemaining} seconds` });
     }
   }
 
