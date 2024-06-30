5245b2e993e515ac15e0dd1c2f32a102f531ae42
max <29782397+mxz7@users.noreply.github.com>
2024-06-30 21:12:05 +0100

    add active users and daily commands

--- src/routes/bot/+page.server.ts
+++ src/routes/bot/+page.server.ts
@@ -13,6 +13,44 @@ export const config = {
 export async function load({ url }) {
   const days = parseInt(url.searchParams.get("days") || "30") || 30;
 
+  return {
+    queryGraph: await getDatabaseQueries(days),
+    usersCommandsGraph: await getActiveUsersAndCommands(days),
+  };
+}
+
+async function getData(key: string, days: number) {
+  const query = await prisma.botMetrics.findMany({
+    where: {
+      AND: [{ category: key }, { createdAt: { gt: dayjs().subtract(days, "days").toDate() } }],
+    },
+    select: {
+      createdAt: true,
+      value: true,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+
+  return query;
+}
+
+function getLabels(...dates: Date[][]) {
+  const labels: string[] = [];
+
+  for (const datesGroup of dates) {
+    for (const date of datesGroup) {
+      const formatted = dayjs(date).set("minutes", 0).set("seconds", 0).format("YYYY-MM-DD HH:mm");
+
+      if (!labels.includes(formatted)) labels.push(formatted);
+    }
+  }
+
+  return sort(labels).asc((i) => dayjs(i).valueOf());
+}
+
+async function getDatabaseQueries(days: number) {
   const queryGraph: ChartConfiguration = {
     type: "line",
     data: {
@@ -74,36 +112,72 @@ export async function load({ url }) {
     queryGraph.data.datasets[1].data.push(query.value);
   }
 
-  return { queryGraph };
+  return queryGraph;
 }
 
-async function getData(key: string, days: number) {
-  const query = await prisma.botMetrics.findMany({
-    where: {
-      AND: [{ category: key }, { createdAt: { gt: dayjs().subtract(days, "days").toDate() } }],
+async function getActiveUsersAndCommands(days: number) {
+  const graph: ChartConfiguration = {
+    type: "line",
+    data: {
+      labels: [],
+      datasets: [
+        {
+          yAxisID: "1",
+          label: "active users",
+          data: [],
+          fill: false,
+          borderColor: "#8b5cf6",
+          backgroundColor: "#8b5cf6",
+        },
+        {
+          yAxisID: "2",
+          label: "commands",
+          data: [],
+          fill: false,
+          borderColor: "#1e293b",
+          backgroundColor: "#1e293b",
+        },
+      ],
     },
-    select: {
-      createdAt: true,
-      value: true,
-    },
-    orderBy: {
-      createdAt: "asc",
-    },
-  });
+  };
 
-  return query;
-}
+  const [users, commands] = await Promise.all([
+    getData("daily_active_users", days),
+    getData("daily_commands", days),
+  ]);
 
-function getLabels(...dates: Date[][]) {
-  const labels: string[] = [];
+  graph.data.labels = getLabels(
+    users.map((i) => dayjs(i.createdAt).set("hours", 0).toDate()),
+    commands.map((i) => dayjs(i.createdAt).set("hours", 0).toDate()),
+  );
 
-  for (const datesGroup of dates) {
-    for (const date of datesGroup) {
-      const formatted = dayjs(date).set("minutes", 0).set("seconds", 0).format("YYYY-MM-DD HH:mm");
+  for (const query of users) {
+    const formattedDate = dayjs(query.createdAt)
+      .set("minutes", 0)
+      .set("seconds", 0)
+      .set("hours", 0)
+      .format("YYYY-MM-DD HH:mm");
 
-      if (!labels.includes(formatted)) labels.push(formatted);
-    }
+    const labelIndex = graph.data.labels.indexOf(formattedDate);
+
+    if (labelIndex === -1) continue;
+
+    graph.data.datasets[0].data.push(query.value);
   }
 
-  return sort(labels).asc((i) => dayjs(i).valueOf());
+  for (const query of commands) {
+    const formattedDate = dayjs(query.createdAt)
+      .set("minutes", 0)
+      .set("seconds", 0)
+      .set("hours", 0)
+      .format("YYYY-MM-DD HH:mm");
+
+    const labelIndex = graph.data.labels.indexOf(formattedDate);
+
+    if (labelIndex === -1) continue;
+
+    graph.data.datasets[1].data.push(query.value);
+  }
+
+  return graph;
 }
--- src/routes/bot/+page.svelte
+++ src/routes/bot/+page.svelte
@@ -5,7 +5,7 @@
 
   export let data;
 
-  const chartOptions: ChartOptions = {
+  const queryChartOptions: ChartOptions = {
     plugins: {
       legend: {
         display: false,
@@ -70,13 +70,71 @@
       },
     },
   };
+
+  const userChartOptions: ChartOptions = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        intersect: false,
+        mode: "index",
+      },
+    },
+    maintainAspectRatio: false,
+    responsive: true,
+    elements: {
+      line: {
+        tension: 0.2,
+      },
+      point: {
+        radius: 1,
+      },
+    },
+    scales: {
+      2: {
+        // min: 0,
+        // max: Number(sort(data.queryGraph.data.datasets[1].data).desc()[0]) * 2,
+        suggestedMax: Number(sort(data.usersCommandsGraph.data.datasets[1].data).desc()[0]) * 1.5,
+        suggestedMin: Number(sort(data.usersCommandsGraph.data.datasets[1].data).asc()[0]) / 1.5,
+        grid: {
+          display: false,
+        },
+        position: "right",
+        ticks: {
+          callback(tickValue) {
+            return Math.floor(Number(tickValue)).toLocaleString();
+          },
+          // maxTicksLimit: 3,
+          // autoSkipPadding: 100,
+        },
+      },
+      1: {
+        suggestedMax: Number(sort(data.usersCommandsGraph.data.datasets[0].data).desc()[0]) * 1.5,
+        suggestedMin: Number(sort(data.usersCommandsGraph.data.datasets[0].data).asc()[0]) / 1.5,
+        position: "left",
+        ticks: {
+          callback(tickValue) {
+            return Math.floor(Number(tickValue)).toLocaleString();
+          },
+          // maxTicksLimit: 3,
+          // autoSkipPadding: 100,
+        },
+      },
+    },
+  };
 </script>
 
 <div class="flex w-full justify-center">
   <div class="mt-14 w-full max-w-6xl">
     <h2 class="text-center text-3xl font-bold">database queries</h2>
     <div class="mt-6 w-full">
-      <Chart chartData={data.queryGraph} {chartOptions} />
+      <Chart chartData={data.queryGraph} chartOptions={queryChartOptions} />
+    </div>
+
+    <h2 class="mt-16 text-center text-3xl font-bold">active users and commands</h2>
+    <div class="mt-6 w-full">
+      <Chart chartData={data.usersCommandsGraph} chartOptions={userChartOptions} />
     </div>
   </div>
 </div>
