bb9e5a6726b16705498fdb0a6152e227bce1bc59
max <29782397+mxz7@users.noreply.github.com>
2024-07-09 21:50:29 +0100

    bigger text for mobile

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -15,15 +15,15 @@
         </div>
         <ul
           tabindex="0"
-          class="menu dropdown-content menu-sm z-[1] mt-3 w-52 gap-2 rounded-box bg-base-200 p-2 text-xl shadow"
+          class="menu dropdown-content menu-sm z-[1] mt-3 w-52 gap-3 rounded-box bg-base-200 p-2 shadow"
         >
-          <li><a href="/">home</a></li>
-          <li><a href="/leaderboard">leaderboards</a></li>
-          <li><a href="/status">status</a></li>
-          <li><a href="/discord" target="_blank">discord</a></li>
-          <li><a href="https://docs.nypsi.xyz/" target="_blank">docs</a></li>
+          <li><a href="/" class="text-xl">home</a></li>
+          <li><a href="/leaderboard" class="text-xl">leaderboards</a></li>
+          <li><a href="/status" class="text-xl">status</a></li>
+          <li><a href="/discord" target="_blank" class="text-xl">discord</a></li>
+          <li><a href="https://docs.nypsi.xyz/" target="_blank" class="text-xl">docs</a></li>
           <li>
-            <a href="https://ko-fi.com/tekoh/tiers" target="_blank">
+            <a href="https://ko-fi.com/tekoh/tiers" target="_blank" class="text-xl">
               <span
                 class="bg-gradient-to-br from-violet-500 to-purple-500 bg-clip-text font-semibold text-transparent"
                 >premium</span
