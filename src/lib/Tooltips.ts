96e20a39c059ed36b8d69324098252a7cebe3913
max <29782397+mxz7@users.noreply.github.com>
2024-07-09 17:26:15 +0100

    Revert "remove svelte-tippy"
    This reverts commit b175119a30d1b83e145772eebf9a63a33c41c12b.

---
+++ src/lib/Tooltips.ts
@@ -0,0 +1,11 @@
+import { createTippy } from "svelte-tippy";
+import "tippy.js/animations/perspective-subtle.css";
+
+const tooltip = createTippy({
+  touch: "hold",
+  inertia: true,
+  delay: 25,
+  animation: "perspective-subtle",
+});
+
+export default tooltip;
