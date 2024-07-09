98b41b730430312643a7b680dc774702e0fe8f8f
max <29782397+mxz7@users.noreply.github.com>
2024-07-09 12:31:43 +0100

    add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
-  theme: {
-    extend: {
-      colors: {
-        accent: "#8b5cf6",
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       accent: "#8b5cf6",
+  //     },
+  //   },
+  // },
+
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: {
+      nypsi: {
+        primary: "#8b5cf6",
+        secondary: "#C7F65C",
+        accent: "#5CF6D8",
+        "base-100": "#0f172a",
+        error: "#e31e3b",
+        success: "#68f78c",
+
+        // https://www.canva.com/colors/color-wheel/
       },
     },
   },
