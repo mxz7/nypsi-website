3209a007dd614f3305f2dfe9f8bb3867ad0abf33
max <29782397+mxz7@users.noreply.github.com>
2024-07-10 11:47:23 +0100

    fix: target=_blank

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -97,6 +97,7 @@
       <a
         href="https://discord.com/oauth2/authorize?client_id=678711738845102087&permissions=1377879583830&scope=bot%20applications.commands"
         class="animation-3 btn btn-primary bg-gradient-to-r from-violet-600 to-purple-600 fill-white text-lg text-white"
+        target="_blank"
       >
         <Discord class=" h-6 w-6 lg:h-7 lg:w-7" />
         <span class="lg:text-xl">add to discord</span>
