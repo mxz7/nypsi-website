e843d8a790751156acd9ff2fe8913172c17d7753
max <29782397+mxz7@users.noreply.github.com>
2024-07-10 13:05:48 +0100

    update terms of service

--- src/routes/(info-pages)/terms/+page.svelte
+++ src/routes/(info-pages)/terms/+page.svelte
@@ -3,30 +3,30 @@
 </svelte:head>
 
 <div class="flex w-full justify-center">
-  <div class="font-xl w-full pt-14 md:max-w-xl [&>h2]:my-4 [&>h2]:font-semibold">
-    <h1 class="mb-6 text-center text-2xl font-semibold">Terms of Service</h1>
+  <div class="font-xl w-full pt-14 md:max-w-3xl">
+    <h1 class="mb-6 text-center text-2xl font-semibold text-white">Terms of Service</h1>
 
     <h2>Usage Agreement</h2>
     <p>
-      By inviting the bot and using its features you are agreeing to the below mentioned Terms and
-      <a class="text-sky-300 underline" href="/privacy">Privacy Policy (Policy)</a> of the bot.
+      By inviting the bot to your Discord Server (server) and/or using its commands you are agreeing
+      to the below mentioned Terms and
+      <a class="link" href="/privacy">Privacy Policy</a> of the bot.
       <br />
-      You acknowledge that you have the priviledge to use the bot freely on any Discord Server (Server)
-      you share with it, that you can invite it to any Server that you have "Manage Server" rights for
-      and that this priviledge might get revoked for you, if you're subject of breaking the terms and/or
-      policy of this bot, or the Terms of Service, Privacy Policy and/or Community Guidelines of Discord
-      Inc.
+      You acknowledge that you have the privilege to use the bot freely on any server you share with
+      it, that you can invite it to any Server that you have "Manage Server" rights for and that this
+      priviledge might get revoked for you, if you're subject of breaking the terms and/or policy of
+      this bot, or the Terms of Service, Privacy Policy and/or Community Guidelines of Discord Inc.
       <br />
-      Through Inviting the bot may it collect specific data as described in its Policy. The intended
+      Through inviting the bot may it collect specific data as described in its Policy. The intended
       usage of this data is for core functionalities of the bot such as but not limited to: command handling
       and guild-specific settings
     </p>
 
     <h2>Liability</h2>
     <p>
-      The owner of the bot may face to liability due to users using the bot in its otherwise
+      The owner of the bot may not face liability due to users using the bot in its otherwise
       intended purpose.<br />This website and the bot may include user created content which is by
-      their own self and opinion, not created by the owner of nypsi. Moderation of user created
+      their own self and opinion, not created by the owner of the bot. Moderation of user created
       content is ongoing however errors do happen and things are missed.
     </p>
 
@@ -35,9 +35,10 @@
       Your access may be terminated to the bot and any services at any time without reason or notice
       under our discretion. To prevent this, please follow the rules in the <a
         href="https://discord.com/invite/hJTDNST"
-        class="text-sky-300 underline"
-        target="_blank">community Discord Server</a
-      >
+        class="link"
+        target="_blank"
+        >community Discord Server
+      </a>.
     </p>
 
     <h2>Changes</h2>
@@ -51,6 +52,12 @@
     </p>
 
     <h2>Contact</h2>
-    <a href="https://discord.com/invite/hJTDNST" class="text-sky-300 underline">Discord</a>
+    <p>Discord: @m.axz</p>
   </div>
 </div>
+
+<style>
+  h2 {
+    @apply my-4 font-semibold text-white;
+  }
+</style>
