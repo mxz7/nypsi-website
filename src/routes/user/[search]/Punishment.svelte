3d4e6d3ec30c4fc8a033d0d9862c6384ef3c8b2b
max <29782397+mxz7@users.noreply.github.com>
2024-07-10 16:23:07 +0100

    fix: punishment padding

--- src/routes/user/[search]/Punishment.svelte
+++ src/routes/user/[search]/Punishment.svelte
@@ -1,5 +1,5 @@
 <div
-  class="mt-4 rounded-lg border border-primary border-opacity-5 bg-base-200 duration-300 hover:border-opacity-20"
+  class="mt-4 rounded-lg border border-primary border-opacity-5 bg-base-200 py-5 duration-300 hover:border-opacity-20"
 >
   <h1 class="text-center text-error lg:text-lg">
     <slot />
