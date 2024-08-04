44ab468157970940353c379684a33124ecb737d2
amplitudes. <hey@amplitudes.me>
2024-08-04 20:37:33 +0100

    feat: update max age on tax endpoint to 10 minutes

--- src/routes/api/tax/+server.ts
+++ src/routes/api/tax/+server.ts
@@ -2,7 +2,7 @@ import { BOT_SERVER_URL } from "$env/static/private";
 import { json } from "@sveltejs/kit";
 
 export const GET = async ({ setHeaders }) => {
-  setHeaders({ "cache-control": "s-maxage=15" });
+  setHeaders({ "cache-control": "s-maxage=600" });
 
   const res = await fetch(`${BOT_SERVER_URL}/tax`).then((r) =>
     r.json().catch(() => {
