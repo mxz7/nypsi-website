f737ce0b045371c3455053136c02cd909ab65711
max <29782397+mxz7@users.noreply.github.com>
2024-10-03 17:48:23 +0100

    remove discord from footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,5 @@
 <script>
-  import Discord from "$lib/assets/svg/discord.svelte";
-  import { BadgeDollarSign, Code, FileText, Lock, Hammer } from "lucide-svelte";
+  import { BadgeDollarSign, Code, FileText, Hammer, Lock } from "lucide-svelte";
 </script>
 
 <footer class="mt-24 flex w-full justify-center bg-base-200 py-5">
@@ -23,13 +22,6 @@
     <a href="https://github.com/mxz7/nypsi-website" target="_blank">
       <Code size={16} strokeWidth={2} />
       <span>github</span>
-    </a>
-
-    <a href="/discord" target="_blank">
-      <div class="h-4 w-4">
-        <Discord />
-      </div>
-      <span>discord</span>
     </a>
 
     <a href="/supporters">
