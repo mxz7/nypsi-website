2556cbd7bed6a9e0c33d61a6fd77b8b4873e79ff
max <29782397+mxz7@users.noreply.github.com>
2024-10-03 17:46:27 +0100

    add rules page

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,22 +1,27 @@
 <script>
   import Discord from "$lib/assets/svg/discord.svelte";
-  import { BadgeDollarSign, Code, FileText, Lock } from "lucide-svelte";
+  import { BadgeDollarSign, Code, FileText, Lock, Hammer } from "lucide-svelte";
 </script>
 
 <footer class="mt-24 flex w-full justify-center bg-base-200 py-5">
   <div class="flex flex-wrap justify-evenly gap-8 px-4 lg:max-w-5xl lg:px-0">
     <a href="/terms">
-      <FileText size={16} strokeWidth={3} />
+      <FileText size={16} strokeWidth={2} />
       <span>terms</span>
     </a>
 
     <a href="/privacy">
-      <Lock size={16} strokeWidth={3} />
+      <Lock size={16} strokeWidth={2} />
       <span>privacy</span>
     </a>
 
+    <a href="/rules">
+      <Hammer size={16} strokeWidth={2} />
+      <span>rules</span>
+    </a>
+
     <a href="https://github.com/mxz7/nypsi-website" target="_blank">
-      <Code size={16} strokeWidth={3} />
+      <Code size={16} strokeWidth={2} />
       <span>github</span>
     </a>
 
@@ -28,7 +33,7 @@
     </a>
 
     <a href="/supporters">
-      <BadgeDollarSign size={16} strokeWidth={3} />
+      <BadgeDollarSign size={16} strokeWidth={2} />
       <span>supporters</span>
     </a>
   </div>
