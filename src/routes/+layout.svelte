5ad6ea63d56b9be299fdf800764c99abb7d8fbab
max <29782397+mxz7@users.noreply.github.com>
2024-10-26 16:39:31 +0100

    move font to head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -68,6 +68,13 @@
       src="https://analytics.maxz.dev/js/script.tagged-events.js"
     ></script>
   {/if}
+
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+    rel="stylesheet"
+  />
 </svelte:head>
 
 <div class="min-h-[100vh]">
