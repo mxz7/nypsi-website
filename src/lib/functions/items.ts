f96866f73b9ad0b986f72175cca22eff8180d124
max <29782397+mxz7@users.noreply.github.com>
2024-10-31 17:45:15 +0000

    only do state in browser

--- src/lib/functions/items.ts
+++ src/lib/functions/items.ts
@@ -1,9 +1,24 @@
+import { browser } from "$app/environment";
 import { items } from "$lib/state.svelte";
 import type { Item } from "$lib/types/Item";
 import { inPlaceSort } from "fast-sort";
 import parseEmoji from "./parseEmoji";
 
 export default async function getItems() {
+  if (!browser) {
+    let itemsData: Item[] = Object.values(
+      JSON.parse(
+        await fetch("https://raw.githubusercontent.com/mxz7/nypsi/main/data/items.json").then((r) =>
+          r.text(),
+        ),
+      ),
+    );
+
+    itemsData = itemsData.filter((i) => !["beginner_booster", "cycle"].includes(i.id));
+
+    return itemsData;
+  }
+
   if (items.value) return items.value;
 
   let itemsData: Item[] = Object.values(
