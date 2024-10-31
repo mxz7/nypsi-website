58817225414e6e0838ad1b0aac2148b38bd5301c
max <29782397+mxz7@users.noreply.github.com>
2024-10-31 14:55:39 +0000

    add new page stuff to mobile nav bar

--- src/lib/components/nav/MobileScreen.svelte
+++ src/lib/components/nav/MobileScreen.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import { X } from "lucide-svelte";
-  import { fly } from "svelte/transition";
+  import { page } from "$app/stores";
+  import { BadgePoundSterling, ChartArea, Coins, X } from "lucide-svelte";
+  import { fade, fly } from "svelte/transition";
 
   let { visible = $bindable(false) } = $props();
 </script>
@@ -9,23 +10,76 @@
   <div
     in:fly={{ x: -200, duration: 250 }}
     out:fly={{ x: -200, duration: 250 }}
-    class="fixed left-0 top-0 z-20 h-full w-[70%] rounded-box bg-base-200 p-2 shadow-xl shadow-base-300"
+    class="fixed left-0 top-0 z-20 h-full w-[70%] rounded-box bg-base-200 bg-opacity-95 p-2 shadow-xl shadow-base-300"
   >
     <button class="btn btn-ghost" onclick={() => (visible = !visible)}>
       <X strokeWidth={2.5} />
     </button>
 
     <ul class="menu font-medium" onclick={() => (visible = !visible)}>
-      <li><a href="/">home</a></li>
-      <li><a href="/leaderboard">leaderboards</a></li>
-      <li><a href="/status">status</a></li>
+      <li><a href="/" class={$page.url.pathname === "/" ? "text-primary" : ""}>home</a></li>
+      <li>
+        <a
+          href="/leaderboard"
+          class={$page.url.pathname.startsWith("/leaderboard") ? "text-primary" : ""}
+          >leaderboards</a
+        >
+      </li>
+      <li>
+        <a href="/status" class={$page.url.pathname.startsWith("/status") ? "text-primary" : ""}
+          >status</a
+        >
+      </li>
       <li><a href="/discord" target="_blank">discord</a></li>
       <li><a href="https://docs.nypsi.xyz" target="_blank">docs</a></li>
+
+      <li class="mt-1">
+        <h2 class={$page.url.pathname.startsWith("/me") ? "text-primary" : ""}>dashboard</h2>
+      </li>
+      <div class="pl-2">
+        <li>
+          <a
+            class="flex items-center {$page.url.pathname.startsWith('/me/stats')
+              ? 'text-primary'
+              : ''}"
+            href="/me/stats"
+          >
+            <Coins size={16} />
+            <span>stats</span>
+          </a>
+        </li>
+
+        <li>
+          <a
+            class="flex items-center {$page.url.pathname.startsWith('/me/graphs')
+              ? 'text-primary'
+              : ''}"
+            href="/me/graphs"
+          >
+            <ChartArea size={16} />
+            <span>graphs</span>
+          </a>
+        </li>
+
+        <li>
+          <a
+            class="flex items-center {$page.url.pathname.startsWith('/me/purchases')
+              ? 'text-primary'
+              : ''}"
+            href="/me/purchases"
+          >
+            <BadgePoundSterling size={16} />
+            <span>purchases</span>
+          </a>
+        </li>
+      </div>
     </ul>
   </div>
 
   <div
-    class="fixed left-0 top-0 z-10 h-full w-full"
+    in:fade={{ duration: 250 }}
+    out:fade={{ duration: 250 }}
+    class="fixed left-0 top-0 z-10 h-full w-full backdrop-blur-sm"
     role="button"
     onclick={() => (visible = !visible)}
   />
