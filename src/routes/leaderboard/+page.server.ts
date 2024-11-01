401417332619c34137e727e657523553c8218ee0
max <29782397+mxz7@users.noreply.github.com>
2024-11-01 00:33:27 +0000

    do not fetch net!! not needed!

--- src/routes/leaderboard/+page.server.ts
+++ src/routes/leaderboard/+page.server.ts
@@ -13,9 +13,6 @@ export async function load({ fetch, request, setHeaders }) {
       balance: await fetch("/api/leaderboard/balance").then(
         (r) => r.json() as Promise<LeaderboardData>,
       ),
-      net: await fetch("/api/leaderboard/networth").then(
-        (r) => r.json() as Promise<LeaderboardData>,
-      ),
       prestige: await fetch("/api/leaderboard/prestige").then(
         (r) => r.json() as Promise<LeaderboardData>,
       ),
@@ -23,7 +20,6 @@ export async function load({ fetch, request, setHeaders }) {
   } else {
     return {
       balance: fetch("/api/leaderboard/balance").then((r) => r.json() as Promise<LeaderboardData>),
-      net: fetch("/api/leaderboard/networth").then((r) => r.json() as Promise<LeaderboardData>),
       prestige: fetch("/api/leaderboard/prestige").then(
         (r) => r.json() as Promise<LeaderboardData>,
       ),
