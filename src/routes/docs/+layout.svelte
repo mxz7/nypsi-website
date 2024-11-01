1c3e05b9caccc74f775d6328485cb18db040ba2a
max <29782397+mxz7@users.noreply.github.com>
2024-11-01 15:27:30 +0000

    bye bye poppins

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -10,7 +10,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
   <link
-    href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&display=swap"
     rel="stylesheet"
   />
 </svelte:head>
@@ -52,17 +52,14 @@
 <style>
   :global(.docs-content h1) {
     @apply mb-6 text-3xl font-bold text-white lg:text-4xl;
-    font-family: "Poppins", sans-serif;
   }
 
   :global(.docs-content h2) {
     @apply my-4 text-2xl font-bold text-white;
-    font-family: "Poppins", sans-serif;
   }
 
   :global(.docs-content h3) {
     @apply my-2 text-xl font-semibold text-white;
-    font-family: "Poppins", sans-serif;
   }
 
   :global(.docs-content p) {
