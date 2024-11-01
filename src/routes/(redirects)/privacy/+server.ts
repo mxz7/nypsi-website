a0b1f06059c8b299d5c4a7b66bcf1e4147b70506
max <29782397+mxz7@users.noreply.github.com>
2024-11-01 15:33:10 +0000

    move privacy policy

--- src/routes/(redirects)/privacy/+server.ts
+++ src/routes/(redirects)/privacy/+server.ts
@@ -2,6 +2,6 @@ import { redirect } from "@sveltejs/kit";
 
 export const prerender = true;
 
-export function GET() {
-  return redirect(301, "/docs/rules");
+export function load() {
+  return redirect(301, "/docs/privacy");
 }
