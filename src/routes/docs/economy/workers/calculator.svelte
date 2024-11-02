95870b50280d4e18618742d5f05a3bfbd31be89f
max <29782397+mxz7@users.noreply.github.com>
2024-11-02 11:14:41 +0000

    worker calculator

--- src/routes/docs/economy/workers/calculator.svelte
+++ src/routes/docs/economy/workers/calculator.svelte
@@ -1,64 +1,60 @@
-<script>
-  let scanners = 0;
-  let radars = 0;
-  let stored = 10000;
+<script lang="ts">
+  let scanners = $state(0);
+  let radars = $state(0);
+  let stored: number | undefined = $state(0);
 
-  const scrapBase = 0.00009
-  const shardBase = 0.00003
+  $effect(() => {
+    console.log(isNaN(stored));
+    if (stored && isNaN(stored)) stored = 0;
+  });
 
-  const perScanner = 0.00077
-  const perRadar = 0.00027
+  const scrapBase = 0.00009;
+  const shardBase = 0.00003;
+
+  const perScanner = 0.00077;
+  const perRadar = 0.00027;
+
+  let scrapChance = $derived(
+    Math.round(Math.min(scrapBase + perScanner * scanners, 0.5) * stored * 10 ** 5) / 10 ** 5,
+  );
+  let shardChance = $derived(
+    Math.round(Math.min(shardBase + perRadar * radars, 0.1) * stored * 10 ** 5) / 10 ** 5,
+  );
 </script>
 
+<h2>quarry calculator</h2>
 
-<div class="bg-base-200" style="margin-left: 15rem; margin-right: 15rem; text-align: center">
-    
-  <select class="bg-base-200" style="margin-right:50px; margin-top:10px" bind:value={scanners}>
-    {#each {length: 11} as _, i}
-      {#if scanners === i}
-        <option value={i} selected>{i} scanner{i != 1 ? "s" : ""}</option>
-      {:else}
-        <option value={i}>{i} scanner{i != 1 ? "s" : ""}</option>
-      {/if}
-    {/each}
-  </select>
+<div class="w-64">
+  <div class="flex w-full gap-2">
+    <select class="input input-bordered grow" bind:value={scanners}>
+      {#each { length: 11 } as _, i}
+        {#if scanners === i}
+          <option value={i} selected>{i} scanner{i != 1 ? "s" : ""}</option>
+        {:else}
+          <option value={i}>{i} scanner{i != 1 ? "s" : ""}</option>
+        {/if}
+      {/each}
+    </select>
 
-  <select class="bg-base-200" bind:value={radars}>
-    {#each {length: 11} as _, i}
-      {#if radars === i}
-        <option value={i} selected>{i} radar{i != 1 ? "s" : ""}</option>
-      {:else}
-        <option value={i}>{i} radar{i != 1 ? "s" : ""}</option>
-      {/if}
-    {/each}
-  </select>
+    <select class="input input-bordered grow" bind:value={radars}>
+      {#each { length: 11 } as _, i}
+        {#if radars === i}
+          <option value={i} selected>{i} radar{i != 1 ? "s" : ""}</option>
+        {:else}
+          <option value={i}>{i} radar{i != 1 ? "s" : ""}</option>
+        {/if}
+      {/each}
+    </select>
+  </div>
 
-  <br>
-  <input class="bg-base-200" style="margin-top:10px" placeholder="stored" bind:value={stored} min=0 maxlength=6>
-
-  {#if stored > 0}
-    <br>
-    <br>
-    <span>Chance for a scrap: {Math.round(Math.min(scrapBase + perScanner * scanners, 0.5) * stored * 10 ** 5)/(10**5)}%</span>
-    <br>
-    <span>Chance for a shard: {Math.round(Math.min(shardBase + perRadar * radars, 0.1) * stored * 10 ** 5)/(10**5)}%</span>
-  {/if}
-
+  <label class="input input-bordered mt-2 flex w-full items-center gap-2">
+    stored
+    <input type="text" class="grow" placeholder="stored" maxlength="6" bind:value={stored} />
+  </label>
 </div>
 
-<style>
-  select {
-    width: 7.25rem;
-    height: 1.5rem;
-    border-width:1px;
-    border-radius: 6px;
-    border-color: var(--fallback-bc,oklch(var(--bc)/0.2))
-  }
-  input {
-    width: 7.25rem;
-    height: 1.5rem;
-    border-width:1px;
-    border-radius: 6px;
-    border-color: var(--fallback-bc,oklch(var(--bc)/0.2))
-  }
-</style>
+<br />
+<br />
+<span>Chance for a scrap: {scrapChance || 0}%</span>
+<br />
+<span>Chance for a shard: {shardChance || 0}%</span>
