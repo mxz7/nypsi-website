eb4a7d8f8536a70e7868fa3abf1508e6bc63ead8
max <29782397+mxz7@users.noreply.github.com>
2024-11-02 11:01:17 +0000

    meow

--- src/routes/docs/economy/karma/shop-items.svelte
+++ src/routes/docs/economy/karma/shop-items.svelte
@@ -1,44 +1,57 @@
 <script lang="ts">
-  import { browser } from "$app/environment";
   import parseEmoji from "$lib/functions/parseEmoji";
   import { onMount } from "svelte";
 
-  let items: {name: string, emoji: string, id: string, cost: number, items_left: number, aliases: string[], type: string, value: string, limit: number}[];
+  let items: {
+    name: string;
+    emoji: string;
+    id: string;
+    cost: number;
+    items_left: number;
+    aliases: string[];
+    type: string;
+    value: string;
+    limit: number;
+  }[] = $state();
 
   onMount(async () => {
-    items = await fetch("https://raw.githubusercontent.com/mxz7/nypsi/main/data/karmashop.json").then(response => response.json());
-  })
+    items = await fetch(
+      "https://raw.githubusercontent.com/mxz7/nypsi/main/data/karmashop.json",
+    ).then((response) => response.json());
+  });
 </script>
 
-<div class="container">
-  {#if browser && items != null}
+<div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
+  {#if items != null}
     {#each Object.values(items) as item}
       <div>
         <div class="flex items-center gap-2">
-          <img src={parseEmoji(item.emoji)} width=20 alt="">
-          <span class="font-bold">{item.name}</span>
-        </div>
-        
-        <div class="flex items-center gap-2" style="margin-top: 2px; margin-left: 33px">
-          <img src={parseEmoji("📦")} width=20 alt="">
-          <span>stock of {item.items_left}{item.items_left > 1 ? ` (limit of ${item.limit} per person)` : ""}</span>
+          <img
+            src={parseEmoji(item.emoji)}
+            class="w-5"
+            alt={item.id}
+            loading="lazy"
+            decoding="async"
+          />
+          <span class="font-semibold">{item.name}</span>
         </div>
 
-        <div class="flex items-center gap-2" style="margin-top: 2px; margin-bottom: 5px; margin-left: 33px">
-          <img src={parseEmoji("🔮")} width=20 alt="">
+        <div class="ml-8 mt-1 flex items-center gap-2">
+          <img src={parseEmoji("📦")} class="w-5" alt={item.id} loading="lazy" decoding="async" />
+          <span
+            >stock of {item.items_left}{item.items_left > 1
+              ? ` (limit of ${item.limit} per person)`
+              : ""}</span
+          >
+        </div>
+
+        <div class="my-1 ml-8 flex items-center gap-2">
+          <img src={parseEmoji("🔮")} class="w-5" alt={item.id} loading="lazy" decoding="async" />
           <span>{item.cost} karma</span>
         </div>
       </div>
     {/each}
   {:else}
-    loading...
+    <span class="loading loading-spinner"></span>
   {/if}
 </div>
-
-<style>
-  .container {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 10px;
-  }
-</style>
