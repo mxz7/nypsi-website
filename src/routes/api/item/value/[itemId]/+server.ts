c1d2b13795601025a355419763e91694076fa014
max <29782397+mxz7@users.noreply.github.com>
2024-11-12 20:34:11 +0000

    fuck it edge doesnt work

--- src/routes/api/item/value/[itemId]/+server.ts
+++ src/routes/api/item/value/[itemId]/+server.ts
@@ -1,29 +1,16 @@
 import { BOT_SERVER_URL } from "$env/static/private";
 import { json } from "@sveltejs/kit";
 
-export const config = {
-  runtime: "edge",
-  regions: "all",
-};
-
 export async function GET({ setHeaders, params, fetch }) {
   setHeaders({ "cache-control": "public, s-maxage=3600" });
-
-  console.log(`${BOT_SERVER_URL}/item/value/${params.itemId}`);
 
   const value = await fetch(`${BOT_SERVER_URL}/item/value/${params.itemId}`).then((r) => {
     if (r.ok) {
       return r.json().then((r) => r.value as number);
     } else {
-      console.error(r);
-      console.error(r.status);
-      console.error(r.statusText);
-      console.error(JSON.stringify(r.headers));
       return 0;
     }
   });
-
-  console.log(value);
 
   return json({ value });
 }
