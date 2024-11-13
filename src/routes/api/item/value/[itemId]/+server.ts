479cef92522b20ed37dec0a610fa39146a612415
max <29782397+mxz7@users.noreply.github.com>
2024-11-13 17:55:37 +0000

    use dynamic env

--- src/routes/api/item/value/[itemId]/+server.ts
+++ src/routes/api/item/value/[itemId]/+server.ts
@@ -1,4 +1,4 @@
-import { BOT_SERVER_URL } from "$env/static/private";
+import { env } from "$env/dynamic/private";
 import { json } from "@sveltejs/kit";
 
 export const config = {
@@ -9,7 +9,7 @@ export const config = {
 export async function GET({ setHeaders, params, fetch }) {
   setHeaders({ "cache-control": "public, max-age=600" });
 
-  const value = await fetch(`${BOT_SERVER_URL}/item/value/${params.itemId}`).then(async (r) => {
+  const value = await fetch(`${env.BOT_SERVER_URL}/item/value/${params.itemId}`).then(async (r) => {
     if (r.ok) return r.json().then((r) => r.value as number);
     else return 0;
   });
