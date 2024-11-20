1aa4d45f9239b26d6a7804dca4eb06eef05600f2
max <29782397+mxz7@users.noreply.github.com>
2024-11-20 20:49:34 +0000

    show avatar on guild page

--- src/routes/guild/[name]/Guild.svelte
+++ src/routes/guild/[name]/Guild.svelte
@@ -18,31 +18,28 @@
 
 <div class="mx-3 mb-10 mt-7 flex flex-col gap-4 sm:mx-auto md:w-full md:max-w-3xl">
   <div
-    class="w-full rounded-lg border border-primary border-opacity-15 bg-base-200 p-4 duration-300 hover:border-primary hover:border-opacity-30"
+    class="flex w-full gap-2 rounded-lg border border-primary border-opacity-15 bg-base-200 p-4 duration-300 hover:border-primary hover:border-opacity-30"
   >
-    <h1 class="text-center text-2xl font-bold text-white">{guild.guildName} [{guild.level}]</h1>
-
-    <p class="mt-2 flex w-full flex-row items-center justify-center gap-2 md:text-sm">
-      owned by
-      <a
-        class="link-hover flex gap-1 font-medium text-primary"
-        href="/user/{guild.owner.user.lastKnownUsername}"
-        ><img
-          class="m-0 h-5 rounded-full"
-          alt="owner pfp"
-          src={guild.owner.user.avatar}
-          onerror={handleFallbackImage}
-          decoding="async"
-          loading="lazy"
-        />
-        {guild.owner.user.lastKnownUsername}</a
-      >
-    </p>
-
-    <div class="mt-6 flex w-full justify-center">
-      <p class="w-fit whitespace-pre-line">
-        {guild.motd}
+    <div class="h-24 w-24 sm:h-36 sm:w-36">
+      <img
+        src={guild.avatarId ? `https://cdn.nypsi.xyz/${guild.avatarId}` : guild.owner.user.avatar}
+        alt=""
+        class="h-full w-full rounded-full"
+        loading="eager"
+        decoding="sync"
+        fetchpriority="high"
+      />
+    </div>
+    <div class="w-fit">
+      <h1 class="line-clamp-1 text-3xl font-extrabold text-white sm:text-4xl">{guild.guildName}</h1>
+      <p class="text-sm text-slate-200 sm:text-base">
+        owned by <a
+          href="/user/{guild.owner.user.lastKnownUsername}"
+          class="link-hover link-primary font-medium">{guild.owner.user.lastKnownUsername}</a
+        >
       </p>
+      <p class="text-xs text-slate-400 sm:text-sm">level {guild.level}</p>
+      <p class="mt-3 text-sm md:text-base">{guild.motd}</p>
     </div>
   </div>
 
