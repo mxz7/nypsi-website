43754fbd80adf1215202c252da8c8bb65db57b9c
max <29782397+mxz7@users.noreply.github.com>
2024-11-22 13:15:07 +0000

    guild: skip form redirect

--- src/routes/guild/+layout.svelte
+++ src/routes/guild/+layout.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
   import { navigating, page } from "$app/stores";
   import { guildSearchTerm } from "$lib/state.svelte";
-  interface Props {
-    children?: import("svelte").Snippet;
-  }
 
-  let { children }: Props = $props();
+  let { children } = $props();
 
   guildSearchTerm.value = $page.url.searchParams.get("search") || $page.params.name || "";
 </script>
 
 <div class="mb-2 mt-5 flex justify-center">
-  <form>
-    <!-- svelte-ignore a11y_autofocus -->
+  <form
+    onsubmit={(e) => {
+      e.preventDefault();
+      goto(`/guild/${encodeURIComponent(guildSearchTerm.value.replaceAll(" ", "-"))}`);
+    }}
+  >
     <input
       class="input input-bordered"
       type="text"
