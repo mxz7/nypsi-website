ae54c5a54c1846ea03416731e8792a9701730114
max <29782397+mxz7@users.noreply.github.com>
2024-11-23 21:05:19 +0000

    longer cache

--- src/routes/api/user/getid/[username]/+server.ts
+++ src/routes/api/user/getid/[username]/+server.ts
@@ -3,7 +3,7 @@ import { error, json } from "@sveltejs/kit";
 
 export const GET = async ({ params, setHeaders }) => {
   setHeaders({
-    "cache-control": "public, max-age=600, must-revalidate",
+    "cache-control": "public, max-age=3600",
   });
 
   const query = await prisma.user.findFirst({
