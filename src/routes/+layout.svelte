1f50bdc19f786eade115c4c35a06fbc6d1640680
max <29782397+mxz7@users.noreply.github.com>
2024-11-24 18:55:50 +0000

    improve favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -44,17 +44,29 @@
 </script>
 
 <svelte:head>
-  <meta name="og:url" content={$page.url.toString()} />
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-  <link rel="manifest" href="/site.webmanifest" />
-  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+  <link rel="apple-touch-icon" sizes="57x57" href="/favicon-57x57.png" type="image/png" />
+  <link rel="apple-touch-icon" sizes="60x60" href="/favicon-60x60.png" type="image/png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="/favicon-72x72.png" type="image/png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="/favicon-114x114.png" type="image/png" />
+  <link rel="apple-touch-icon" sizes="120x120" href="/favicon-120x120.png" type="image/png" />
+  <link rel="apple-touch-icon" sizes="144x144" href="/favicon-144x144.png" type="image/png" />
+  <link rel="apple-touch-icon" sizes="152x152" href="/favicon-152x152.png" type="image/png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/favicon-180x180.png" type="image/png" />
+  <link rel="icon" sizes="16x16" href="/favicon-16x16.png" type="image/png" />
+  <link rel="icon" sizes="32x32" href="/favicon-32x32.png" type="image/png" />
+  <link rel="icon" sizes="96x96" href="/favicon-96x96.png" type="image/png" />
+  <link rel="icon" sizes="192x192" href="/favicon-192x192.png" type="image/png" />
+  <link rel="icon" sizes="512x512" href="/favicon-512x512.png" type="image/png" />
+  <link rel="manifest" href="/manifest.json" />
+  <meta name="msapplication-TileColor" content="#0f172a" />
+  <meta name="msapplication-TileImage" content="/favicon-512x512.png" />
 
   <meta
     name="keywords"
     content="nypsi, discord, bot, server, economy, gambling, moderation, reaction roles, wholesome"
   />
+
+  <meta name="og:url" content={$page.url.toString()} />
 
   <meta name="theme-color" content="#0f172a" media="(prefers-color-scheme: dark)" />
   <meta name="theme-color" content="#0f172a" media="(prefers-color-scheme: light)" />
