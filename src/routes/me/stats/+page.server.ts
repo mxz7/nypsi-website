882cce8048fcab630c2029e25c8b07d8dc618eb8
max <29782397+mxz7@users.noreply.github.com>
2024-11-28 18:59:44 +0000

    stats caching

--- src/routes/me/stats/+page.server.ts
+++ src/routes/me/stats/+page.server.ts
@@ -1,4 +1,5 @@
 import prisma from "$lib/server/database.js";
+import redis from "$lib/server/redis.js";
 import { inPlaceSort } from "fast-sort";
 
 export async function load({ parent, setHeaders }) {
@@ -25,66 +26,101 @@ export async function load({ parent, setHeaders }) {
       select: { itemId: true, amount: true },
       orderBy: { amount: "desc" },
     }),
-
-    scratchStats: prisma.game
-      .groupBy({
-        where: {
-          AND: [{ userId: user.id }, { game: { contains: "scratch" } }],
-        },
-        by: ["game"],
-        _count: {
-          _all: true,
-        },
-        _sum: {
-          win: true,
-        },
-      })
-      .then((r) => inPlaceSort(r).desc((r) => r._count._all)),
-    gambleStats: prisma.game
-      .groupBy({
-        where: {
-          AND: [{ userId: user.id }, { game: { not: { contains: "scratch" } } }],
-        },
-        by: ["game"],
-        _count: {
-          _all: true,
-        },
-        _avg: {
-          bet: true,
-        },
-        _sum: {
-          bet: true,
-          earned: true,
-          xpEarned: true,
-        },
-      })
-      .then(async (res) => {
-        inPlaceSort(res).desc((i) => i._count._all);
-
-        const wins = new Map<string, number>();
-
-        const promises: Promise<void>[] = [];
-
-        for (const game of res) {
-          const func = async () => {
-            wins.set(
-              game.game,
-              await prisma.game.count({
-                where: {
-                  AND: [{ userId: user ? user.id : "0" }, { game: game.game }, { win: 1 }],
-                },
-              }),
-            );
-          };
-
-          promises.push(func());
-        }
-
-        await Promise.all(promises);
-
-        return res.map((game) => {
-          return { ...game, wins: wins.get(game.game) };
-        });
-      }),
+    scratchStats: getScratchStats(user.id),
+    gambleStats: getGambleStats(user.id),
   };
+}
+
+async function getScratchStats(userId: string) {
+  const cache = await redis.get(`cache:scratchstats:${userId}`);
+
+  if (cache) {
+    return JSON.parse(cache);
+  }
+
+  const query = await prisma.game
+    .groupBy({
+      where: {
+        AND: [{ userId }, { game: { contains: "scratch" } }],
+      },
+      by: ["game"],
+      _count: {
+        _all: true,
+      },
+      _sum: {
+        win: true,
+      },
+    })
+    .then((r) => inPlaceSort(r).desc((r) => r._count._all));
+
+  await redis.set(`cache:scratchstats:${userId}`, JSON.stringify(query), "EX", 600);
+
+  return query;
+}
+
+async function getGambleStats(userId: string) {
+  const cache = await redis.get(`cache:gamblestats:${userId}`);
+
+  if (cache) {
+    return JSON.parse(cache);
+  }
+
+  const data = await prisma.game
+    .groupBy({
+      where: {
+        AND: [{ userId: userId }, { game: { not: { contains: "scratch" } } }],
+      },
+      by: ["game"],
+      _count: {
+        _all: true,
+      },
+      _avg: {
+        bet: true,
+      },
+      _sum: {
+        bet: true,
+        earned: true,
+        xpEarned: true,
+      },
+    })
+    .then(async (res) => {
+      inPlaceSort(res).desc((i) => i._count._all);
+
+      const wins = new Map<string, number>();
+
+      const promises: Promise<void>[] = [];
+
+      for (const game of res) {
+        const func = async () => {
+          wins.set(
+            game.game,
+            await prisma.game.count({
+              where: {
+                AND: [{ userId }, { game: game.game }, { win: 1 }],
+              },
+            }),
+          );
+        };
+
+        promises.push(func());
+      }
+
+      await Promise.all(promises);
+
+      return res.map((game) => {
+        return {
+          ...game,
+          _sum: {
+            bet: Number(game._sum.bet),
+            earned: Number(game._sum.earned),
+            xpEarned: Number(game._sum.xpEarned),
+          },
+          wins: wins.get(game.game),
+        };
+      });
+    });
+
+  await redis.set(`cache:gamblestats:${userId}`, JSON.stringify(data), "EX", 600);
+
+  return data;
 }
