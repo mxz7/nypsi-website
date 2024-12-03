82173e69740d6dc33d87066cfad076268bba9edc
max <29782397+mxz7@users.noreply.github.com>
2024-12-03 20:44:08 +0000

    improve page load with inline font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -80,13 +80,6 @@
       data-website-id="d0bd590b-cc67-4315-b725-0105ada8ce61"
     ></script>
   {/if}
-
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-    rel="stylesheet"
-  />
 </svelte:head>
 
 <div class="min-h-[100vh]">
