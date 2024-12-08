b96c8946882b53c95fdd8c3178b47fb670e460e8
max <29782397+mxz7@users.noreply.github.com>
2024-12-08 14:15:35 +0000

    fix: remove sleep

--- src/routes/item/(item)/+layout.ts
+++ src/routes/item/(item)/+layout.ts
@@ -1,7 +1,5 @@
 import getItems from "$lib/functions/items";
-import sleep from "$lib/functions/sleep";
 
 export async function load({ fetch }) {
-  await sleep(1000);
   return { items: await getItems(fetch) };
 }
