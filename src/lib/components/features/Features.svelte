6f18c4a79e5eebb379dbe5ef36c6c9f821ff3da2
max <29782397+mxz7@users.noreply.github.com>
2024-12-11 20:33:55 +0000

    add farms feature section

--- src/lib/components/features/Features.svelte
+++ src/lib/components/features/Features.svelte
@@ -1,42 +1,51 @@
 <script>
-  import { BadgeDollarSign, Bell, Book, Clipboard, Coins, Hammer, Keyboard } from "lucide-svelte";
+  import {
+    BadgeDollarSign,
+    Bell,
+    Book,
+    Clipboard,
+    Coins,
+    Hammer,
+    Keyboard,
+    Leaf,
+  } from "lucide-svelte";
 </script>
 
-<div class="flex w-full justify-center bg-base-200" id="features">
-  <div class="w-full py-28 lg:max-w-5xl">
-    <h2>
-      <div class="rounded-lg bg-base-300 p-3">
-        <BadgeDollarSign class="text-primary" size={32} strokeWidth={2.5} />
-      </div>
-      <span>economy</span>
-    </h2>
-    <div class="grid w-full grid-cols-1 px-3 lg:grid-cols-2 lg:px-0">
-      <ul>
-        <li>resets twice a year with <strong>real money rewards</strong></li>
-        <li>over 150+ items to collect</li>
-        <li>bakery, gambling, farming, workers, minecraft, cryptocurrency and much more</li>
-        <li><a href="/seasons" class="link" target="_blank">view season history</a></li>
-        <li><a href="/leaderboard" class="link" target="_blank">view leaderboards</a></li>
-      </ul>
-      <div class="pt-0 lg:pl-12">
-        <picture>
-          <source srcset="https://cdn.nypsi.xyz/static/features/economy.avif" type="image/avif" />
-          <img
-            src="https://cdn.nypsi.xyz/static/features/economy.webp"
-            alt="networth command"
-            loading="lazy"
-            decoding="async"
-            width="814"
-            height="954"
-          />
-        </picture>
+<div id="features">
+  <div class="w-full bg-base-200">
+    <div class="mx-auto w-full py-28 lg:max-w-5xl">
+      <h2>
+        <div class="rounded-lg bg-base-300 p-3">
+          <BadgeDollarSign class="text-primary" size={32} strokeWidth={2.5} />
+        </div>
+        <span>economy</span>
+      </h2>
+      <div class="grid w-full grid-cols-1 px-3 lg:grid-cols-2 lg:px-0">
+        <ul>
+          <li>resets twice a year with <strong>real money rewards</strong></li>
+          <li>over 150+ items to collect</li>
+          <li>bakery, gambling, farming, workers, minecraft, cryptocurrency and much more</li>
+          <li><a href="/seasons" class="link" target="_blank">view season history</a></li>
+          <li><a href="/leaderboard" class="link" target="_blank">view leaderboards</a></li>
+        </ul>
+        <div class="pt-0 lg:pl-12">
+          <picture>
+            <source srcset="https://cdn.nypsi.xyz/static/features/economy.avif" type="image/avif" />
+            <img
+              src="https://cdn.nypsi.xyz/static/features/economy.webp"
+              alt="networth command"
+              loading="lazy"
+              decoding="async"
+              width="814"
+              height="954"
+            />
+          </picture>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
-<div class="flex w-full justify-center bg-base-100" id="features">
-  <div class="w-full py-28 lg:max-w-5xl">
+  <div class="mx-auto w-full py-28 lg:max-w-5xl">
     <h2>
       <div class="rounded-lg bg-base-300 p-3">
         <Keyboard class="text-primary" size={32} strokeWidth={2.5} />
@@ -81,48 +90,46 @@
       </div>
     </div>
   </div>
-</div>
 
-<div class="flex w-full justify-center bg-base-200" id="features">
-  <div class="w-full py-28 lg:max-w-5xl">
-    <h2>
-      <div class="rounded-lg bg-base-300 p-3">
-        <Coins class="text-primary" size={32} strokeWidth={2.5} />
-      </div>
-      <span>gambling</span>
-    </h2>
-    <div class="grid w-full grid-cols-1 px-3 lg:grid-cols-2 lg:px-0">
-      <ul>
-        <li>
-          many different gambling games including blackjack, mines, dragon tower, races and scratch
-          cards
-        </li>
-        <li>earn xp to level up</li>
-        <li>contribute earned rewards to guilds for higher bonuses</li>
-        <li>use boosters to push the limits</li>
-      </ul>
-      <div class="pt-0 lg:pl-12">
-        <picture>
-          <source
-            srcset="https://cdn.nypsi.xyz/static/features/scratchcard.avif"
-            type="image/avif"
-          />
-          <img
-            src="https://cdn.nypsi.xyz/static/features/scratchcard.webp"
-            alt="finished scratch card"
-            loading="lazy"
-            decoding="async"
-            width="674"
-            height="716"
-          />
-        </picture>
+  <div class="w-full bg-base-200">
+    <div class="mx-auto w-full py-28 lg:max-w-5xl">
+      <h2>
+        <div class="rounded-lg bg-base-300 p-3">
+          <Coins class="text-primary" size={32} strokeWidth={2.5} />
+        </div>
+        <span>gambling</span>
+      </h2>
+      <div class="grid w-full grid-cols-1 px-3 lg:grid-cols-2 lg:px-0">
+        <ul>
+          <li>
+            many different gambling games including blackjack, mines, dragon tower, races and
+            scratch cards
+          </li>
+          <li>earn xp to level up</li>
+          <li>contribute earned rewards to guilds for higher bonuses</li>
+          <li>use boosters to push the limits</li>
+        </ul>
+        <div class="pt-0 lg:pl-12">
+          <picture>
+            <source
+              srcset="https://cdn.nypsi.xyz/static/features/scratchcard.avif"
+              type="image/avif"
+            />
+            <img
+              src="https://cdn.nypsi.xyz/static/features/scratchcard.webp"
+              alt="finished scratch card"
+              loading="lazy"
+              decoding="async"
+              width="674"
+              height="716"
+            />
+          </picture>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
-<div class="flex w-full justify-center bg-base-100" id="features">
-  <div class="w-full py-28 lg:max-w-5xl">
+  <div class="mx-auto w-full py-28 lg:max-w-5xl">
     <h2>
       <div class="rounded-lg bg-base-300 p-3">
         <Hammer class="text-primary" size={32} strokeWidth={2.5} />
@@ -155,45 +162,43 @@
       </div>
     </div>
   </div>
-</div>
 
-<div class="flex w-full justify-center bg-base-200" id="features">
-  <div class="w-full py-28 lg:max-w-5xl">
-    <h2>
-      <div class="rounded-lg bg-base-300 p-3">
-        <Book class="text-primary" size={32} strokeWidth={2.5} />
-      </div>
-      <span>reaction roles</span>
-    </h2>
-    <div class="grid w-full grid-cols-1 px-3 lg:grid-cols-2 lg:px-0">
-      <ul>
-        <li>modern implementation using buttons rather than emojis reactions</li>
-        <li>use an emoji or a text label</li>
-        <li>unique and many modes preventing users from collecting all roles</li>
-        <li>role whitelist - require members to have a specific role before using</li>
-      </ul>
-      <div class="pt-0 lg:pl-12">
-        <picture>
-          <source
-            srcset="https://cdn.nypsi.xyz/static/features/reactionroles.avif"
-            type="image/avif"
-          />
-          <img
-            src="https://cdn.nypsi.xyz/static/features/reactionroles.webp"
-            alt="reaction roles"
-            loading="lazy"
-            decoding="async"
-            width="872"
-            height="1158"
-          />
-        </picture>
+  <div class="w-full bg-base-200">
+    <div class="mx-auto w-full py-28 lg:max-w-5xl">
+      <h2>
+        <div class="rounded-lg bg-base-300 p-3">
+          <Book class="text-primary" size={32} strokeWidth={2.5} />
+        </div>
+        <span>reaction roles</span>
+      </h2>
+      <div class="grid w-full grid-cols-1 px-3 lg:grid-cols-2 lg:px-0">
+        <ul>
+          <li>modern implementation using buttons rather than emojis reactions</li>
+          <li>use an emoji or a text label</li>
+          <li>unique and many modes preventing users from collecting all roles</li>
+          <li>role whitelist - require members to have a specific role before using</li>
+        </ul>
+        <div class="pt-0 lg:pl-12">
+          <picture>
+            <source
+              srcset="https://cdn.nypsi.xyz/static/features/reactionroles.avif"
+              type="image/avif"
+            />
+            <img
+              src="https://cdn.nypsi.xyz/static/features/reactionroles.webp"
+              alt="reaction roles"
+              loading="lazy"
+              decoding="async"
+              width="872"
+              height="1158"
+            />
+          </picture>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
-<div class="flex w-full justify-center bg-base-100" id="features">
-  <div class="w-full py-28 lg:max-w-5xl">
+  <div class="mx-auto w-full py-28 lg:max-w-5xl">
     <h2>
       <div class="rounded-lg bg-base-300 p-3">
         <Clipboard class="text-primary" size={32} strokeWidth={2.5} />
@@ -226,32 +231,60 @@
       </div>
     </div>
   </div>
-</div>
 
-<div class="flex w-full justify-center bg-base-200" id="features">
-  <div class="w-full py-28 lg:max-w-5xl">
+  <div class="w-full bg-base-200">
+    <div class="mx-auto w-full py-28 lg:max-w-5xl">
+      <h2>
+        <div class="rounded-lg bg-base-300 p-3">
+          <Bell class="text-primary" size={32} strokeWidth={2.5} />
+        </div>
+        <span>mention history</span>
+      </h2>
+      <div class="grid w-full grid-cols-1 px-3 lg:grid-cols-2 lg:px-0">
+        <ul>
+          <li>no more annoying ghost pings 🙄</li>
+          <li>works in every server</li>
+          <li>your messages are safe with two layer encryption</li>
+        </ul>
+        <div class="pt-0 lg:pl-12">
+          <picture>
+            <source srcset="https://cdn.nypsi.xyz/static/features/pings.avif" type="image/avif" />
+            <img
+              src="https://cdn.nypsi.xyz/static/features/pings.webp"
+              alt="pings command"
+              loading="lazy"
+              decoding="async"
+              width="606"
+              height="706"
+            />
+          </picture>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="mx-auto w-full py-28 lg:max-w-5xl">
     <h2>
       <div class="rounded-lg bg-base-300 p-3">
-        <Bell class="text-primary" size={32} strokeWidth={2.5} />
+        <Leaf class="text-primary" size={32} strokeWidth={2.5} />
       </div>
-      <span>mention history</span>
+      <span>farms</span>
     </h2>
     <div class="grid w-full grid-cols-1 px-3 lg:grid-cols-2 lg:px-0">
       <ul>
-        <li>no more annoying ghost pings 🙄</li>
-        <li>works in every server</li>
-        <li>your messages are safe with two layer encryption</li>
+        <li>grow plants and make money from your yield</li>
+        <li>take care of your plants with watering and fertiliser</li>
       </ul>
-      <div class="pt-0 lg:pl-12">
+      <div class="pt-0 lg:-order-1 lg:pr-12">
         <picture>
-          <source srcset="https://cdn.nypsi.xyz/static/features/pings.avif" type="image/avif" />
+          <source srcset="https://cdn.nypsi.xyz/static/features/farm.avif" type="image/avif" />
           <img
-            src="https://cdn.nypsi.xyz/static/features/pings.webp"
-            alt="pings command"
+            src="https://cdn.nypsi.xyz/static/features/farm.webp"
+            alt="role persist list"
             loading="lazy"
             decoding="async"
-            width="606"
-            height="706"
+            width="1196"
+            height="668"
           />
         </picture>
       </div>
