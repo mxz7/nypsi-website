768cc63f66942aabd8271a13f17800ab954f58f2
max <29782397+mxz7@users.noreply.github.com>
2024-12-24 13:55:26 +0000

    unneccessary menu

--- src/routes/me/guilds/+layout.svelte
+++ src/routes/me/guilds/+layout.svelte
@@ -20,41 +20,38 @@
   <ul class="menu hidden h-fit w-72 rounded-box bg-base-200 p-4 lg:block">
     <li><h2 class="menu-title">server management</h2></li>
 
-    <ul class="menu font-medium">
-      <li>
-        <a class="text-sm opacity-70" href="/me">
-          <ArrowLeft size={16} />
-          <span>back</span>
+    <li class="m-0">
+      <a class="text-sm opacity-70" href="/me">
+        <ArrowLeft size={16} />
+        <span>back</span>
+      </a>
+    </li>
+
+    {#each data.guilds as guild}
+      <li class={(parseInt(guild.permissions) & 0x20) == 0x20 ? "" : "disabled"}>
+        <a
+          class="flex items-center {page.url.pathname.startsWith(`/me/guilds/${guild.id}`)
+            ? 'text-primary'
+            : ''} {(parseInt(guild.permissions) & 0x20) == 0x20 ? '' : 'cursor-not-allowed'}"
+          href="/me/guilds/{guild.id}"
+        >
+          <img
+            src={guild.icon
+              ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}`
+              : "https://cdn.discordapp.com/avatars/678711738845102087/cb2dcd61010f2b89ceb1cd5ff15816cf.png?size=256"}
+            alt=""
+            height="256"
+            width="256"
+            class="h-8 w-8 rounded-xl"
+            loading="lazy"
+            decoding="async"
+          />
+          <span>{guild.name}</span>
         </a>
       </li>
-    </ul>
-
-    <ul class="menu">
-      {#each data.guilds as guild}
-        <li class={(parseInt(guild.permissions) & 0x20) == 0x20 ? "" : "disabled"}>
-          <a
-            class="flex items-center {page.url.pathname.startsWith(`/me/guilds/${guild.id}`)
-              ? 'text-primary'
-              : ''} {(parseInt(guild.permissions) & 0x20) == 0x20 ? '' : 'cursor-not-allowed'}"
-            href="/me/guilds/{guild.id}"
-          >
-            <img
-              src={guild.icon
-                ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}`
-                : "https://cdn.discordapp.com/avatars/678711738845102087/cb2dcd61010f2b89ceb1cd5ff15816cf.png?size=256"}
-              alt=""
-              height="256"
-              width="256"
-              class="h-8 w-8 rounded-xl"
-              loading="lazy"
-              decoding="async"
-            />
-            <span>{guild.name}</span>
-          </a>
-        </li>
-      {/each}
-    </ul>
+    {/each}
   </ul>
+
   <div class="w-full p-4 lg:p-0">
     {@render children()}
   </div>
