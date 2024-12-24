f5297ee66a95239fc659b596086646f8b911bec3
max <29782397+mxz7@users.noreply.github.com>
2024-12-24 12:41:59 +0000

    allow logout to follow next

--- src/routes/(auth)/logout/+server.ts
+++ src/routes/(auth)/logout/+server.ts
@@ -2,12 +2,16 @@ import { lucia } from "$lib/server/functions/auth.js";
 import redis from "$lib/server/redis.js";
 import { redirect } from "@sveltejs/kit";
 
-export const GET = async ({ locals }) => {
+export const GET = async ({ locals, url }) => {
   const auth = await locals.validate();
 
   if (!auth?.session) return redirect(302, "/");
   await lucia.invalidateSession(auth.session.id);
   await redis.del(`cache:session:${auth.session.id}`);
+
+  const next = url.searchParams.get("next");
+
+  if (next) return redirect(302, next);
 
   return redirect(302, "/");
 };
