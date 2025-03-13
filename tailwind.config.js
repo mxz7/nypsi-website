ffb702a5962d4757485cb55252906d13e5a29217
mxz7 <29782397+mxz7@users.noreply.github.com>
2025-03-13 19:20:46 +0000

    remove daisyui stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,4 @@
-import { createRequire } from "module";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
-  // theme: {
-  //   extend: {
-  //     colors: {
-  //       accent: "#8b5cf6",
-  //     },
-  //   },
-  // },
-
-  plugins: [createRequire(import.meta.url)("daisyui")],
-
-  daisyui: {
-    logs: false,
-    themes: [
-      {
-        nypsi: {
-          primary: "#8b5cf6",
-          secondary: "#342C7D",
-          accent: "#6F49BF",
-          "base-100": "#091121",
-          error: "#e31e3b",
-          success: "#68f78c",
-          "base-content": "#d9dce1",
-
-          // https://www.canva.com/colors/color-wheel/
-        },
-      },
-    ],
-  },
 };
