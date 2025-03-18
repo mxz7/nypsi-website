acf78041acea5e22fce5ffdd521224996b6c8e36
mxz7 <29782397+mxz7@users.noreply.github.com>
2025-03-18 13:01:59 +0000

    use menu for navbar

--- src/lib/components/nav/Navigation.svelte
+++ src/lib/components/nav/Navigation.svelte
@@ -37,18 +37,24 @@
           </picture>
         </a>
 
-        <a href="/leaderboard" class="btn btn-ghost font-semibold">leaderboards</a>
-        <a href="/item" class="btn btn-ghost font-semibold">items</a>
-        <a href="/status" class="btn btn-ghost font-semibold">status</a>
-        <a href="/docs" class="btn btn-ghost font-semibold">docs</a>
-        <a href="/discord" class="btn btn-ghost font-semibold" target="_blank">discord</a>
-        <a href="https://ko-fi.com/tekoh/tiers" target="_blank" class="btn btn-ghost py-0">
-          <span
-            class="bg-linear-to-br from-violet-500 to-purple-500 bg-clip-text py-1 font-bold text-transparent"
-          >
-            premium
-          </span>
-        </a>
+        <ul class="menu menu-md menu-horizontal">
+          <li><a href="/leaderboard" class=" font-semibold">leaderboards</a></li>
+          <li><a href="/item" class=" font-semibold">items</a></li>
+          <li><a href="/status" class=" font-semibold">status</a></li>
+          <li><a href="/docs" class=" font-semibold">docs</a></li>
+          <li>
+            <a href="/discord" class=" font-semibold" target="_blank">discord</a>
+          </li>
+          <li>
+            <a href="https://ko-fi.com/tekoh/tiers" target="_blank">
+              <span
+                class="bg-linear-to-br from-violet-500 to-purple-500 bg-clip-text font-bold text-transparent"
+              >
+                premium
+              </span>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
 
