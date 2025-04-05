8abe6f061274b323b35aa15325215e95ec63ced3
mxz7 <29782397+mxz7@users.noreply.github.com>
2025-04-05 16:54:58 +0100

    more semantic html

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,42 +3,48 @@
 </script>
 
 <footer class="bg-base-200 mt-24 flex w-full justify-center py-5">
-  <div class="flex flex-wrap justify-evenly gap-x-10 gap-y-6 px-12 lg:max-w-5xl lg:px-0">
-    <a href="/terms">
-      <FileText size={16} strokeWidth={2} />
-      <span>terms</span>
-    </a>
+  <menu class="flex flex-wrap justify-evenly gap-x-10 gap-y-6 px-12 lg:max-w-5xl lg:px-0">
+    <li>
+      <a href="/terms">
+        <FileText size={16} strokeWidth={2} />
+        <span>terms</span>
+      </a>
+    </li>
 
-    <a href="/privacy">
-      <Lock size={16} strokeWidth={2} />
-      <span>privacy</span>
-    </a>
+    <li>
+      <a href="/privacy">
+        <Lock size={16} strokeWidth={2} />
+        <span>privacy</span>
+      </a>
+    </li>
 
-    <a href="/rules">
-      <Hammer size={16} strokeWidth={2} />
-      <span>rules</span>
-    </a>
+    <li>
+      <a href="/rules">
+        <Hammer size={16} strokeWidth={2} />
+        <span>rules</span>
+      </a>
+    </li>
 
-    <a href="https://github.com/mxz7/nypsi-website" target="_blank">
-      <Code size={16} strokeWidth={2} />
-      <span>github</span>
-    </a>
+    <li>
+      <a href="https://github.com/mxz7/nypsi-website" target="_blank">
+        <Code size={16} strokeWidth={2} />
+        <span>github</span>
+      </a>
+    </li>
 
-    <a href="/supporters">
-      <BadgeDollarSign size={16} strokeWidth={2} />
-      <span>supporters</span>
-    </a>
-  </div>
+    <li>
+      <a href="/supporters">
+        <BadgeDollarSign size={16} strokeWidth={2} />
+        <span>supporters</span>
+      </a>
+    </li>
+  </menu>
 </footer>
 
 <style>
   @reference "../../app.css";
 
   a {
-    @apply fill-base-content flex items-center gap-1 duration-300 hover:fill-white hover:text-white;
-  }
-
-  a > span {
-    @apply text-xs;
+    @apply fill-base-content link-hover flex items-center gap-1 text-xs duration-300;
   }
 </style>
