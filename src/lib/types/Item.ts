0a3d6417df236d10d30a8a3921e5d92e64798dc7
mxz7 <29782397+mxz7@users.noreply.github.com>
2025-04-06 12:13:33 +0100

    use new loot pools for scratch card odds

--- src/lib/types/Item.ts
+++ src/lib/types/Item.ts
@@ -37,3 +37,31 @@ export type Item = {
   // upgrades?: CarUpgradeType;
   plantId: string; // for seeds
 };
+
+export type LootPool = {
+  nothing?: number; // weight
+  money?: {
+    [amount: number]: number; // amount: weight
+  };
+  xp?: {
+    [amount: number]: number; // amount: weight
+  };
+  karma?: {
+    [amount: number]: number; // amount: weight
+  };
+  items?: {
+    [item: string]: LootPoolItemEntry;
+  };
+};
+
+export type LootPoolItemEntry =
+  | {
+      weight?: number;
+      count?:
+        | {
+            min: number;
+            max: number;
+          }
+        | number;
+    }
+  | number; // item: weight, count assumed to be 1
--- src/routes/item/(item)/[itemId]/+page.ts
+++ src/routes/item/(item)/[itemId]/+page.ts
@@ -1,10 +1,11 @@
 import { browser } from "$app/environment";
+import { getCrateOdds } from "$lib/functions/items.js";
 import sleep from "$lib/functions/sleep";
 import { error } from "@sveltejs/kit";
-import { sort } from "fast-sort";
+import { inPlaceSort } from "fast-sort";
 
 export async function load({ params, parent, fetch, setHeaders }) {
-  const { items } = await parent();
+  const [{ items }, crateOdds] = await Promise.all([parent(), getCrateOdds(fetch)]);
 
   const selected = items.find((i) => i.id === params.itemId);
 
@@ -42,23 +43,41 @@ export async function load({ params, parent, fetch, setHeaders }) {
         ),
       );
     } else if (selected.role === "scratch-card") {
-      const arr: { itemId: string; chance: string }[] = [];
+      const lootPool = crateOdds[selected.id];
 
-      for (const item of selected.items) {
-        if (item.split(":")[0] === "id") {
-          arr.push({
-            itemId: item.split(":")[1],
-            chance: (item.split(":")[2] || "100") + "%",
-          });
-        } else {
-          arr.push({
-            itemId: item.split(":")[0] + ":" + item.split(":")[1],
-            chance: (item.split(":")[2] || "100") + "%",
-          });
+      odds.crate_open = [];
+
+      if (Object.hasOwn(lootPool, "money")) {
+        for (const amount in lootPool.money) {
+          odds.crate_open.push({ itemId: `money:${amount}`, chance: lootPool.money[amount] + "%" });
         }
       }
 
-      odds.crate_open = sort(arr).desc((i) =>
+      if (Object.hasOwn(lootPool, "xp")) {
+        for (const amount in lootPool.xp) {
+          odds.crate_open.push({ itemId: `xp:${amount}`, chance: lootPool.xp[amount] + "%" });
+        }
+      }
+
+      if (Object.hasOwn(lootPool, "karma")) {
+        for (const amount in lootPool.karma) {
+          odds.crate_open.push({ itemId: `karma:${amount}`, chance: lootPool.karma[amount] + "%" });
+        }
+      }
+
+      if (Object.hasOwn(lootPool, "items")) {
+        for (const item in lootPool.items) {
+          const entry = lootPool.items[item];
+
+          if (typeof entry === "number") {
+            odds.crate_open.push({ itemId: item, chance: entry + "%" });
+          } else {
+            odds.crate_open.push({ itemId: item, chance: entry.weight + "%" });
+          }
+        }
+      }
+
+      inPlaceSort(odds.crate_open).desc((i) =>
         parseFloat(i.chance.substring(0, i.chance.length - 1)),
       );
     }
@@ -69,9 +88,19 @@ export async function load({ params, parent, fetch, setHeaders }) {
 
     for (const item of items) {
       if (item.role === "scratch-card") {
-        if (item.items.find((i) => i.split(":")[1] === selected.id)) {
-          odds.found[item.id] =
-            (item.items.find((i) => i.split(":")[1] === selected.id).split(":")[2] || "100") + "%";
+        const lootPool = crateOdds[item.id];
+
+        if (Object.hasOwn(lootPool, "items")) {
+          const items = lootPool["items"];
+
+          if (items[selected.id]) {
+            const entry = items[selected.id];
+            if (typeof entry === "number") {
+              odds.found[item.id] = entry + "%";
+            } else {
+              odds.found[item.id] = entry.weight + "%";
+            }
+          }
         }
       } else if (item.role === "crate" || item.role === "tool") {
         if (
