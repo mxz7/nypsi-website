96d948b6f4302af57c7af5885f7d33caec0d755d
mxz7 <29782397+mxz7@users.noreply.github.com>
2025-06-08 15:55:55 +0100

    ad vote streak to vote leaderboards page

---
+++ src/routes/leaderboard/vote/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import MiniLeaderboard from "../MiniLeaderboard.svelte";
+
+  let { data } = $props();
+</script>
+
+<svelte:head>
+  <title>leaderboards | nypsi</title>
+  <meta name="og:title" content="leaderboards | nypsi" />
+</svelte:head>
+
+<div class="mt-8 flex w-full justify-center">
+  <div class="grid w-full grid-cols-1 flex-wrap justify-center gap-16 lg:max-w-4xl lg:grid-cols-2">
+    <MiniLeaderboard title="votes this month" data={data.balance} tags={data.tags} limit={100} />
+    <MiniLeaderboard title="vote streak" data={data.prestige} tags={data.tags} limit={100} />
+  </div>
+</div>
---
+++ src/routes/leaderboard/vote/+page.ts
@@ -0,0 +1,32 @@
+import { browser } from "$app/environment";
+import sleep from "$lib/functions/sleep.js";
+import type { LeaderboardData } from "$lib/types/LeaderboardData.js";
+
+export async function load({ fetch, setHeaders }) {
+  const monthly = fetch("/api/leaderboard/vote/month").then(
+    (r) => r.json() as Promise<LeaderboardData>,
+  );
+  const streak = fetch("/api/leaderboard/vote/streak").then(
+    (r) => r.json() as Promise<LeaderboardData>,
+  );
+
+  if (browser) {
+    const res = await Promise.race([Promise.all([monthly, streak]), sleep(50)]);
+
+    if (typeof res === "boolean") {
+      setHeaders({ "x-accel-buffering": "no" });
+
+      return {
+        balance: monthly,
+        prestige: streak,
+      };
+    } else {
+      return { balance: await monthly, prestige: await streak };
+    }
+  } else {
+    return {
+      balance: await monthly,
+      prestige: await streak,
+    };
+  }
+}
