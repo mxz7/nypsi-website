68e7a8a13f402ee2215f71940fded065732b46bc
mxz7 <29782397+mxz7@users.noreply.github.com>
2025-07-23 11:33:24 +0100

    nicer ui

--- src/routes/me/guilds/[guildId]/chatfilter/+page.svelte
+++ src/routes/me/guilds/[guildId]/chatfilter/+page.svelte
@@ -25,17 +25,19 @@
   <span>{data.guild.name} chat filter</span>
 </h1>
 
-<div class="flex w-full flex-col-reverse gap-8 lg:flex-row">
+<div class="grid w-full grid-cols-1 gap-8 lg:grid-cols-2">
   {#if data.filter.length > 0}
     <Table filter={data.filter} />
   {:else}
-    <p class="text-error">no filter items</p>
+    <div class="bg-base-200 text-error mt-4 h-fit w-full rounded-lg p-4 text-center">
+      <p>no filter items</p>
+    </div>
   {/if}
 
   <form
     method="post"
     action="?/create"
-    class="mt-4 flex w-full flex-col gap-2 lg:max-w-xs"
+    class="-order-1 mt-4 flex w-full flex-col gap-2 lg:order-1 lg:max-w-xs"
     use:superEnhance
   >
     <label class="form-control w-full">
@@ -52,11 +54,12 @@
     </label>
 
     <input
-      type="number"
+      type="text"
       defaultValue="100"
       max="100"
       min="1"
       class="input input-bordered w-full"
+      inputmode="numeric"
       name="match"
       bind:value={$form.match}
     />
