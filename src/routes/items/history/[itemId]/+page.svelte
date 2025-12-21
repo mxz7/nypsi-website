8b3e8b2640f61aeb080928197f1eeaa843a93d42
max <29782397+mxz7@users.noreply.github.com>
2025-12-21 11:59:32 +0000

    wip: redesigning chart page

--- src/routes/items/history/[itemId]/+page.svelte
+++ src/routes/items/history/[itemId]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { page } from "$app/state";
+  import Card from "$lib/components/Card.svelte";
   import Chart from "$lib/components/Chart.svelte";
   import type { ChartOptions } from "chart.js";
 
@@ -13,10 +14,9 @@
         mode: "index",
         callbacks: {
           label(tooltipItem) {
-            if (tooltipItem.dataset.label.includes("items in world"))
+            if (tooltipItem.dataset.label.includes("items in world")) {
               return `in world: ${tooltipItem.formattedValue}`;
-            else if (tooltipItem.dataset.label === (data.user ? data.user.username : "null"))
-              return `${data.user ? data.user.username : "null"}: ${tooltipItem.formattedValue}`;
+            }
 
             return `${tooltipItem.dataset.label} average: $${tooltipItem.formattedValue}`;
           },
@@ -71,33 +71,27 @@
   <title>{data.item?.name} history | nypsi</title>
 </svelte:head>
 
-{#if !data.auth}
-  <div class="flex w-full justify-center">
-    <main class="mt-14 px-3 text-center">
-      <h1 class="mb-4 text-2xl font-bold text-white">you must be logged in to view this page</h1>
-      <p>
-        to view item history you need a <a
-          href="https://ko-fi.com/nypsi"
-          target="_blank"
-          class="link link-primary">premium membership</a
-        >
-      </p>
+<main class="mx-auto mt-4 w-full max-w-6xl space-y-4">
+  <header class="flex justify-center gap-3">
+    <div class="rounded-box bg-base-300 h-14 w-14 p-2">
+      <img
+        class="h-full w-full object-contain"
+        src={data.item.emoji}
+        alt=""
+        decoding="sync"
+        loading="eager"
+      />
+    </div>
 
-      <a href="/login?next={encodeURIComponent(page.url.pathname)}" class="btn mt-4">log in</a>
-    </main>
-  </div>
-{:else if data.premium}
-  <header class="mt-5 mb-10 text-center sm:mb-3 sm:w-full">
-    <h1 class="text-4xl font-bold text-white sm:text-5xl">
-      {data.item?.name} history
-    </h1>
-    <div class="bg-primary m-auto mt-3 h-1 w-3/4 rounded-full sm:w-1/2"></div>
+    <h1 class="my-auto text-3xl font-bold text-white">{data.item.name} history</h1>
   </header>
 
+  <!-- <h1 class="text-center text-4xl font-bold text-white sm:text-5xl">
+      {data.item.name}
+    </h1> -->
+
   {#key data.graphData}
-    <ol
-      class="menu menu-horizontal rounded-box bg-base-200 mx-auto my-10 flex justify-center gap-2"
-    >
+    <menu class="menu menu-horizontal rounded-box bg-base-200 mx-auto flex justify-center gap-2">
       {#each [14, 30, 45, 60, 90, 69420] as option}
         {@const focused = days === option.toString()}
         <li>
@@ -110,23 +104,7 @@
           >
         </li>
       {/each}
-      <!-- <select
-        name="days"
-        id="days"
-        class="bg-gray-950 text-gray-100"
-        bind:value={days}
-        onchange={() => {
-          goto(`?days=${days}`);
-        }}
-      >
-        <option value="14">14 days</option>
-        <option value="30">30 days</option>
-        <option value="45">45 days</option>
-        <option value="60">60 days</option>
-        <option value="90">90 days</option>
-        <option value="69420">all time</option>
-      </select> -->
-    </ol>
+    </menu>
 
     <div>
       {#if data.graphData === "invalid item"}
@@ -138,25 +116,10 @@
           <h1>not enough data</h1>
         </div>
       {:else if typeof data.graphData !== "string"}
-        <div class="flex h-full w-full justify-center">
-          <div class="h-[40vh] w-full px-4 sm:h-[65vh] sm:w-[70vw] sm:px-0">
-            <Chart chartData={data.graphData} {chartOptions} />
-          </div>
-        </div>
+        <Card class="mx-auto max-w-6xl">
+          <Chart chartData={data.graphData} {chartOptions} />
+        </Card>
       {/if}
     </div>
   {/key}
-{:else}
-  <div class="flex w-full justify-center">
-    <div class="mt-14 text-center">
-      <h1 class="text-3xl font-bold text-white">you must have premium for item graphs</h1>
-      <p>
-        you can buy premium <a
-          href="https://ko-fi.com/nypsi"
-          target="_blank"
-          class="link link-primary">here</a
-        >
-      </p>
-    </div>
-  </div>
-{/if}
+</main>
