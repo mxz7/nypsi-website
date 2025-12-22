65a3dc4dc599acb183ab131315bb6a50f70edc42
max <29782397+mxz7@users.noreply.github.com>
2025-12-22 18:34:22 +0000

    add caching of market orders

--- src/lib/api/market.remote.ts
+++ src/lib/api/market.remote.ts
@@ -1,10 +1,37 @@
 import { query } from "$app/server";
 import prisma from "$lib/server/database";
+import redis from "$lib/server/redis";
+import type { OrderType } from "@generated/prisma";
+import ms from "ms";
 import z from "zod";
+
+type MarketOrder = {
+  owner: {
+    id: string;
+    username: string;
+    avatar: string;
+  };
+  itemAmount: number;
+  price: bigint;
+  orderType: OrderType;
+  messageId: string;
+  createdAt: Date;
+  completed: boolean;
+};
 
 export const getOrders = query(
   z.object({ itemId: z.string(), page: z.number().min(1).default(1) }),
   async ({ itemId, page }) => {
+    const cache = await redis.get(`cache:market:orders:${itemId}:p${page}`);
+
+    if (cache) {
+      const parsed = JSON.parse(cache);
+
+      return parsed.map((order) => {
+        return { ...order, createdAt: new Date(order.createdAt) };
+      }) as MarketOrder[];
+    }
+
     const orders = await prisma.market.findMany({
       where: {
         itemId,
@@ -40,7 +67,7 @@ export const getOrders = query(
       },
     });
 
-    return orders.map((order) => {
+    const transformed = orders.map((order) => {
       const shouldAnonymize = !order.owner.user.Preferences?.leaderboards;
 
       const owner = shouldAnonymize
@@ -57,6 +84,17 @@ export const getOrders = query(
         ...order,
         owner,
       };
-    });
+    }) as MarketOrder[];
+
+    await redis.set(
+      `cache:market:orders:${itemId}:p${page}`,
+      JSON.stringify(transformed, (key, value) =>
+        typeof value === "bigint" ? Number(value) : value,
+      ),
+      "EX",
+      ms("12 hours") / 1000,
+    );
+
+    return transformed;
   },
 );
