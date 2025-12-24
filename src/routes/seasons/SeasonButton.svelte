c5718984ed74237a2d621ef362150cc0953324c2
max <29782397+mxz7@users.noreply.github.com>
2025-12-24 11:43:11 +0000

    give seasonbutton new home with seasons page

--- src/routes/seasons/SeasonButton.svelte
+++ src/routes/seasons/SeasonButton.svelte
@@ -11,5 +11,4 @@
   class="bg-base-200 m-1 flex items-center justify-center rounded-md border border-transparent p-4 text-lg duration-200 ease-in hover:scale-105 hover:border-slate-500 sm:m-1.5"
 >
   <p class="text-center text-white sm:text-xl">{number}</p>
-  <!-- <img class="max-w-full max-h-full object-contain w-auto h-auto" src={item.emoji} alt="" /> -->
 </a>
