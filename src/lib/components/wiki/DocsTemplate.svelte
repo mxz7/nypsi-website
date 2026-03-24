1ce0b14a97a5e16e75d5777197704785ceb759cf
max <29782397+mxz7@users.noreply.github.com>
2026-03-24 19:00:16 +0000

    fix(wiki): add `og:description` meta tag

--- src/lib/components/wiki/DocsTemplate.svelte
+++ src/lib/components/wiki/DocsTemplate.svelte
@@ -13,6 +13,7 @@
   <title>{title} | nypsi wiki</title>
   <meta name="og:title" content="{title} | nypsi wiki" />
   <meta name="description" content={description} />
+  <meta name="og:description" content={description} />
 </svelte:head>
 
 <DocsHeader anchor={title.replaceAll(" ", "-")} header="h1" text={title} />
