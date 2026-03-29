e2285a94c15a0101bea7f642cbd927adc335bf9e
max <29782397+mxz7@users.noreply.github.com>
2026-03-29 14:56:10 +0100

    fix(profile): remove error routes

--- src/routes/users/private/+page.svelte
+++
@@ -1,6 +0,0 @@
-<svelte:head>
-  <title>unknown user | nypsi</title>
-  <meta name="robots" content="noindex" />
-</svelte:head>
-
-<h1 class="w-full text-center text-xl font-bold text-slate-200">private profile</h1>
