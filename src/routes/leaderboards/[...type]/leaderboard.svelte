48313a89b655434f980308a603c146aa324d3541
max <29782397+mxz7@users.noreply.github.com>
2026-04-03 14:28:13 +0100

    feat(leaderboards): improve styling

--- src/routes/leaderboards/[...type]/leaderboard.svelte
+++ src/routes/leaderboards/[...type]/leaderboard.svelte
@@ -3,6 +3,7 @@
   import type { LeaderboardData } from "$lib/types/LeaderboardData";
   import { Crown, LoaderCircle } from "@lucide/svelte";
   import { fade } from "svelte/transition";
+  import { twMerge } from "tailwind-merge";
 
   interface Props {
     title: string;
@@ -26,8 +27,12 @@
         <LoaderCircle class="text-primary animate-spin" size={32} strokeWidth={2.5} />
       </div>
     {:else}
+      {@const tableClasses = "table table-sm md:table-md w-full"}
+      {@const tableModifiers =
+        "[&_tbody_tr:nth-child(odd)]:bg-base-200 [&_tbody_td]:border-b-0 [&_tbody_tr:nth-child(even)]:bg-transparent"}
+
       <table
-        class="table-sm md:table-md [&_tbody_tr:nth-child(odd)]:bg-base-200/80 table w-full [&_tbody_td]:border-b-0 [&_tbody_td]:align-middle [&_tbody_td:first-child]:w-px [&_tbody_tr]:border-b-0 [&_tbody_tr:nth-child(even)]:bg-transparent [&_thead_th:first-child]:w-px"
+        class={twMerge(tableClasses, tableModifiers)}
         in:fade={{ duration: 200, delay: 100 }}
         out:fade={{ duration: 100 }}
       >
@@ -38,52 +43,51 @@
             <th class="text-right">value</th>
           </tr>
         </thead>
+
         <tbody>
           {#each data as { position, user, value }, i}
             <tr>
               <td
-                class="rounded-l-lg py-3 pr-0 pl-1 text-center text-sm whitespace-nowrap md:py-5 md:pl-3 md:text-base {i ===
+                class="rounded-l-lg py-3 pr-1 pl-1 text-center font-mono text-sm whitespace-nowrap md:py-5 md:pl-3 {i ===
                 0
                   ? 'text-primary font-semibold'
-                  : ''}"
+                  : 'text-base-content/75'}"
               >
                 {#if position === 1}
-                  <Crown size={24} class="inline" />
+                  <Crown size={20} class="inline" />
                 {:else}
                   {position}
                 {/if}
               </td>
 
-              <td class="min-w-0 py-3 md:py-5">
-                <span class="flex w-full min-w-0 items-center gap-2 md:gap-3">
+              <td class="max-w-48 min-w-0 truncate py-3 pl-2 sm:max-w-full md:py-5">
+                <span class="flex items-center gap-1">
                   {#if user.id}
-                    <span class="flex min-w-0 items-center gap-1">
-                      {#if user.tag}
-                        <span
-                          class="tooltip tooltip-top flex flex-none items-center"
-                          data-tip={tags[user.tag]?.name}
-                        >
-                          [
-                          <img
-                            class="h-5 w-5 object-contain"
-                            height="32"
-                            width="32"
-                            src={tags[user.tag]?.emoji}
-                            alt=""
-                            decoding="async"
-                          />
-                          ]
-                        </span>
-                      {/if}
-                      <a
-                        href={`${userRoute}/${user.id.replaceAll(" ", "-")}`}
-                        class="{i === 0
-                          ? 'text-primary font-semibold'
-                          : ''} link-hover min-w-0 overflow-hidden text-sm text-ellipsis whitespace-nowrap underline-offset-2 md:text-base"
+                    {#if user.tag}
+                      <span
+                        class="tooltip tooltip-top flex flex-none items-center"
+                        data-tip={tags[user.tag]?.name}
                       >
-                        {user.username}
-                      </a>
-                    </span>
+                        [
+                        <img
+                          class="h-5 w-5 object-contain"
+                          height="32"
+                          width="32"
+                          src={tags[user.tag]?.emoji}
+                          alt=""
+                          decoding="async"
+                        />
+                        ]
+                      </span>
+                    {/if}
+                    <a
+                      href={`${userRoute}/${user.id.replaceAll(" ", "-")}`}
+                      class="{i === 0
+                        ? 'text-primary font-semibold'
+                        : ''} link-hover min-w-0 overflow-hidden text-sm text-ellipsis whitespace-nowrap underline-offset-2 md:text-base"
+                    >
+                      {user.username}
+                    </a>
                   {:else}
                     <a
                       href="/wiki/economy/user-settings/hidden"
@@ -98,7 +102,7 @@
               </td>
 
               <td
-                class="rounded-r-lg py-3 pr-1 text-right text-sm md:py-5 md:pr-3 md:text-base {i ===
+                class="rounded-r-lg py-3 pr-2 text-right text-sm md:py-5 md:pr-3 md:text-base {i ===
                 0
                   ? 'text-primary font-semibold'
                   : ''}"
