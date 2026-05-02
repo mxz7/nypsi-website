38ef65caacca5906c6d6d59aa203a3a1e1593528
max <29782397+mxz7@users.noreply.github.com>
2026-05-02 11:42:12 +0100

    perf(db): max 10 connections

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -2,7 +2,7 @@ import { env } from "$env/dynamic/private";
 import { PrismaClient } from "@generated/prisma";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL, max: 10 });
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
