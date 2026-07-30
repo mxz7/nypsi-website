a12894d24485e460d5aaa68554abc0ec5218b31e
max <29782397+mxz7@users.noreply.github.com>
2026-07-30 18:27:05 +0100

    fix(docs): pets styling

--- src/routes/wiki/economy/pets/pets.svelte
+++ src/routes/wiki/economy/pets/pets.svelte
@@ -10,7 +10,7 @@
   });
 </script>
 
-<div class="grid grid-cols-1 gap-3 lg:grid-cols-2">
+<div class="grid grid-cols-1 gap-3 lg:grid-cols-2 mt-2">
   {#if pets.length === 0}
     <span class="loading loading-spinner"></span>
   {:else}
@@ -18,11 +18,15 @@
       <a
         href="/items/{pet.id}"
         class="bg-base-200 hover:bg-base-300 flex items-center gap-3 rounded-lg p-3 duration-100"
+        style:color="inherit"
+        style:text-decoration="none"
       >
         <img src={pet.emoji} alt="" class="size-8 object-contain" loading="lazy" decoding="async" />
 
         <span>
-          <span class="block font-semibold">{pet.name}</span>
+          <span class="text-primary block font-semibold underline underline-offset-3"
+            >{pet.name}</span
+          >
           <span class="text-sm">{pet.longDesc}</span>
         </span>
       </a>
